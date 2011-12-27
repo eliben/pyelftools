54ff260e99e42183830de2415cd93cc1d6e044b9
Eli Bendersky <eliben@gmail.com>
2011-12-27 06:51:20 +0200

    a bit progress on locationlists

--- elftools/dwarf/locationlists.py
+++ elftools/dwarf/locationlists.py
@@ -11,8 +11,34 @@ from collections import namedtuple
 from ..common.utils import struct_parse
 
 
+LocationEntry = namedtuple('LocationEntry', 'begin_offset end_offset loc_expr')
+BaseAddressEntry = namedtuple('BaseAddressEntry', 'base_address')
+
+
 class LocationLists(object):
     def __init__(self, stream, structs):
-        pass
+        self._max_addr = 2 ** (self.structs.address_size * 8) - 1
         
+    def get_location_list_at_offset(self, offset):
+        pass
+
+    def iter_location_lists(self):
+        pass
+
+    def _parse_location_list_from_stream(self):
+        lst = []
+        while True:
+            begin_offset = struct_parse(
+                self.structs.Dwarf_target_addr(''), self.stream)
+            end_offset = struct_parse(
+                self.structs.Dwarf_target_addr(''), self.stream)
+            if begin_offset == 0 and end_offset == 0:
+                # End of list - we're done.
+                break
+            elif begin_offset == self._max_addr:
+                # base
+            else: 
+                # entry...
+        
+
 
