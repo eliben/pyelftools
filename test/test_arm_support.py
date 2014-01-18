260f0e2389e6bab0334ebac85f41e383f44091dc
Eli Bendersky <eliben@gmail.com>
2014-01-18 06:34:37 -0800

    Adding unit test for an ARM file with DW_FORM_indirect

--- test/test_arm_support.py
+++ test/test_arm_support.py
@@ -25,6 +25,20 @@ class TestARMSupport(unittest.TestCase):
             self.assertEqual(elf.num_sections(), 14)
             self.assertEqual(elf.num_segments(), 2)
 
+    def test_DWARF_indirect_forms(self):
+        # This file uses a lot of DW_FORM_indirect, and is also an ARM ELF
+        # with non-trivial DWARF info.
+        # So this is a simple sanity check that we can successfully parse it
+        # and extract the expected amount of CUs.
+        with open(os.path.join('test', 'testfiles_for_unittests',
+                               'arm_with_form_indirect.elf'), 'rb') as f:
+            elffile = ELFFile(f)
+            self.assertTrue(elffile.has_dwarf_info())
+
+            dwarfinfo = elffile.get_dwarf_info()
+            all_CUs = list(dwarfinfo.iter_CUs())
+            self.assertEqual(len(all_CUs), 9)
+
 if __name__ == '__main__':
     unittest.main()
 
