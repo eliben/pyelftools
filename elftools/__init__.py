0a08d62e649e49457a77f53d874256e144fd11b3
Eli Bendersky <eliben@gmail.com>
2014-03-30 06:42:37 -0700

    Bump version to 0.22 and update TODO a bit

--- elftools/__init__.py
+++ elftools/__init__.py
@@ -4,5 +4,4 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-__version__ = '0.21'
-
+__version__ = '0.22'
