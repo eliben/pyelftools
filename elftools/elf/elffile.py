5abc7bfe5f78980f6ce5c3c4c32b5b56147e0045
Zach Riggle <zach@riggle.me>
2014-07-30 12:46:57 -0700

    Convenientce: Enable opening an ELF file by pathname rather than file()

--- elftools/elf/elffile.py
+++ elftools/elf/elffile.py
@@ -46,6 +46,9 @@ class ELFFile(object):
                 the raw e_ident field of the header
     """
     def __init__(self, stream):
+        if isinstance(stream, "".__class__):
+            stream = open(stream, 'rb')
+
         self.stream = stream
         self._identify_file()
         self.structs = ELFStructs(
