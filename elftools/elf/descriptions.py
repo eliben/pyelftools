a96f37174d5edcd619e7fa9f9b0bab4aa6b2c137
Pierre-Marie de Rodat <pmderodat@kawie.fr>
2017-08-17 04:27:11 +0200

    elftools.elf.description: re-indent describe_note according to PEP8

--- elftools/elf/descriptions.py
+++ elftools/elf/descriptions.py
@@ -124,20 +124,21 @@ def describe_ver_flags(x):
         VER_FLAGS.VER_FLG_INFO) if x & flag)
 
 def describe_note(x):
-  n_desc = x['n_desc']
-  desc = ''
-  if x['n_type'] == 'NT_GNU_ABI_TAG':
-      desc = '\n    OS: %s, ABI: %d.%d.%d' % (
-          _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
-          n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
-  elif x['n_type'] == 'NT_GNU_BUILD_ID':
-      desc = '\n    Build ID: %s' % (n_desc)
+    n_desc = x['n_desc']
+    desc = ''
+    if x['n_type'] == 'NT_GNU_ABI_TAG':
+        desc = '\n    OS: %s, ABI: %d.%d.%d' % (
+            _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
+            n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
+    elif x['n_type'] == 'NT_GNU_BUILD_ID':
+        desc = '\n    Build ID: %s' % (n_desc)
 
-  note_type = (x['n_type'] if isinstance(x['n_type'], str)
-               else 'Unknown note type:')
-  note_type_desc = ('0x%.8x' % x['n_type'] if isinstance(x['n_type'], int) else
-                    _DESCR_NOTE_N_TYPE.get(x['n_type'], _unknown))
-  return '%s (%s)%s' % (note_type, note_type_desc, desc)
+    note_type = (x['n_type'] if isinstance(x['n_type'], str)
+                 else 'Unknown note type:')
+    note_type_desc = ('0x%.8x' % x['n_type']
+                      if isinstance(x['n_type'], int) else
+                      _DESCR_NOTE_N_TYPE.get(x['n_type'], _unknown))
+    return '%s (%s)%s' % (note_type, note_type_desc, desc)
 
 #-------------------------------------------------------------------------------
 _unknown = '<unknown>'
