aa3341ab4047b6da0c3c3787f0abf9d4d7fbe177
Adam Greene <github@mzpqnxow.com>
2017-11-25 15:01:01 -0500

    Fix order of fix, was "cpic, pic" needs to be "pic, cpic", reordered the if statement to address this

--- scripts/readelf.py
+++ scripts/readelf.py
@@ -139,10 +139,10 @@ class ReadElf(object):
         elif self.elffile['e_machine'] == "EM_MIPS":
             if flags & E_FLAGS.EF_MIPS_NOREORDER:
                 description += ", noreorder"
-            if flags & E_FLAGS.EF_MIPS_CPIC:
-                description += ", cpic"
             if flags & E_FLAGS.EF_MIPS_PIC:
                 description += ", pic"
+            if flags & E_FLAGS.EF_MIPS_CPIC:
+                description += ", cpic"
             if not (flags & E_FLAGS.EF_MIPS_ABI2) and not (flags & E_FLAGS.EF_MIPS_ABI_ON32):
                 description += ", o32"
             if (flags & E_FLAGS.EF_MIPS_ARCH) == E_FLAGS.EF_MIPS_ARCH_1:
