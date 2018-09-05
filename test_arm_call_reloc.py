117bb6878379e4e97582748b3de911461d0e8017
Eli Bendersky <eliben@gmail.com>
2018-09-05 05:29:44 -0700

    Redo the new arm reloc test as a proper unit test, aligned with other tests

---
+++ test_arm_call_reloc.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# elftools tests
+#
+# Test 'R_ARM_CALL' relocation type support.
+# Compare the '.text' section data of ELF file that was relocated by elftools
+# with an ELF file that was relocated by linker.
+#
+# Dmitry Koltunov (koltunov@ispras.ru)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+import os
+import sys
+
+from elftools.common.py3compat import BytesIO
+from elftools.elf.elffile import ELFFile
+from elftools.elf.relocation import RelocationHandler
+
+
+def do_relocation(rel_elf):
+    data = rel_elf.get_section_by_name('.text').data()
+    rh = RelocationHandler(rel_elf)
+
+    stream = BytesIO()
+    stream.write(data)
+
+    rel = rel_elf.get_section_by_name('.rel.text')
+    rh.apply_section_relocations(stream, rel)
+
+    stream.seek(0)
+    data = stream.readlines()
+
+    return data
+
+
+class TestARMRElocation(unittest.TestCase):
+    def test_reloc(self):
+        test_dir = os.path.joinjoin('test', 'testfiles_for_unittests')
+        with open(join(test_dir, 'reloc_simple_arm_llvm.o'), 'rb') as rel_f, \
+                open(join(test_dir, 'simple_arm_llvm.elf'), 'rb') as f:
+            rel_elf = ELFFile(rel_f)
+            elf = ELFFile(f)
+
+            # Comparison of '.text' section data
+            self.assertEquals(do_relocation(rel_elf).pop(),
+                              elf.get_section_by_name('.text').data())
