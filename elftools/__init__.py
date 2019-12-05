8a40f295bc36f96efeb8633dc4a5314f6077d0bf
Eli Bendersky <eliben@gmail.com>
2019-12-05 05:34:21 -0800

    Prepare for release 0.26

--- elftools/__init__.py
+++ elftools/__init__.py
@@ -4,4 +4,4 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-__version__ = '0.25'
+__version__ = '0.26'
