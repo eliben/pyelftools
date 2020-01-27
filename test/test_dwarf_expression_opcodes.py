393b88d4af87531ccb33bd3f6dd057806f6dfb80
Seva Alekseyev <sevaa@nih.gov>
2020-01-27 12:50:46 -0500

    Unit test for expression parsing

--- test/test_dwarf_expression_opcodes.py
+++ test/test_dwarf_expression_opcodes.py
@@ -11,25 +11,29 @@ import os, sys
 
 from elftools.elf.elffile import ELFFile
 from elftools.dwarf.locationlists import LocationParser, LocationExpr
-from elftools.dwarf.dwarf_expr import DW_OP_opcode2name, GenericExprDumper
+from elftools.dwarf.dwarf_expr import DW_OP_opcode2name, GenericExprDumper, DW_OP_opcode2name
 
 class TestDwarfExpressionOpcodes(unittest.TestCase):
-    """ Go through file(s) in the test folder
-        parse all location expressions for every DIE with a location
-        make sure all the opcodes are known and supported
+    """ Parse all location expressions for every DIE with a location
+        make sure all the opcodes are known to the expression parser
     """
 
     def _test_parse_expression(self, expr, dumper):
         for op in dumper.dump(expr):
             self.assertTrue(op[0] in DW_OP_opcode2name, "Unknown opcode 0x%x" % (op[0]))
+            if  DW_OP_opcode2name[op[0]].startswith('DW_OP_GNU_') and not op[0] in self.ops:
+                self.ops.add(op[0])
  
     def _test_file(self, filename):
+        filepath = os.path.join('test', 'testfiles_for_unittests', filename)
         print('Reading %s...' % (filename))
-        with open(os.path.join('test', 'testfiles_for_unittests', filename), 'rb') as f:
+        with open(filepath, 'rb') as f:
             elffile = ELFFile(f)
             dwarfinfo = elffile.get_dwarf_info()
             location_lists = dwarfinfo.location_lists()
             loc_parser = LocationParser(location_lists)
+
+            self.ops = set()
 
             for CU in dwarfinfo.iter_CUs():
                 ver = CU['version'];
@@ -44,10 +48,12 @@ class TestDwarfExpressionOpcodes(unittest.TestCase):
                         else: 
                             for loc in ll:
                                 self._test_parse_expression(loc.loc_expr, dumper)
+                for op in self.ops:
+                    print(DW_OP_opcode2name[op])
 
     def test_main(self):
-        self._test_file('TBD')
-
+        self._test_file('dwarf_gnuops1.o')
+        self._test_file('dwarf_gnuops2.o')
 
 if __name__ == '__main__':
     unittest.main()
