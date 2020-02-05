54f6a18d635ac938962a0ac8e5359751f942f787
Seva Alekseyev <sevaa@nih.gov>
2020-02-05 15:24:01 -0500

    In DWARF2-3, a DW_AT_const_value is neither a location expr nor a location list offset

--- elftools/dwarf/locationlists.py
+++ elftools/dwarf/locationlists.py
@@ -104,13 +104,15 @@ class LocationParser(object):
 
     @staticmethod
     def _attribute_has_loc_expr(attr, dwarf_version):
-        return (dwarf_version < 4 and attr.form == 'DW_FORM_block1' or
-                attr.form == 'DW_FORM_exprloc')
+        return ((dwarf_version < 4 and attr.form == 'DW_FORM_block1' and
+            not attr.name == 'DW_AT_const_value') or
+            attr.form == 'DW_FORM_exprloc')
 
     @staticmethod
     def _attribute_has_loc_list(attr, dwarf_version):
         return ((dwarf_version < 4 and
-                 attr.form in ('DW_FORM_data4', 'DW_FORM_data8')) or
+                 attr.form in ('DW_FORM_data4', 'DW_FORM_data8') and
+                 not attr.name == 'DW_AT_const_value') or
                 attr.form == 'DW_FORM_sec_offset')
 
     @staticmethod
