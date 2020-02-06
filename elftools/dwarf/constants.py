77844bf612e5ceec080b710f4f7ab7fa046605ab
William Woodruff <william@trailofbits.com>
2020-02-06 15:33:21 -0500

    dwarf/constants: Add DW_LNE_{lo,hi}_user

--- elftools/dwarf/constants.py
+++ elftools/dwarf/constants.py
@@ -157,6 +157,8 @@ DW_LNE_end_sequence = 0x01
 DW_LNE_set_address = 0x02
 DW_LNE_define_file = 0x03
 DW_LNE_set_discriminator = 0x04
+DW_LNE_lo_user = 0x80
+DW_LNE_hi_user = 0xff
 
 
 # Call frame instructions
