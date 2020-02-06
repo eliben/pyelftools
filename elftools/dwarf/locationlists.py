a917b80edae3e40d9f3210394784f6890b844a83
Seva Alekseyev <sevaa@nih.gov>
2020-02-05 21:34:00 -0500

    Some more attribute classes where location expression definitely can be

--- elftools/dwarf/locationlists.py
+++ elftools/dwarf/locationlists.py
@@ -122,4 +122,7 @@ class LocationParser(object):
                                'DW_AT_data_member_location',
                                'DW_AT_frame_base', 'DW_AT_segment',
                                'DW_AT_static_link', 'DW_AT_use_location',
-                               'DW_AT_vtable_elem_location'))
+                               'DW_AT_vtable_elem_location',
+                               'DW_AT_GNU_call_site_value',
+                               'DW_AT_GNU_call_site_target',
+                               'DW_AT_GNU_call_site_data_value'))
