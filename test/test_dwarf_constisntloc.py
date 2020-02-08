8757f6775a3c0998e75dca1fa88bc538be31741a
Seva Alekseyev <sevaa@nih.gov>
2020-02-07 19:54:10 -0500

    Unit test for const_value with data4 not being a location offset

--- test/test_dwarf_constisntloc.py
+++ test/test_dwarf_constisntloc.py
@@ -8,32 +8,32 @@
 import unittest
 import os, sys, io
 
-# sys.path.insert(1, os.getcwd())
+sys.path.insert(1, os.getcwd())
 
 from elftools.elf.elffile import ELFFile
 from elftools.dwarf.dwarfinfo import DWARFInfo, DebugSectionDescriptor, DwarfConfig
 from elftools.dwarf.locationlists import LocationParser
 
-class TestGNUCallAttributesHaveLocation(unittest.TestCase):
+class TestConstWithData4IsntLocation(unittest.TestCase):
     def _test_file(self, filename):
         filepath = os.path.join('test', 'testfiles_for_unittests', filename)
         print('Reading %s...' % (filename))
         with open(filepath, 'rb') as f:
             elffile = ELFFile(f)
             dwarfinfo = elffile.get_dwarf_info()
+            locparser = LocationParser(dwarfinfo.location_lists())
             for CU in dwarfinfo.iter_CUs():
-                ver = CU['version']
                 print("Compile unit %s..." % CU.get_top_DIE().attributes['DW_AT_name'].value.decode('utf-8'))
-
+                ver = CU['version']
                 for DIE in CU.iter_DIEs():
                     for key in DIE.attributes:
                         attr = DIE.attributes[key]
-                        if attr.form == 'DW_FORM_exprloc':
-                            self.assertTrue(LocationParser.attribute_has_location(attr, CU['version']), "Attribute %s not recognized as a location" % key)
-
+                        if LocationParser.attribute_has_location(attr, ver):
+                            # This will crash on unpatched library on DIE at 0x9f
+                            locparser.parse_from_attribute(attr, ver)
 
     def test_main(self):
-        self._test_file('dwarf_gnuops1.o')
+        self._test_file('pascalenum.o')
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
