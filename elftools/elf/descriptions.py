783ac4e934128e7e9207a453661d028aa829eabf
Seva Alekseyev <sevaa@nih.gov>
2020-03-20 12:00:02 -0400

    readelf -n fix: for Android SO's, native readelf displays NT_GNU_ABI_TAG differently

--- elftools/elf/descriptions.py
+++ elftools/elf/descriptions.py
@@ -177,9 +177,12 @@ def describe_note(x):
     n_desc = x['n_desc']
     desc = ''
     if x['n_type'] == 'NT_GNU_ABI_TAG':
-        desc = '\n    OS: %s, ABI: %d.%d.%d' % (
-            _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
-            n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
+        if x['n_name'] == 'Android': # Lame patch :(
+            desc = '\n   description data: %s ' % ' '.join("%02x" % ord(b) for b in x['n_descdata'])
+        else:
+            desc = '\n    OS: %s, ABI: %d.%d.%d' % (
+                _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
+                n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
     elif x['n_type'] == 'NT_GNU_BUILD_ID':
         desc = '\n    Build ID: %s' % (n_desc)
     elif x['n_type'] == 'NT_GNU_GOLD_VERSION':
@@ -189,11 +192,15 @@ def describe_note(x):
             '{:02x}'.format(ord(byte)) for byte in n_desc
         ))
 
-    note_type = (x['n_type'] if isinstance(x['n_type'], str)
-                 else 'Unknown note type:')
-    note_type_desc = ('0x%.8x' % x['n_type']
-                      if isinstance(x['n_type'], int) else
-                      _DESCR_NOTE_N_TYPE.get(x['n_type'], _unknown))
+    if x['n_type'] == 'NT_GNU_ABI_TAG' and x['n_name'] == 'Android':
+        note_type = 'NT_VERSION'
+        note_type_desc = 'version'
+    else:
+        note_type = (x['n_type'] if isinstance(x['n_type'], str)
+                    else 'Unknown note type:')
+        note_type_desc = ('0x%.8x' % x['n_type']
+                        if isinstance(x['n_type'], int) else
+                        _DESCR_NOTE_N_TYPE.get(x['n_type'], _unknown))
     return '%s (%s)%s' % (note_type, note_type_desc, desc)
 
 
