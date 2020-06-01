1ed78f5fef91ecd792c42e6f805114ab5a38ba4f
William Woodruff <william@trailofbits.com>
2020-06-01 09:00:00 -0400

    dwarf/dwarf_expr: Add support for DW_OP_GNU_push_tls_address (#315)
    * dwarf/dwarf_expr: Add support for DW_OP_GNU_push_tls_address
    
    * dwarf/dwarf_expr: Use a single 64-bit operand for const8x
    
    DWARFv4 2.5.1.1: this should be consumed as a single 64-bit operand,
    not as two 32-bit operands.
    
    * dwarf/descriptions: Fix descriptions for const8{u,s}
    
    * test: Add tests for changed OPs

--- elftools/dwarf/descriptions.py
+++ elftools/dwarf/descriptions.py
@@ -559,16 +559,15 @@ class ExprDumper(object):
     def _init_lookups(self):
         self._ops_with_decimal_arg = set([
             'DW_OP_const1u', 'DW_OP_const1s', 'DW_OP_const2u', 'DW_OP_const2s',
-            'DW_OP_const4u', 'DW_OP_const4s', 'DW_OP_constu', 'DW_OP_consts',
-            'DW_OP_pick', 'DW_OP_plus_uconst', 'DW_OP_bra', 'DW_OP_skip',
-            'DW_OP_fbreg', 'DW_OP_piece', 'DW_OP_deref_size',
-            'DW_OP_xderef_size', 'DW_OP_regx',])
+            'DW_OP_const4u', 'DW_OP_const4s', 'DW_OP_const8u', 'DW_OP_const8s',
+            'DW_OP_constu', 'DW_OP_consts', 'DW_OP_pick', 'DW_OP_plus_uconst',
+            'DW_OP_bra', 'DW_OP_skip', 'DW_OP_fbreg', 'DW_OP_piece',
+            'DW_OP_deref_size', 'DW_OP_xderef_size', 'DW_OP_regx',])
 
         for n in range(0, 32):
             self._ops_with_decimal_arg.add('DW_OP_breg%s' % n)
 
-        self._ops_with_two_decimal_args = set([
-            'DW_OP_const8u', 'DW_OP_const8s', 'DW_OP_bregx', 'DW_OP_bit_piece'])
+        self._ops_with_two_decimal_args = set(['DW_OP_bregx', 'DW_OP_bit_piece'])
 
         self._ops_with_hex_arg = set(
             ['DW_OP_addr', 'DW_OP_call2', 'DW_OP_call4', 'DW_OP_call_ref'])
