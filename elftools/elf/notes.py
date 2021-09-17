cc47f5fb6dbe16aee110faa6270d9ef2fa7a290f
Marco Bonelli <mb5.marcob@gmail.com>
2021-09-17 22:37:03 +0200

    Keep raw note descriptors in ELF note sections as raw bytes (#372)
    * ELF notes: keep raw note descriptors as bytes
    
    * py3compat: add bytes2hex function
    
    * elf/descriptions: use bytes2hex where needed
    
    * ELF notes: convert to string only for known types

--- elftools/elf/notes.py
+++ elftools/elf/notes.py
@@ -6,7 +6,7 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-from ..common.py3compat import bytes2str
+from ..common.py3compat import bytes2hex, bytes2str
 from ..common.utils import struct_parse, roundup
 from ..construct import CString
 
@@ -29,14 +29,16 @@ def iter_notes(elffile, offset, size):
             CString('').parse(elffile.stream.read(disk_namesz)))
         offset += disk_namesz
 
-        desc_data = bytes2str(elffile.stream.read(note['n_descsz']))
+        desc_data = elffile.stream.read(note['n_descsz'])
         note['n_descdata'] = desc_data
         if note['n_type'] == 'NT_GNU_ABI_TAG':
             note['n_desc'] = struct_parse(elffile.structs.Elf_abi,
                                           elffile.stream,
                                           offset)
         elif note['n_type'] == 'NT_GNU_BUILD_ID':
-            note['n_desc'] = ''.join('%.2x' % ord(b) for b in desc_data)
+            note['n_desc'] = bytes2hex(desc_data)
+        elif note['n_type'] == 'NT_GNU_GOLD_VERSION':
+            note['n_desc'] = bytes2str(desc_data)
         elif note['n_type'] == 'NT_PRPSINFO':
             note['n_desc'] = struct_parse(elffile.structs.Elf_Prpsinfo,
                                           elffile.stream,
