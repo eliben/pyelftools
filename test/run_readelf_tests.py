494f365e6aca5643ce896a449600b0a727513e09
Seva Alekseyev <sevaa@yarxi.ru>
2022-06-08 16:07:11 -0400

    GNU binutils bug worked around in a slightly less disturbing way - patched the binary, left a comment in the test script.

--- test/run_readelf_tests.py
+++ test/run_readelf_tests.py
@@ -80,14 +80,11 @@ def run_test_on_file(filename, verbose=False, opt=None):
                 testlog.info('.......................SKIPPED')
             continue
 
-        # TODO(sevaa): I've checked the readelf sources, the DWARF docs,
-        # and concluded the the readelf.py's behavior is correct while
-        # GNU binutils' is wrong.
-        if filename.endswith("dwarf_lineprogramv5.elf") and option == "--debug-dump=frames-interp":
-            if verbose:
-                testlog.warning("....will fail because GNU readelf has a bug here, see eliben/pyelftools PR #411")
-                testlog.info('.......................SKIPPED')
-            continue
+        # sevaa says: there is another shorted out test; in dwarf_lineprogramv5.elf, the two bytes at 0x2072 were
+        # patched from 0x07 0x10 to 00 00.
+        # Those represented the second instruction in the first FDE in .eh_frame. This changed the instruction
+        # from "DW_CFA_undefined 16" to two NOPs.
+        # GNU readelf had a bug here, had to work around. See PR #411.
 
         # stdouts will be a 2-element list: output of readelf and output
         # of scripts/readelf.py
