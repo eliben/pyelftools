b4340e3bd1eb5417f4b0c46550eb094343b2166b
Seva Alekseyev <sevaa@yarxi.ru>
2022-06-10 09:46:03 -0400

    Windows compatibility, take 2

--- examples/dwarf_die_tree.py
+++ examples/dwarf_die_tree.py
@@ -9,22 +9,13 @@
 #-------------------------------------------------------------------------------
 from __future__ import print_function
 import sys
-import posixpath
+from pathlib import Path
 
 # If pyelftools is not installed, the example can also run from the root or
 # examples/ dir of the source distribution.
 sys.path[0:0] = ['.', '..']
 
 from elftools.elf.elffile import ELFFile
-from elftools.common.py3compat import bytes2str
-
-# Replacement for DIE.get_full_path, with POSIX paths instead of host paths
-def get_full_path_posix(die):
-    comp_dir_attr = die.attributes.get('DW_AT_comp_dir', None)
-    comp_dir = bytes2str(comp_dir_attr.value) if comp_dir_attr else ''
-    fname_attr = die.attributes.get('DW_AT_name', None)
-    fname = bytes2str(fname_attr.value) if fname_attr else ''
-    return posixpath.join(comp_dir, fname)    
 
 
 def process_file(filename):
@@ -54,7 +45,7 @@ def process_file(filename):
             print('    Top DIE with tag=%s' % top_DIE.tag)
 
             # We're interested in the filename...
-            print('    name=%s' % get_full_path_posix(top_DIE))
+            print('    name=%s' % Path(top_DIE.get_full_path()).as_posix())
 
             # Display DIEs recursively starting with top_DIE
             die_info_rec(top_DIE)
