bab6db280ebdb2c24c4643b09b9f35a6d915150d
Seva Alekseyev <sevaa@nih.gov>
2023-08-16 15:14:45 -0400

    MIPS too

--- elftools/dwarf/descriptions.py
+++ elftools/dwarf/descriptions.py
@@ -594,6 +594,16 @@ _REG_NAMES_ARM = [
      'tpidruro', 'tpidrurw', 'tpidpr', 'htpidpr'
 ]
 
+# From mips-tdep.h in the GDB sources
+_REG_NAMES_MIPS = [
+    '$zero', '$at', '$v0', '$v1', '$a0', '$a1', '$a2', '$a3',
+    '$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7',
+    '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7',
+    '$t8', '$t9', '$k0', '$k1', '$gp', '$sp', '$fp', '$ra',
+    '$ps', '$lo', '$hi', '$badvaddr', '$cause', '$pc'] + [
+        '$fp%d' % n for n in range(35)] + ['<none>'] + [
+        '$cp%d' % n for n in range(15)] + ['$prid']
+
 
 class ExprDumper(object):
     """ A dumper for DWARF expressions that dumps a textual
