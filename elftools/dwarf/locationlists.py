81ef8b365db09ac74d095e49fe7dccc32901386c
Seva Alekseyev <sevaa@yarxi.ru>
2023-08-23 10:01:46 -0400

    Friendly error in get_location_list_at_offset

--- elftools/dwarf/locationlists.py
+++ elftools/dwarf/locationlists.py
@@ -92,6 +92,8 @@ class LocationLists(object):
         Passing the die is only neccessary in DWARF5+, for decoding
         location entry encodings that contain references to other sections.
         """
+        if self.version >= 5 and die is None:
+            raise DWARFError("For this binary, \"die\" needs to be provided")              
         self.stream.seek(offset, os.SEEK_SET)
         return self._parse_location_list_from_stream_v5(die.cu) if self.version >= 5 else self._parse_location_list_from_stream()
 
