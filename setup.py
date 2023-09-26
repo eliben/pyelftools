422df2439438e06d19a5dab31dc7d03a94d7831e
Farid Zakaria <fmzakari@google.com>
2023-09-26 15:55:32 +0000

    Changed to single quotes

--- setup.py
+++ setup.py
@@ -1,48 +1,49 @@
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 # pyelftools: setup.py
 #
 # Setup/installation script.
 #
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
-# -------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 import os, sys
 from setuptools import setup
 
 
 try:
-    with open("README", "rt") as readme:
-        description = "\n" + readme.read()
+    with open('README', 'rt') as readme:
+        description = '\n' + readme.read()
 except IOError:
     # maybe running setup.py from some other dir
-    description = ""
+    description = ''
 
 
 setup(
     # metadata
-    name="pyelftools",
-    description="Library for analyzing ELF files and DWARF debugging information",
+    name='pyelftools',
+    description='Library for analyzing ELF files and DWARF debugging information',
     long_description=description,
-    license="Public domain",
-    version="0.30",
-    author="Eli Bendersky",
-    maintainer="Eli Bendersky",
-    author_email="eliben@gmail.com",
-    url="https://github.com/eliben/pyelftools",
-    platforms="Cross Platform",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
+    license='Public domain',
+    version='0.30',
+    author='Eli Bendersky',
+    maintainer='Eli Bendersky',
+    author_email='eliben@gmail.com',
+    url='https://github.com/eliben/pyelftools',
+    platforms='Cross Platform',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        ],
+
     # All packages and sub-packages must be listed here
     packages=[
-        "elftools",
-        "elftools.elf",
-        "elftools.common",
-        "elftools.dwarf",
-        "elftools.ehabi",
-        "elftools.construct",
-        "elftools.construct.lib",
-    ],
-    scripts=["scripts/readelf.py"],
-    package_data={"elftools": ["py.typed"]},
+        'elftools',
+        'elftools.elf',
+        'elftools.common',
+        'elftools.dwarf',
+        'elftools.ehabi',
+        'elftools.construct', 'elftools.construct.lib',
+        ],
+
+    scripts=['scripts/readelf.py'],
+    package_data={'elftools': ['py.typed']}
 )
