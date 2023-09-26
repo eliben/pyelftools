e94f803039b9f9b8ce2164642fc07c47cca8ca20
Seva Alekseyev <sevaa@yarxi.ru>
2023-09-26 10:27:29 -0400

    Latest readelf, ranges autotest exception removed

--- elftools/dwarf/ranges.py
+++ elftools/dwarf/ranges.py
@@ -77,6 +77,13 @@ class RangeListsPair(object):
         section dumper should check if one is present.
         """
         return self._rnglists.iter_CU_range_lists_ex(cu)
+    
+    def translate_v5_entry(self, entry, cu):
+        """See RangeLists.translate_v5_entry()
+        
+        Forwards a DWARFv5 rangelist entry translation request to the V5 section
+        """
+        return self._rnglists.translate_v5_entry(entry, cu)    
 
 class RangeLists(object):
     """ A single range list is a Python list consisting of RangeEntry or
