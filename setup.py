d9ea5f1d1f1b4dc69b3685cbcf2376a0f1a5a3e6
Farid Zakaria <farid.m.zakaria@gmail.com>
2023-10-20 17:37:11 +0000

    Add py.typed to elftools (#507)
    * Add py.typed to elftools
    
    fixes #506
    
    * Changed to single quotes

--- setup.py
+++ setup.py
@@ -44,5 +44,6 @@ setup(
         'elftools.construct', 'elftools.construct.lib',
         ],
 
-    scripts=['scripts/readelf.py']
+    scripts=['scripts/readelf.py'],
+    package_data={'elftools': ['py.typed']}
 )
