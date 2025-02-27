bc967b2b6a8330b715416394335556fa1f18769f
Philipp Hahn <phahn-oss@avm.de>
2025-02-27 18:22:01 +0100

    Refactor describe_attr_tag_arm
    Refactor code and lookup `_DESCR_ATTR_TAG_ARM` only once for all cases.
    Also add a fallback in case the tag is not known.
    
    Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

--- elftools/elf/descriptions.py
+++ elftools/elf/descriptions.py
@@ -237,31 +237,30 @@ def describe_note(x, machine):
 
 
 def describe_attr_tag_arm(tag, val, extra):
+    s = _DESCR_ATTR_TAG_ARM.get(tag, '"%s"' % tag)
     idx = ENUM_ATTR_TAG_ARM[tag] - 1
     d_entry = _DESCR_ATTR_VAL_ARM[idx]
 
     if d_entry is None:
         if tag == 'TAG_COMPATIBILITY':
-            return (_DESCR_ATTR_TAG_ARM[tag]
-                    + 'flag = %d, vendor = %s' % (val, extra))
+            return s + 'flag = %d, vendor = %s' % (val, extra)
 
         elif tag == 'TAG_ALSO_COMPATIBLE_WITH':
             if val.tag == 'TAG_CPU_ARCH':
                 d_entry = _DESCR_ATTR_VAL_ARM[5]  # TAG_CPU_ARCH
-                return _DESCR_ATTR_TAG_ARM[tag] + d_entry.get(val.value, '??? (%d)' % val.value)
+                return s + d_entry.get(val.value, '??? (%d)' % val.value)
 
             else:
-                return _DESCR_ATTR_TAG_ARM[tag] + '??? (%d)' % val.tag
+                return s + '??? (%d)' % val.tag
 
         elif tag == 'TAG_NODEFAULTS':
-            return _DESCR_ATTR_TAG_ARM[tag] + 'True'
+            return s + 'True'
 
-        s = _DESCR_ATTR_TAG_ARM[tag]
         s += '"%s"' % val if val else ''
         return s
 
     else:
-        return _DESCR_ATTR_TAG_ARM[tag] + d_entry[val]
+        return s + d_entry[val]
 
 def describe_attr_tag_riscv(tag, val, extra):
     idx = ENUM_ATTR_TAG_RISCV[tag] - 1
