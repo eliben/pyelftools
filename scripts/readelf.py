eliben/pyelftools

commit c50de91f3d8d3ff2d3824bfd3c0ab39df72df94b

Issue #119: readelf - don't do hex/string dumps for SHT_NOBITS sections

scripts/readelf.py
@@ -304,7 +304,6 @@ class ReadElf(object):
                 self._emitline('   Num:    Value          Size Type    Bind   Vis      Ndx Name')
 
             for nsym, symbol in enumerate(section.iter_symbols()):
-
                 version_info = ''
                 # readelf doesn't display version info for Solaris versioning
                 if (section['sh_type'] == 'SHT_DYNSYM' and
@@ -572,6 +571,10 @@ class ReadElf(object):
             self._emitline("Section '%s' does not exist in the file!" % (
                 section_spec))
             return
+        if section['sh_type'] == 'SHT_NOBITS':
+            self._emitline("\nSection '%s' has no data to dump." % (
+                section_spec))
+            return
 
         self._emitline("\nHex dump of section '%s':" % section.name)
         self._note_relocs_for_section(section)
@@ -613,6 +616,10 @@ class ReadElf(object):
         section = self._section_from_spec(section_spec)
         if section is None:
             self._emitline("Section '%s' does not exist in the file!" % (
+                section_spec))
+            return
+        if section['sh_type'] == 'SHT_NOBITS':
+            self._emitline("\nSection '%s' has no data to dump." % (
                 section_spec))
             return
 

commit 5c3571108abccfacea38b16de8b35d376c06fe5f

Fix several -x.text difference
```console
$ elf='test/testfiles_for_readelf/arm-eabi-attr-names.o.elf'
$ diff <(LC_ALL=C readelf -x.text "$elf") <(scripts/readelf.py -x.text "$elf")
1c1,3
< Section '.text' has no data to dump.
---
>
> Hex dump of section '.text':
>
```

Fixes: c50de91f3d8d ("Issue #119: readelf - don't do hex/string dumps for SHT_NOBITS sections")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

scripts/readelf.py
@@ -815,15 +815,14 @@ class ReadElf(object):
             sys.stderr.write('readelf: Warning: Section \'%s\' was not dumped because it does not exist!\n' % (
                 section_spec))
             return
-        if section['sh_type'] == 'SHT_NOBITS':
-            self._emitline("\nSection '%s' has no data to dump." % (
+        if section['sh_type'] == 'SHT_NOBITS' or not (data := section.data()):
+            self._emitline("Section '%s' has no data to dump." % (
                 section_spec))
             return
 
         self._emitline("\nHex dump of section '%s':" % section.name)
         self._note_relocs_for_section(section)
         addr = section['sh_addr']
-        data = section.data()
         dataptr = 0
 
         while dataptr < len(data):