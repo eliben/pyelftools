eliben/pyelftools

commit e972a570d18f32210fa9d4bcfc615e244ad54555

Supplementary object files (#426)
* Recognize DW_FORM_ref_udata as a reference type.

References to other DIEs can also be implemented with a form
DW_FORM_ref_udata, for using the ULEB128 encoding

* Add support for DWARF supplementary object files.

elftools/dwarf/dwarfinfo.py
@@ -79,7 +79,10 @@ class DWARFInfo(object):
             debug_str_offsets_sec,
             debug_line_str_sec,
             debug_loclists_sec,
-            debug_rnglists_sec): # Not parsed for now
+            debug_rnglists_sec,
+            debug_sup_sec,
+            gnu_debugaltlink_sec
+            ):
         """ config:
                 A DwarfConfig object
 
@@ -105,6 +108,13 @@ class DWARFInfo(object):
         self.debug_pubnames_sec = debug_pubnames_sec
         self.debug_loclists_sec = debug_loclists_sec
         self.debug_rnglists_sec = debug_rnglists_sec
+        self.debug_sup_sec = debug_sup_sec
+        self.gnu_debugaltlink_sec = gnu_debugaltlink_sec
+
+        # Sets the supplementary_dwarfinfo to None. Client code can set this
+        # to something else, typically a DWARFInfo file read from an ELFFile
+        # which path is stored in the debug_sup_sec or gnu_debugaltlink_sec.
+        self.supplementary_dwarfinfo = None
 
         # This is the DWARFStructs the context uses, so it doesn't depend on
         # DWARF format and address_size (these are determined per CU) - set them
@@ -507,6 +517,11 @@ class DWARFInfo(object):
                         replace_value(data, field.content_type, self.get_string_from_linetable)
                     elif field.form == 'DW_FORM_strp':
                         replace_value(data, field.content_type, self.get_string_from_table)
+                    elif field.form in ('DW_FORM_strp_sup', 'DW_FORM_GNU_strp_alt'):
+                        if self.supplementary_dwarfinfo:
+                            replace_value(data, field.content_type, self.supplementary_dwarfinfo.get_string_fromtable)
+                        else:
+                            replace_value(data, field.content_type, lambda x: str(x))
                     elif field.form in ('DW_FORM_strp_sup', 'DW_FORM_strx', 'DW_FORM_strx1', 'DW_FORM_strx2', 'DW_FORM_strx3', 'DW_FORM_strx4'):
                         raise NotImplementedError()
 
@@ -535,4 +550,19 @@ class DWARFInfo(object):
             structs=structs,
             program_start_offset=self.debug_line_sec.stream.tell(),
             program_end_offset=end_offset)
+
+    def parse_debugsupinfo(self):
+        """
+        Extract a filename from either .debug_sup or .gnu_debualtlink sections.
+        """
+        if self.debug_sup_sec is not None:
+            self.debug_sup_sec.stream.seek(0)
+            suplink = self.structs.Dwarf_debugsup.parse_stream(self.debug_sup_sec.stream)
+            if suplink.is_supplementary == 0:
+                return suplink.sup_filename
+        if self.gnu_debugaltlink_sec is not None:
+            self.gnu_debugaltlink_sec.stream.seek(0)
+            suplink = self.structs.Dwarf_debugaltlink.parse_stream(self.gnu_debugaltlink_sec.stream)
+            return suplink.sup_filename
+        return None
 

elftools/common/py3compat.py
@@ -13,6 +13,7 @@ PY3 = sys.version_info[0] == 3
 if PY3:
     import io
     from pathlib import Path
+
     StringIO = io.StringIO
     BytesIO = io.BytesIO
 

elftools/dwarf/compileunit.py
@@ -165,7 +165,7 @@ class CompileUnit(object):
                 sibling = child.attributes["DW_AT_sibling"]
                 if sibling.form in ('DW_FORM_ref1', 'DW_FORM_ref2',
                                     'DW_FORM_ref4', 'DW_FORM_ref8',
-                                    'DW_FORM_ref'):
+                                    'DW_FORM_ref', 'DW_FORM_ref_udata'):
                     cur_offset = sibling.value + self.cu_offset
                 elif sibling.form == 'DW_FORM_ref_addr':
                     cur_offset = sibling.value
@@ -198,10 +198,14 @@ class CompileUnit(object):
         """ Given a DIE, this yields it with its subtree including null DIEs
             (child list terminators).
         """
+        # If the die is an imported unit, replace it with what it refers to if
+        # we can
+        if die.tag == 'DW_TAG_imported_unit' and self.dwarfinfo.supplementary_dwarfinfo:
+            die = die.get_DIE_from_attribute('DW_AT_import')
         yield die
         if die.has_children:
             for c in die.iter_children():
-                for d in self._iter_DIE_subtree(c):
+                for d in die.cu._iter_DIE_subtree(c):
                     yield d
             yield die._terminator
 

elftools/dwarf/die.py
@@ -108,7 +108,7 @@ class DIE(object):
         """
         attr = self.attributes[name]
         if attr.form in ('DW_FORM_ref1', 'DW_FORM_ref2', 'DW_FORM_ref4',
-                         'DW_FORM_ref8', 'DW_FORM_ref'):
+                         'DW_FORM_ref8', 'DW_FORM_ref', 'DW_FORM_ref_udata'):
             refaddr = self.cu.cu_offset + attr.raw_value
             return self.cu.get_DIE_from_refaddr(refaddr)
         elif attr.form in ('DW_FORM_ref_addr'):
@@ -116,7 +116,10 @@ class DIE(object):
         elif attr.form in ('DW_FORM_ref_sig8'):
             # Implement search type units for matching signature
             raise NotImplementedError('%s (type unit by signature)' % attr.form)
-        elif attr.form in ('DW_FORM_ref_sup4', 'DW_FORM_ref_sup8'):
+        elif attr.form in ('DW_FORM_ref_sup4', 'DW_FORM_ref_sup8', 'DW_FORM_GNU_ref_alt'):
+            if self.dwarfinfo.supplementary_dwarfinfo:
+                return self.dwarfinfo.supplementary_dwarfinfo.get_DIE_from_refaddr(attr.raw_value)
+            # FIXME: how to distinguish supplementary files from dwo ?
             raise NotImplementedError('%s to dwo' % attr.form)
         else:
             raise DWARFError('%s is not a reference class form attribute' % attr)
@@ -275,6 +278,11 @@ class DIE(object):
         elif form == 'DW_FORM_line_strp':
             with preserve_stream_pos(self.stream):
                 value = self.dwarfinfo.get_string_from_linetable(raw_value)
+        elif form in ('DW_FORM_GNU_strp_alt', 'DW_FORM_strp_sup'):
+            if self.dwarfinfo.supplementary_dwarfinfo:
+                return self.dwarfinfo.supplementary_dwarfinfo.get_string_from_table(raw_value)
+            else:
+                value = raw_value
         elif form == 'DW_FORM_flag':
             value = not raw_value == 0
         elif form == 'DW_FORM_flag_present':

elftools/dwarf/structs.py
@@ -13,7 +13,7 @@ from ..construct import (
     SBInt8, SBInt16, SBInt32, SBInt64, SLInt8, SLInt16, SLInt32, SLInt64,
     Adapter, Struct, ConstructError, If, Enum, Array, PrefixedArray,
     CString, Embed, StaticField, IfThenElse, Construct, Rename, Sequence,
-    Switch, Value
+    String, Switch, Value
     )
 from ..common.construct_utils import (RepeatUntilExcluding, ULEB128, SLEB128,
     StreamOffset)
@@ -146,6 +146,9 @@ class DWARFStructs(object):
         self._create_loclists_parsers()
         self._create_rnglists_parsers()
 
+        self._create_debugsup()
+        self._create_gnu_debugaltlink()
+
     def _create_initial_length(self):
         def _InitialLength(name):
             # Adapts a Struct that parses forward a full initial length field.
@@ -191,6 +194,18 @@ class DWARFStructs(object):
                     If(lambda ctx: ctx['form'] == 'DW_FORM_implicit_const',
                         self.Dwarf_sleb128('value')))))
 
+    def _create_debugsup(self):
+        # We don't care about checksums, for now.
+        self.Dwarf_debugsup = Struct('Elf_debugsup',
+            self.Dwarf_int16('version'),
+            self.Dwarf_uint8('is_supplementary'),
+            CString('sup_filename'))
+
+    def _create_gnu_debugaltlink(self):
+        self.Dwarf_debugaltlink = Struct('Elf_debugaltlink',
+            CString("sup_filename"),
+            String("sup_checksum", length=20))
+
     def _create_dw_form(self):
         self.Dwarf_dw_form = dict(
             DW_FORM_addr=self.Dwarf_target_addr(''),
@@ -215,6 +230,7 @@ class DWARFStructs(object):
 
             DW_FORM_string=CString(''),
             DW_FORM_strp=self.Dwarf_offset(''),
+            DW_FORM_strp_sup=self.Dwarf_offset(''),
             DW_FORM_line_strp=self.Dwarf_offset(''),
             DW_FORM_strx1=self.Dwarf_uint8(''),
             DW_FORM_strx2=self.Dwarf_uint16(''),
@@ -226,7 +242,9 @@ class DWARFStructs(object):
             DW_FORM_ref1=self.Dwarf_uint8(''),
             DW_FORM_ref2=self.Dwarf_uint16(''),
             DW_FORM_ref4=self.Dwarf_uint32(''),
+            DW_FORM_ref_sup4=self.Dwarf_uint32(''),
             DW_FORM_ref8=self.Dwarf_uint64(''),
+            DW_FORM_ref_sup8=self.Dwarf_uint64(''),
             DW_FORM_ref_udata=self.Dwarf_uleb128(''),
             DW_FORM_ref_addr=self.Dwarf_target_addr('') if self.dwarf_version == 2 else self.Dwarf_offset(''),
 

elftools/elf/elffile.py
@@ -7,6 +7,7 @@
 # This code is in the public domain
 #-------------------------------------------------------------------------------
 import io
+import os
 import struct
 import zlib
 
@@ -46,6 +47,12 @@ class ELFFile(object):
     """ Creation: the constructor accepts a stream (file-like object) with the
         contents of an ELF file.
 
+        Optionally, a stream_loader function can be passed as the second
+        argument. This stream_loader function takes a relative file path to
+        load a supplementary object file, and returns a stream suitable for
+        creating a new ELFFile. Currently, the only such relative file path is
+        obtained from the supplementary object files.
+
         Accessible attributes:
 
             stream:
@@ -69,7 +76,7 @@ class ELFFile(object):
             e_ident_raw:
                 the raw e_ident field of the header
     """
-    def __init__(self, stream):
+    def __init__(self, stream, stream_loader=None):
         self.stream = stream
         self._identify_file()
         self.structs = ELFStructs(
@@ -88,6 +95,23 @@ class ELFFile(object):
         self._section_header_stringtable = \
             self._get_section_header_stringtable()
         self._section_name_map = None
+        self.stream_loader = stream_loader
+
+    @classmethod
+    def load_from_path(cls, path):
+        """Takes a path to a file on the local filesystem, and returns an
+        ELFFile from it, setting up a correct stream_loader relative to the
+        original file.
+        """
+        base_directory = os.path.dirname(path)
+        def loader(elf_path):
+            # FIXME: use actual path instead of str/bytes
+            if not os.path.isabs(elf_path):
+                elf_path = os.path.join(base_directory,
+                                        elf_path)
+            return open(elf_path, 'rb')
+        stream = open(path, 'rb')
+        return ELFFile(stream, loader)
 
     def num_sections(self):
         """ Number of sections in the file
@@ -203,12 +227,15 @@ class ELFFile(object):
             self.get_section_by_name('.zdebug_info') or
             self.get_section_by_name('.eh_frame'))
 
-    def get_dwarf_info(self, relocate_dwarf_sections=True):
+    def get_dwarf_info(self, relocate_dwarf_sections=True, follow_links=True):
         """ Return a DWARFInfo object representing the debugging information in
             this file.
 
             If relocate_dwarf_sections is True, relocations for DWARF sections
             are looked up and applied.
+
+            If follow_links is True, we will try to load the supplementary
+            object file (if any), and use it to resolve references and imports.
         """
         # Expect that has_dwarf_info was called, so at least .debug_info is
         # present.
@@ -219,8 +246,8 @@ class ELFFile(object):
                          '.debug_loc', '.debug_ranges', '.debug_pubtypes',
                          '.debug_pubnames', '.debug_addr',
                          '.debug_str_offsets', '.debug_line_str',
-                         '.debug_loclists', '.debug_rnglists')
-
+                         '.debug_loclists', '.debug_rnglists',
+                         '.debug_sup', '.gnu_debugaltlink')
 
         compressed = bool(self.get_section_by_name('.zdebug_info'))
         if compressed:
@@ -234,7 +261,7 @@ class ELFFile(object):
          debug_loc_sec_name, debug_ranges_sec_name, debug_pubtypes_name,
          debug_pubnames_name, debug_addr_name, debug_str_offsets_name,
          debug_line_str_name, debug_loclists_sec_name, debug_rnglists_sec_name,
-         eh_frame_sec_name) = section_names
+         debug_sup_name, gnu_debugaltlink_name, eh_frame_sec_name) = section_names
 
         debug_sections = {}
         for secname in section_names:
@@ -249,7 +276,11 @@ class ELFFile(object):
                     dwarf_section = self._decompress_dwarf_section(dwarf_section)
                 debug_sections[secname] = dwarf_section
 
-        return DWARFInfo(
+        # Lookup if we have any of the .gnu_debugaltlink (GNU proprietary
+        # implementation) or .debug_sup sections, referencing a supplementary
+        # DWARF file
+
+        dwarfinfo = DWARFInfo(
                 config=DwarfConfig(
                     little_endian=self.little_endian,
                     default_address_size=self.elfclass // 8,
@@ -269,8 +300,29 @@ class ELFFile(object):
                 debug_str_offsets_sec=debug_sections[debug_str_offsets_name],
                 debug_line_str_sec=debug_sections[debug_line_str_name],
                 debug_loclists_sec=debug_sections[debug_loclists_sec_name],
-                debug_rnglists_sec=debug_sections[debug_rnglists_sec_name]
+                debug_rnglists_sec=debug_sections[debug_rnglists_sec_name],
+                debug_sup_sec=debug_sections[debug_sup_name],
+                gnu_debugaltlink_sec=debug_sections[gnu_debugaltlink_name]
                 )
+        if follow_links:
+            dwarfinfo.supplementary_dwarfinfo = self.get_supplementary_dwarfinfo(dwarfinfo)
+        return dwarfinfo
+
+
+    def get_supplementary_dwarfinfo(self, dwarfinfo):
+        """
+        Read supplementary dwarfinfo, from either the standared .debug_sup
+        section or the GNU proprietary .gnu_debugaltlink.
+        """
+        supfilepath = dwarfinfo.parse_debugsupinfo()
+        if supfilepath is not None and self.stream_loader is not None:
+            stream = self.stream_loader(supfilepath)
+            supelffile = ELFFile(stream)
+            dwarf_info = supelffile.get_dwarf_info()
+            stream.close()
+            return dwarf_info
+        return None
+
 
     def has_ehabi_info(self):
         """ Check whether this file appears to have arm exception handler index table.
@@ -765,3 +817,12 @@ class ELFFile(object):
                 )
 
         return section._replace(stream=uncompressed_stream, size=size)
+
+    def close(self):
+        self.stream.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()

elftools/elf/structs.py
@@ -380,6 +380,11 @@ class ELFStructs(object):
             self.Elf_word('abi_tiny'),
         )
 
+    def _create_gnu_debugaltlink(self):
+        self.Elf_debugaltlink = Struct('Elf_debugaltlink',
+            CString("sup_filename"),
+            String("sup_checksum", length=20))
+
     def _create_gnu_property(self):
         # Structure of GNU property notes is documented in
         # https://github.com/hjl-tools/linux-abi/wiki/linux-abi-draft.pdf

test/test_refaddr_bitness.py
@@ -48,7 +48,9 @@ class TestRefAddrOnDWARFv2With64BitTarget(unittest.TestCase):
             debug_str_offsets_sec=None,
             debug_line_str_sec=None,
             debug_loclists_sec = None,
-            debug_rnglists_sec = None
+            debug_rnglists_sec = None,
+            debug_sup_sec = None,
+            gnu_debugaltlink_sec = None
         )
 
         CUs = [cu for cu in di.iter_CUs()]

commit 6c3623a74b8e26e754460b88ea8d48deb201037f

Fixed found while adding type hints (#602)
* Fix supplementary_dwarfinfo.get_string_from{→ _}table

Fixes: e972a570d18f ("Supplementary object files (#426)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* Fix architecture specific dump

`get_section_by_name()` might return `None` in case architecture
specific dumping is requested:

```console
$ scripts/readelf.py --arch-specific test/testfiles_for_unittests/arm_with_form_indirect.elf
…
Traceback (most recent call last):
  File "scripts/readelf.py", line 1986, in <module>
    main()
  File "scripts/readelf.py", line 1956, in main
    readelf.display_arch_specific()
  File "scripts/readelf.py", line 800, in display_arch_specific
    self._display_arch_specific_arm()
  File "scripts/readelf.py", line 1832, in _display_arch_specific_arm
    self._display_attributes(attr_sec, describe_attr_tag_arm)
  File "scripts/readelf.py", line 1818, in _display_attributes
    for s in attr_sec.iter_subsections():
             ^^^^^^^^^^^^^^^^^^^^^^^^^
AttributeError: 'NoneType' object has no attribute 'iter_subsections'
```

Fixes: 906f28610b7b ("ARM build attributes section parsing (#160)")
Fixes: 43e77712efde ("Add support for RISC-V attributes (#459)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* Fix _parse_cie_augmentation: Skip armcc augmentation

```console
$ scripts/readelf.py --debug-dump frames test/testfiles_for_unittests/arm_with_form_indirect.elf
…
Traceback (most recent call last):
  File "scripts/readelf.py", line 1988, in <module>
    main()
  File "scripts/readelf.py", line 1964, in main
    readelf.display_debug_dump(args.debug_dump_what)
  File "scripts/readelf.py", line 913, in display_debug_dump
    self._dump_debug_frames()
  File "scripts/readelf.py", line 1381, in _dump_debug_frames
    self._dwarfinfo.CFI_entries())
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "elftools/dwarf/dwarfinfo.py", line 378, in CFI_entries
    return cfi.get_entries()
           ^^^^^^^^^^^^^^^^^

elftools/dwarf/callframe.py
@@ -270,6 +270,10 @@ class CallFrameInfo(object):
         if not augmentation:
             return ('', {})
 
+        # Ignore armcc augmentations.
+        if augmentation.startswith(b'armcc'):
+            return (b'', {})
+
         # Augmentation parsing works in minimal mode here: we need the length
         # field to be able to skip unhandled augmentation fields.
         assert augmentation.startswith(b'z'), (

elftools/dwarf/dwarfinfo.py
@@ -677,7 +677,7 @@ class DWARFInfo(object):
                         replace_value(data, field.content_type, self.get_string_from_table)
                     elif field.form in ('DW_FORM_strp_sup', 'DW_FORM_GNU_strp_alt'):
                         if self.supplementary_dwarfinfo:
-                            replace_value(data, field.content_type, self.supplementary_dwarfinfo.get_string_fromtable)
+                            replace_value(data, field.content_type, self.supplementary_dwarfinfo.get_string_from_table)
                         else:
                             replace_value(data, field.content_type, lambda x: str(x))
                     elif field.form in ('DW_FORM_strp_sup', 'DW_FORM_strx', 'DW_FORM_strx1', 'DW_FORM_strx2', 'DW_FORM_strx3', 'DW_FORM_strx4'):

elftools/elf/descriptions.py
@@ -237,30 +237,30 @@ def describe_note(x, machine):
 
 
 def describe_attr_tag_arm(tag, val, extra):
+    s = _DESCR_ATTR_TAG_ARM.get(tag, '"%s"' % tag)
     idx = ENUM_ATTR_TAG_ARM[tag] - 1
     d_entry = _DESCR_ATTR_VAL_ARM[idx]
 
     if d_entry is None:
         if tag == 'TAG_COMPATIBILITY':
-            return (_DESCR_ATTR_TAG_ARM[tag]
-                    + 'flag = %d, vendor = %s' % (val, extra))
+            return s + 'flag = %d, vendor = %s' % (val, extra)
 
         elif tag == 'TAG_ALSO_COMPATIBLE_WITH':
             if val.tag == 'TAG_CPU_ARCH':
-                return _DESCR_ATTR_TAG_ARM[tag] + d_entry[val]
+                d_entry = _DESCR_ATTR_VAL_ARM[5]  # TAG_CPU_ARCH
+                return s + d_entry.get(val.value, '??? (%d)' % val.value)
 
             else:
-                return _DESCR_ATTR_TAG_ARM[tag] + '??? (%d)' % val.tag
+                return s + '??? (%d)' % val.tag
 
         elif tag == 'TAG_NODEFAULTS':
-            return _DESCR_ATTR_TAG_ARM[tag] + 'True'
+            return s + 'True'
 
-        s = _DESCR_ATTR_TAG_ARM[tag]
         s += '"%s"' % val if val else ''
         return s
 
     else:
-        return _DESCR_ATTR_TAG_ARM[tag] + d_entry[val]
+        return s + d_entry[val]
 
 def describe_attr_tag_riscv(tag, val, extra):
     idx = ENUM_ATTR_TAG_RISCV[tag] - 1
@@ -733,14 +733,14 @@ _DESCR_ATTR_TAG_ARM = dict(
     TAG_CPU_ARCH='Tag_CPU_arch: ',
     TAG_CPU_ARCH_PROFILE='Tag_CPU_arch_profile: ',
     TAG_ARM_ISA_USE='Tag_ARM_ISA_use: ',
-    TAG_THUMB_ISA_USE='Tag_Thumb_ISA_use: ',
+    TAG_THUMB_ISA_USE='Tag_THUMB_ISA_use: ',
     TAG_FP_ARCH='Tag_FP_arch: ',
     TAG_WMMX_ARCH='Tag_WMMX_arch: ',
     TAG_ADVANCED_SIMD_ARCH='Tag_Advanced_SIMD_arch: ',
     TAG_PCS_CONFIG='Tag_PCS_config: ',
     TAG_ABI_PCS_R9_USE='Tag_ABI_PCS_R9_use: ',
-    TAG_ABI_PCS_RW_DATA='Tag_ABI_PCS_RW_use: ',
-    TAG_ABI_PCS_RO_DATA='Tag_ABI_PCS_RO_use: ',
+    TAG_ABI_PCS_RW_DATA='Tag_ABI_PCS_RW_data: ',
+    TAG_ABI_PCS_RO_DATA='Tag_ABI_PCS_RO_data: ',
     TAG_ABI_PCS_GOT_USE='Tag_ABI_PCS_GOT_use: ',
     TAG_ABI_PCS_WCHAR_T='Tag_ABI_PCS_wchar_t: ',
     TAG_ABI_FP_ROUNDING='Tag_ABI_FP_rounding: ',
@@ -951,10 +951,7 @@ _DESCR_ATTR_VAL_ARM = [
         5: 'Prefer Accuracy',
         6: 'Aggressive Accuracy',
     },
-    { #32 TAG_COMPATIBILITY
-        0: 'No',
-        1: 'Yes',
-    },
+    None, #32 TAG_COMPATIBILITY
     None, #33
     { #34 TAG_CPU_UNALIGNED_ACCESS
         0: 'None',

elftools/elf/relocation.py
@@ -137,7 +137,7 @@ class RelrRelocationTable(object):
 
         # If DT_RELRSZ is zero, offset is meaningless and could be None.
         if self._size == 0:
-            return []
+            return
 
         limit = self._offset + self._size
         relr = self._offset

scripts/readelf.py
@@ -815,15 +815,14 @@ class ReadElf(object):
             sys.stderr.write('readelf: Warning: Section \'%s\' was not dumped because it does not exist!\n' % (
                 section_spec))
             return
-        if section['sh_type'] == 'SHT_NOBITS':
-            self._emitline("\nSection '%s' has no data to dump." % (
+        if section['sh_type'] == 'SHT_NOBITS' or not (data := section.data()):
+            self._emitline("Section '%s' has no data to dump." % (
                 section_spec))
             return
 
         self._emitline("\nHex dump of section '%s':" % section.name)
         self._note_relocs_for_section(section)
         addr = section['sh_addr']
-        data = section.data()
         dataptr = 0
 
         while dataptr < len(data):
@@ -1832,13 +1831,15 @@ class ReadElf(object):
         """ Display the ARM architecture-specific info contained in the file.
         """
         attr_sec = self.elffile.get_section_by_name('.ARM.attributes')
-        self._display_attributes(attr_sec, describe_attr_tag_arm)
+        if attr_sec:
+            self._display_attributes(attr_sec, describe_attr_tag_arm)
 
     def _display_arch_specific_riscv(self):
         """ Display the RISC-V architecture-specific info contained in the file.
         """
         attr_sec = self.elffile.get_section_by_name('.riscv.attributes')
-        self._display_attributes(attr_sec, describe_attr_tag_riscv)
+        if attr_sec:
+            self._display_attributes(attr_sec, describe_attr_tag_riscv)
 
     def _emit(self, s=''):
         """ Emit an object to output

test/run_readelf_tests.py
@@ -67,7 +67,7 @@ def run_test_on_file(filename, verbose=False, opt=None):
             '--debug-dump=frames', '--debug-dump=frames-interp',
             '--debug-dump=aranges', '--debug-dump=pubtypes',
             '--debug-dump=pubnames', '--debug-dump=loc',
-            '--debug-dump=Ranges'
+            '--debug-dump=Ranges', '--arch-specific',
             ]
     else:
         options = [opt]
@@ -103,6 +103,10 @@ def run_test_on_file(filename, verbose=False, opt=None):
         # but produces nonsensical output, but ultimately it's a toolchain bug (in IAR I presume).
         if "dwarf_v4cie" in filename and option == "--debug-dump=aranges":
             continue        
+
+        # TODO(pmhahn): for now only ARM works
+        if option in {"-A", "--arch-specific"} and "-eabi-" not in filename:
+            continue
 
         # sevaa says: there is another shorted out test; in dwarf_lineprogramv5.elf, the two bytes at 0x2072 were
         # patched from 0x07 0x10 to 00 00.

test/testfiles_for_readelf/arm-eabi-attr-names.S
@@ -1,0 +1,46 @@
+@ https://sourceware.org/git?p=binutils-gdb.git;a=blob;f=gas/testsuite/gas/arm/attr-names.s;h=c43fb88f67b7be158cceb13cb84caddaa7cd47b8;hb=HEAD
+@ Test ARM EABI attributes, built on x86-64:
+@ arm-linux-gnueabi-gcc -o arm-eabi-attr-names.o -c arm-eabi-attr-names.S
+.eabi_attribute Tag_CPU_raw_name, "random-cpu"
+.eabi_attribute Tag_CPU_name, "cpu"
+.eabi_attribute Tag_CPU_arch, 1
+.eabi_attribute Tag_CPU_arch_profile, 'S'
+.eabi_attribute Tag_ARM_ISA_use, 1
+.eabi_attribute Tag_THUMB_ISA_use, 1
+.eabi_attribute Tag_FP_arch, 1
+.eabi_attribute Tag_VFP_arch, 1
+.eabi_attribute Tag_WMMX_arch, 1
+.eabi_attribute Tag_Advanced_SIMD_arch, 1
+.eabi_attribute Tag_PCS_config, 1
+.eabi_attribute Tag_ABI_PCS_R9_use, 1
+.eabi_attribute Tag_ABI_PCS_RW_data, 1
+.eabi_attribute Tag_ABI_PCS_RO_data, 1
+.eabi_attribute Tag_ABI_PCS_GOT_use, 1
+.eabi_attribute Tag_ABI_PCS_wchar_t, 2
+.eabi_attribute Tag_ABI_FP_rounding, 1
+.eabi_attribute Tag_ABI_FP_denormal, 1
+.eabi_attribute Tag_ABI_FP_exceptions, 1
+.eabi_attribute Tag_ABI_FP_user_exceptions, 1
+.eabi_attribute Tag_ABI_FP_number_model, 1
+.eabi_attribute Tag_ABI_align_needed, 1
+.eabi_attribute Tag_ABI_align8_needed, 1
+.eabi_attribute Tag_ABI_align_preserved, 1
+.eabi_attribute Tag_ABI_align8_preserved, 1
+.eabi_attribute Tag_ABI_enum_size, 1
+.eabi_attribute Tag_ABI_HardFP_use, 1
+.eabi_attribute Tag_ABI_VFP_args, 1
+.eabi_attribute Tag_ABI_WMMX_args, 1
+.eabi_attribute Tag_ABI_optimization_goals, 1
+.eabi_attribute Tag_ABI_FP_optimization_goals, 1
+.eabi_attribute Tag_compatibility, 1, "gnu"
+.eabi_attribute Tag_CPU_unaligned_access, 1
+.eabi_attribute Tag_FP_HP_extension, 1
+.eabi_attribute Tag_VFP_HP_extension, 1
+.eabi_attribute Tag_ABI_FP_16bit_format, 1
+.eabi_attribute Tag_MPextension_use, 1
+.eabi_attribute Tag_DIV_use, 1
+.eabi_attribute Tag_nodefaults, 0
+.eabi_attribute Tag_also_compatible_with, "\06\013"
+.eabi_attribute Tag_conformance, "2.08"
+.eabi_attribute Tag_T2EE_use, 1
+.eabi_attribute Tag_Virtualization_use, 3