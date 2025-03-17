6c6cb5c683ed75cf2f2c157027d7610539cd170a
Philipp Hahn <phahn-oss@avm.de>
2025-03-17 08:40:40 +0100

    datatype: Check for parent None
    `get_parent()` may return `None`, in wich case `parent.tag` would raise
    an `AttributeError`. Make the check explicit for type checking.
    
    Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

--- elftools/dwarf/datatype_cpp.py
+++ elftools/dwarf/datatype_cpp.py
@@ -107,7 +107,7 @@ def parse_cpp_datatype(var_die: DIE) -> TypeDesc:
     # Check the nesting - important for parameters
     parent = type_die.get_parent()
     scopes: list[str] = []
-    while parent.tag in ('DW_TAG_class_type', 'DW_TAG_structure_type', 'DW_TAG_union_type', 'DW_TAG_namespace'):
+    while parent and parent.tag in ('DW_TAG_class_type', 'DW_TAG_structure_type', 'DW_TAG_union_type', 'DW_TAG_namespace'):
         scopes.insert(0, safe_DIE_name(parent, _strip_type_tag(parent) + " "))
         # If unnamed scope, fall back to scope type - like "structure "
         parent = parent.get_parent()
@@ -207,7 +207,7 @@ def get_class_spec_if_member(func_spec: DIE, the_func: DIE) -> ClassDesc | None:
     parent = func_spec.get_parent()
 
     scopes: list[str] = []
-    while parent.tag in ("DW_TAG_class_type", "DW_TAG_structure_type", "DW_TAG_namespace"):
+    while parent and parent.tag in ("DW_TAG_class_type", "DW_TAG_structure_type", "DW_TAG_namespace"):
         scopes.insert(0, DIE_name(parent))
         parent = parent.get_parent()
     if scopes:
