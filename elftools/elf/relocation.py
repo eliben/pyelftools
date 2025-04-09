2512c3082a73f61ec695d6e75d4348e8237c3846
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-04-09 14:25:55 +0000

    Added set8 reloc type

--- elftools/elf/relocation.py
+++ elftools/elf/relocation.py
@@ -537,7 +537,10 @@ class RelocationHandler:
         ENUM_RELOC_TYPE_RISCV['R_RISCV_SET6']: _RELOCATION_RECIPE_TYPE(
             bytesize=1, has_addend=True, calc_func=_reloc_calc_value_plus_sym_6),
         ENUM_RELOC_TYPE_RISCV['R_RISCV_SUB6']: _RELOCATION_RECIPE_TYPE(
-            bytesize=1, has_addend=True, calc_func=_reloc_calc_value_minus_sym_addend_6)
+            bytesize=1, has_addend=True, calc_func=_reloc_calc_value_minus_sym_addend_6),
+        ENUM_RELOC_TYPE_RISCV['R_RISCV_SET8']: _RELOCATION_RECIPE_TYPE(
+            bytesize=1, has_addend=True, calc_func=_reloc_calc_sym_plus_addend),
         }
+    
 
 
