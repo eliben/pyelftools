eliben/pyelftools

commit ba0da09cf47383bf7ea44e21860b70b187bb7026

Modernize python code for `construct` and add types (#609)
* construct: Fix doctests

Python 3 is working on `bytes`.

Use raw-strings to allow backslash escaped bytes.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix String documentation

`paddir=both` -> `center`; see
[PaddedStringAdapter](elftools/construct/adapters.py#L186).

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix Switch documentation

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix BitStreamWriter bytes

Since Python 3 streams work on `bytes`, not (Unicode) `str`ings.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Implement IO for BitStreams

`BitStreamReader` and `BitStreamWriter` are supposed to be used instead
of `IO[bytes]`, but actually implement a different signature: that would
force all functions currently accepting `IO[bytes]` to be changed to
`IO[bytes] | BitStreamReader` respective `IO[bytes] | BitStreamWriter`.

Instead let both inherit from `io.RawIOBase` to inherit the complete API
and implement that interface by returning the correct values and types.

See <https://docs.python.org/3/library/io.html#io.RawIOBase>.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>^

* construct: Use named arguments instead of kwargs

Explicitly name the named parameters in the function signature instead
of using `**kwargs`, which is a pain to type annotate.
This also allows us to get rid of the manual check for extra unknown
arguments, as the compiler will do that for us.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Initialize variables out of loop

Type checkers complain about `pos` being undefined inside the exception
handling code. Actually `ConstructError` will only be raised by
`parse()` inside the loop, so both `c` and `pos` will be assigned at
least once, but all type-checkers fail to detect this.

Initialize both `c` and `pos` before the loop to silence them.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/construct/lib/container.py
@@ -1,14 +1,41 @@
 """
 Various containers.
 """
+from __future__ import annotations
 
 from collections.abc import MutableMapping
 from pprint import pformat
+from typing import IO, TYPE_CHECKING, Any, Literal, TypeVar, overload
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Iterator
+    from typing import Self  # 3.11+
+
+    from typing_extensions import Concatenate, ParamSpec  # 3.10+
+
+    from ..core import Construct
+    from .hex import HexString
+
+    _P = ParamSpec('_P')
+    _R = TypeVar('_R')
+    _T = TypeVar('_T')
 
 
-def recursion_lock(retval, lock_name = "__recursion_lock__"):
-    def decorator(func):
-        def wrapper(self, *args, **kw):
+__all__ = [
+    "recursion_lock",
+    "Container", "FlagsContainer", "ListContainer", "LazyContainer",
+]
+
+
+def recursion_lock(
+    retval: _R,
+    lock_name: str = "__recursion_lock__",
+) -> Callable[[Callable[Concatenate[Any, _P], _T]], Callable[Concatenate[Any, _P], _T | _R]]:
+
+    def decorator(
+        func: Callable[Concatenate[Any, _P], _T],
+    ) -> Callable[Concatenate[Any, _P], _T | _R]:
+        def wrapper(self: Any, *args: _P.args, **kw: _P.kwargs) -> _T | _R:
             if getattr(self, lock_name, False):
                 return retval
             setattr(self, lock_name, True)
@@ -20,45 +47,72 @@ def recursion_lock(retval, lock_name = "__recursion_lock__"):
         return wrapper
     return decorator
 
-class Container(MutableMapping):
+class Container(MutableMapping[str, Any]):
     """
     A generic container of attributes.
 
     Containers are the common way to express parsed data.
     """
 
-    def __init__(self, **kw):
+    def __init__(self, **kw: Any) -> None:
         self.__dict__ = kw
 
     # The core dictionary interface.
 
-    def __getitem__(self, name):
+    @overload
+    def __getitem__(self, name: Literal[
+        "ch_addralign", "ch_size",
+        "length",
+        "n_descsz", "n_offset", "n_namesz",
+        "sh_addralign", "sh_flags", "sh_size",
+        "bloom_size", "nbuckets",
+    ]) -> int: ...
+    @overload
+    def __getitem__(self, name: Literal[
+        "ch_type",
+        "sh_type",
+        "n_name", "n_type",
+        "tag", "vendor_name",
+    ]) -> str: ...
+    @overload
+    def __getitem__(self, name: str) -> Any: ...
+    def __getitem__(self, name: str) -> Any:
         return self.__dict__[name]
 
-    def __delitem__(self, name):
+    def __delitem__(self, name: str) -> None:
         del self.__dict__[name]
 
-    def __setitem__(self, name, value):
+    def __setitem__(self, name: str, value: Any) -> None:
         self.__dict__[name] = value
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self.__dict__)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.__dict__.keys())
 
     # Copy interface.
 
-    def copy(self):
+    def copy(self) -> Self:
         return self.__class__(**self.__dict__)
 
     __copy__ = copy
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "%s(%s)" % (self.__class__.__name__, repr(self.__dict__))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "%s(%s)" % (self.__class__.__name__, str(self.__dict__))
+
+    if TYPE_CHECKING:
+        # elftools.construct.debug Probe.printout()
+        stream_position: int
+        following_stream_data: str | HexString
+        context: Container
+        stack: ListContainer
+        # allow arbitray attributes
+        def __setattr__(self, name: str, value: object) -> None: ...
+        def __getattr__(self, name: str) -> Any: ...
 
 class FlagsContainer(Container):
     """
@@ -68,12 +122,12 @@ class FlagsContainer(Container):
     """
 
     @recursion_lock("<...>")
-    def __str__(self):
+    def __str__(self) -> str:
         d = dict((k, self[k]) for k in self
                  if self[k] and not k.startswith("_"))
         return "%s(%s)" % (self.__class__.__name__, pformat(d))
 
-class ListContainer(list):
+class ListContainer(list[Any]):
     """
     A container for lists.
     """
@@ -81,33 +135,33 @@ class ListContainer(list):
     __slots__ = ["__recursion_lock__"]
 
     @recursion_lock("[...]")
-    def __str__(self):
+    def __str__(self) -> str:
         return pformat(self)
 
 class LazyContainer:
 
     __slots__ = ["subcon", "stream", "pos", "context", "_value"]
 
-    def __init__(self, subcon, stream, pos, context):
+    def __init__(self, subcon: Construct, stream: IO[bytes], pos: int, context: Container) -> None:
         self.subcon = subcon
         self.stream = stream
         self.pos = pos
         self.context = context
         self._value = NotImplemented
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         try:
-            return self._value == other._value
+            return self._value == other._value  # type: ignore[attr-defined]
         except AttributeError:
             return False
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         return not (self == other)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__pretty_str__()
 
-    def __pretty_str__(self, nesting = 1, indentation = "    "):
+    def __pretty_str__(self, nesting: int = 1, indentation: str = "    ") -> str:
         if self._value is NotImplemented:
             text = "<unread>"
         elif hasattr(self._value, "__pretty_str__"):
@@ -116,17 +170,17 @@ class LazyContainer:
             text = str(self._value)
         return "%s: %s" % (self.__class__.__name__, text)
 
-    def read(self):
+    def read(self) -> Any:
         self.stream.seek(self.pos)
         return self.subcon._parse(self.stream, self.context)
 
-    def dispose(self):
-        self.subcon = None
-        self.stream = None
-        self.context = None
-        self.pos = None
+    def dispose(self) -> None:
+        del self.subcon
+        del self.stream
+        del self.context
+        del self.pos
 
-    def _get_value(self):
+    def _get_value(self) -> Any:
         if self._value is NotImplemented:
             self._value = self.read()
         return self._value

elftools/construct/__init__.py
@@ -12,24 +12,24 @@ Homepage:
     http://construct.wikispaces.com (including online tutorial)
 
 Typical usage:
-    >>> from construct import *
+    >>> from ..construct import *
 
 Hands-on example:
-    >>> from construct import *
+    >>> from ..construct import *
     >>> s = Struct("foo",
     ...     UBInt8("a"),
     ...     UBInt16("b"),
     ... )
-    >>> s.parse("\\x01\\x02\\x03")
-    Container(a = 1, b = 515)
-    >>> print(s.parse("\\x01\\x02\\x03"))
-    Container:
-        a = 1
-        b = 515
-    >>> s.build(Container(a = 1, b = 0x0203))
-    "\\x01\\x02\\x03"
+    >>> s.parse(b"\\x01\\x02\\x03")
+    Container({'a': 1, 'b': 515})
+    >>> print(s.parse(b"\\x01\\x02\\x03"))
+    Container({'a': 1, 'b': 515})
+    >>> s.build(Container(a=1, b=0x0203))
+    b'\\x01\\x02\\x03'
 """
+from __future__ import annotations
 
+from .lib.container import *
 from .core import *
 from .adapters import *
 from .macros import *
@@ -39,9 +39,9 @@ from .debug import Probe, Debugger
 #===============================================================================
 # Metadata
 #===============================================================================
-__author__ = "tomer filiba (tomerfiliba [at] gmail.com)"
-__maintainer__ = "Corbin Simpson <MostAwesomeDude@gmail.com>"
-__version__ = "2.06"
+__author__: str = "tomer filiba (tomerfiliba [at] gmail.com)"
+__maintainer__: str = "Corbin Simpson <MostAwesomeDude@gmail.com>"
+__version__: str = "2.06"
 
 #===============================================================================
 # Shorthand expressions
@@ -59,10 +59,20 @@ Embed = Embedded
 #===============================================================================
 import functools
 import warnings
+from typing import TYPE_CHECKING, TypeVar
 
-def deprecated(f):
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from typing_extensions import ParamSpec
+
+    _P = ParamSpec('_P')
+    _T = TypeVar('_T')
+
+
+def deprecated(f: Callable[_P, _T]) -> Callable[_P, _T]:
     @functools.wraps(f)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
         warnings.warn(
             "This name is deprecated, use %s instead" % f.__name__,
             DeprecationWarning, stacklevel=2)

elftools/construct/adapters.py
@@ -1,23 +1,41 @@
+from __future__ import annotations
+
 from io import BytesIO
+from typing import TYPE_CHECKING, Any, Callable, Literal
 
 from .core import Adapter, AdaptationError, Pass
 from .lib import int_to_bin, bin_to_int, swap_bytes
 from .lib import FlagsContainer, HexString
+
+if TYPE_CHECKING:
+    from collections.abc import Hashable, Mapping, Sized
+
+    from .core import Construct, _Pass
+    from .lib import Container, ListContainer
+
+
+__all__ = [
+    "BitIntegerError", "MappingError", "ConstError", "ValidationError", "PaddingError",
+    "BitIntegerAdapter", "MappingAdapter", "FlagsAdapter", "StringAdapter", "PaddedStringAdapter",
+    "LengthValueAdapter", "CStringAdapter", "TunnelAdapter", "ExprAdapter", "HexDumpAdapter", "ConstAdapter",
+    "SlicingAdapter", "IndexingAdapter", "PaddingAdapter",
+    "Validator", "OneOf", "NoneOf",
+]
 
 
 #===============================================================================
 # exceptions
 #===============================================================================
 class BitIntegerError(AdaptationError):
-    __slots__ = []
+    __slots__: list[str] = []
 class MappingError(AdaptationError):
-    __slots__ = []
+    __slots__: list[str] = []
 class ConstError(AdaptationError):
-    __slots__ = []
+    __slots__: list[str] = []
 class ValidationError(AdaptationError):
-    __slots__ = []
+    __slots__: list[str] = []
 class PaddingError(AdaptationError):
-    __slots__ = []
+    __slots__: list[str] = []
 
 #===============================================================================
 # adapters
@@ -37,15 +55,15 @@ class BitIntegerAdapter(Adapter):
     * bytesize - number of bits per byte, used for byte-swapping (if swapped).
       default is 8.
     """
-    __slots__ = ["width", "swapped", "signed", "bytesize"]
-    def __init__(self, subcon, width, swapped = False, signed = False,
-                 bytesize = 8):
+    __slots__: list[str] = ["width", "swapped", "signed", "bytesize"]
+    def __init__(self, subcon: Construct, width: int, swapped: bool = False, signed: bool = False,
+            bytesize: int = 8) -> None:
         Adapter.__init__(self, subcon)
         self.width = width
         self.swapped = swapped
         self.signed = signed
         self.bytesize = bytesize
-    def _encode(self, obj, context):
+    def _encode(self, obj: int, context: Container) -> bytes:
         if obj < 0 and not self.signed:
             raise BitIntegerError("object is negative, but field is not signed",
                 obj)
@@ -53,7 +71,7 @@ class BitIntegerAdapter(Adapter):
         if self.swapped:
             obj2 = swap_bytes(obj2, bytesize = self.bytesize)
         return obj2
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> int:
         if self.swapped:
             obj = swap_bytes(obj, bytesize = self.bytesize)
         return bin_to_int(obj, signed = self.signed)
@@ -74,15 +92,17 @@ class MappingAdapter(Adapter):
       in the encoding mapping. if no object is given, an exception is raised.
       if `Pass` is used, the unmapped object will be passed as-is
     """
-    __slots__ = ["encoding", "decoding", "encdefault", "decdefault"]
-    def __init__(self, subcon, decoding, encoding,
-                 decdefault = NotImplemented, encdefault = NotImplemented):
+    __slots__: list[str] = ["encoding", "decoding", "encdefault", "decdefault"]
+    def __init__(self, subcon: Construct, decoding: Mapping[Any, Any], encoding: Mapping[Any, Any],
+            decdefault: Hashable | _Pass = NotImplemented,
+            encdefault: Hashable | _Pass = NotImplemented,
+    ) -> None:
         Adapter.__init__(self, subcon)
         self.decoding = decoding
         self.encoding = encoding
         self.decdefault = decdefault
         self.encdefault = encdefault
-    def _encode(self, obj, context):
+    def _encode(self, obj: Hashable, context: Container) -> Hashable:
         try:
             return self.encoding[obj]
         except (KeyError, TypeError):
@@ -92,7 +112,7 @@ class MappingAdapter(Adapter):
             if self.encdefault is Pass:
                 return obj
             return self.encdefault
-    def _decode(self, obj, context):
+    def _decode(self, obj: Hashable, context: Container) -> Hashable:
         try:
             return self.decoding[obj]
         except (KeyError, TypeError):
@@ -113,17 +133,17 @@ class FlagsAdapter(Adapter):
     * subcon - the subcon to extract
     * flags - a dictionary mapping flag-names to their value
     """
-    __slots__ = ["flags"]
-    def __init__(self, subcon, flags):
+    __slots__: list[str] = ["flags"]
+    def __init__(self, subcon: Construct, flags: dict[str, int]) -> None:
         Adapter.__init__(self, subcon)
         self.flags = flags
-    def _encode(self, obj, context):
+    def _encode(self, obj: FlagsContainer, context: Container) -> int:
         flags = 0
         for name, value in self.flags.items():
             if getattr(obj, name, False):
                 flags |= value
         return flags
-    def _decode(self, obj, context):
+    def _decode(self, obj: int, context: Container) -> FlagsContainer:
         obj2 = FlagsContainer()
         for name, value in self.flags.items():
             setattr(obj2, name, bool(obj & value))
@@ -140,17 +160,19 @@ class StringAdapter(Adapter):
     * encoding - the character encoding name (e.g., "utf8"), or None to
       return raw bytes (usually 8-bit ASCII).
     """
-    __slots__ = ["encoding"]
-    def __init__(self, subcon, encoding = None):
+    __slots__: list[str] = ["encoding"]
+    def __init__(self, subcon: Construct, encoding: str | None = None) -> None:
         Adapter.__init__(self, subcon)
         self.encoding = encoding
-    def _encode(self, obj, context):
+    def _encode(self, obj: bytes | str, context: Container) -> bytes:
         if self.encoding:
+            assert isinstance(obj, str)
             obj = obj.encode(self.encoding)
+        assert isinstance(obj, bytes)
         return obj
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> bytes | str:
         if self.encoding:
-            obj = obj.decode(self.encoding)
+            return obj.decode(self.encoding)
         return obj
 
 class PaddedStringAdapter(Adapter):
@@ -167,9 +189,9 @@ class PaddedStringAdapter(Adapter):
       "left"). the default is "right". trimming is only meaningful for
       building, when the given string is too long.
     """
-    __slots__ = ["padchar", "paddir", "trimdir"]
-    def __init__(self, subcon, padchar = b"\x00", paddir = "right",
-                 trimdir = "right"):
+    __slots__: list[str] = ["padchar", "paddir", "trimdir"]
+    def __init__(self, subcon: Construct, padchar: bytes = b"\x00", paddir: Literal["right", "left", "center"] = "right",
+            trimdir: Literal["right", "left"] = "right"):
         if paddir not in ("right", "left", "center"):
             raise ValueError("paddir must be 'right', 'left' or 'center'",
                 paddir)
@@ -179,7 +201,7 @@ class PaddedStringAdapter(Adapter):
         self.padchar = padchar
         self.paddir = paddir
         self.trimdir = trimdir
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> bytes:
         if self.paddir == "right":
             obj = obj.rstrip(self.padchar)
         elif self.paddir == "left":
@@ -187,7 +209,7 @@ class PaddedStringAdapter(Adapter):
         else:
             obj = obj.strip(self.padchar)
         return obj
-    def _encode(self, obj, context):
+    def _encode(self, obj: bytes, context: Container) -> bytes:
         size = self._sizeof(context)
         if self.paddir == "right":
             obj = obj.ljust(size, self.padchar)
@@ -211,10 +233,10 @@ class LengthValueAdapter(Adapter):
     Parameters:
     * subcon - the subcon returning a length-value pair
     """
-    __slots__ = []
-    def _encode(self, obj, context):
+    __slots__: list[str] = []
+    def _encode(self, obj: Sized, context: Container) -> tuple[int, Sized]:
         return (len(obj), obj)
-    def _decode(self, obj, context):
+    def _decode(self, obj: tuple[int, Sized] | ListContainer, context: Container) -> Sized:
         return obj[1]
 
 class CStringAdapter(StringAdapter):
@@ -228,13 +250,13 @@ class CStringAdapter(StringAdapter):
       return raw-bytes. the terminator characters are not affected by the
       encoding.
     """
-    __slots__ = ["terminators"]
-    def __init__(self, subcon, terminators = b"\x00", encoding = None):
+    __slots__: list[str] = ["terminators"]
+    def __init__(self, subcon: Construct, terminators: bytes = b"\x00", encoding: str | None = None) -> None:
         StringAdapter.__init__(self, subcon, encoding = encoding)
         self.terminators = terminators
-    def _encode(self, obj, context):
+    def _encode(self, obj: bytes | str, context: Container) -> bytes:
         return StringAdapter._encode(self, obj, context) + self.terminators[0:1]
-    def _decode(self, obj, context):
+    def _decode(self, obj: list[bytes], context: Container) -> bytes | str:  # type: ignore[override]
         return StringAdapter._decode(self, b''.join(obj[:-1]), context)
 
 class TunnelAdapter(Adapter):
@@ -259,13 +281,13 @@ class TunnelAdapter(Adapter):
         GreedyRange(UBInt16("elements"))
     )
     """
-    __slots__ = ["inner_subcon"]
-    def __init__(self, subcon, inner_subcon):
+    __slots__: list[str] = ["inner_subcon"]
+    def __init__(self, subcon: Construct, inner_subcon: Construct) -> None:
         Adapter.__init__(self, subcon)
         self.inner_subcon = inner_subcon
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> Any:
         return self.inner_subcon._parse(BytesIO(obj), context)
-    def _encode(self, obj, context):
+    def _encode(self, obj: Any, context: Container) -> bytes:
         stream = BytesIO()
         self.inner_subcon._build(obj, stream, context)
         return stream.getvalue()
@@ -289,23 +311,27 @@ class ExprAdapter(Adapter):
         decoder = lambda obj, ctx: obj * 4,
     )
     """
-    __slots__ = ["_encode", "_decode"]
-    def __init__(self, subcon, encoder, decoder):
+    __slots__: list[str] = ["__encode", "__decode"]
+    def __init__(self, subcon: Construct, encoder: Callable[[Any, Container], bytes], decoder: Callable[[bytes, Container], Any]) -> None:
         Adapter.__init__(self, subcon)
-        self._encode = encoder
-        self._decode = decoder
+        self.__encode = encoder
+        self.__decode = decoder
+    def _encode(self, obj: Any, context: Container) -> Any:
+        return self.__encode(obj, context)
+    def _decode(self, obj: Any, context: Container) -> Any:
+        return self.__decode(obj, context)
 
 class HexDumpAdapter(Adapter):
     """
     Adapter for hex-dumping strings. It returns a HexString, which is a string
     """
-    __slots__ = ["linesize"]
-    def __init__(self, subcon, linesize = 16):
+    __slots__: list[str] = ["linesize"]
+    def __init__(self, subcon: Construct, linesize: int = 16) -> None:
         Adapter.__init__(self, subcon)
         self.linesize = linesize
-    def _encode(self, obj, context):
+    def _encode(self, obj: Any, context: Container) -> Any:
         return obj
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> HexString:
         return HexString(obj, linesize = self.linesize)
 
 class ConstAdapter(Adapter):
@@ -321,15 +347,15 @@ class ConstAdapter(Adapter):
     Const(Field("signature", 2), "MZ")
     """
     __slots__ = ["value"]
-    def __init__(self, subcon, value):
+    def __init__(self, subcon: Construct, value: object) -> None:
         Adapter.__init__(self, subcon)
         self.value = value
-    def _encode(self, obj, context):
+    def _encode(self, obj: object, context: Container) -> object:
         if obj is None or obj == self.value:
             return self.value
         else:
             raise ConstError("expected %r, found %r" % (self.value, obj))
-    def _decode(self, obj, context):
+    def _decode(self, obj: object, context: Container) -> object:
         if obj != self.value:
             raise ConstError("expected %r, found %r" % (self.value, obj))
         return obj
@@ -344,16 +370,16 @@ class SlicingAdapter(Adapter):
     * stop - stop index (or None for up-to-end)
     * step - step (or None for every element)
     """
-    __slots__ = ["start", "stop", "step"]
-    def __init__(self, subcon, start, stop = None):
+    __slots__: list[str] = ["start", "stop", "step"]
+    def __init__(self, subcon: Construct, start: int, stop: int | None = None) -> None:
         Adapter.__init__(self, subcon)
         self.start = start
         self.stop = stop
-    def _encode(self, obj, context):
+    def _encode(self, obj: list[Any], context: Container) -> list[Any]:
         if self.start is None:
             return obj
         return [None] * self.start + obj
-    def _decode(self, obj, context):
+    def _decode(self, obj: list[Any], context: Container) -> list[Any]:
         return obj[self.start:self.stop]
 
 class IndexingAdapter(Adapter):
@@ -364,15 +390,15 @@ class IndexingAdapter(Adapter):
     * subcon - the subcon to index
     * index - the index of the list to get
     """
-    __slots__ = ["index"]
-    def __init__(self, subcon, index):
+    __slots__: list[str] = ["index"]
+    def __init__(self, subcon: Construct, index: int) -> None:
         Adapter.__init__(self, subcon)
         if type(index) is not int:
             raise TypeError("index must be an integer", type(index))
         self.index = index
-    def _encode(self, obj, context):
+    def _encode(self, obj: Any, context: Container) -> list[Any]:
         return [None] * self.index + [obj]
-    def _decode(self, obj, context):
+    def _decode(self, obj: list[Any], context: Container) -> Any:
         return obj[self.index]
 
 class PaddingAdapter(Adapter):
@@ -385,14 +411,14 @@ class PaddingAdapter(Adapter):
     * strict - whether or not to verify, during parsing, that the given
       padding matches the padding pattern. default is False (unstrict)
     """
-    __slots__ = ["pattern", "strict"]
-    def __init__(self, subcon, pattern = b"\x00", strict = False):
+    __slots__: list[str] = ["pattern", "strict"]
+    def __init__(self, subcon: Construct, pattern: bytes = b"\x00", strict: bool = False) -> None:
         Adapter.__init__(self, subcon)
         self.pattern = pattern
         self.strict = strict
-    def _encode(self, obj, context):
+    def _encode(self, obj: None, context: Container) -> bytes:
         return self._sizeof(context) * self.pattern
-    def _decode(self, obj, context):
+    def _decode(self, obj: bytes, context: Container) -> bytes:
         if self.strict:
             expected = self._sizeof(context) * self.pattern
             if obj != expected:
@@ -411,14 +437,14 @@ class Validator(Adapter):
     Parameters:
     * subcon - the subcon to validate
     """
-    __slots__ = []
-    def _decode(self, obj, context):
+    __slots__: list[str] = []
+    def _decode(self, obj: object, context: Container) -> object:
         if not self._validate(obj, context):
             raise ValidationError("invalid object", obj)
         return obj
-    def _encode(self, obj, context):
+    def _encode(self, obj: object, context: Container) -> object:
         return self._decode(obj, context)
-    def _validate(self, obj, context):
+    def _validate(self, obj: object, context: Container) -> bool:
         raise NotImplementedError()
 
 class OneOf(Validator):
@@ -428,25 +454,26 @@ class OneOf(Validator):
     :param ``Construct`` subcon: object to validate
     :param iterable valids: a set of valid values
 
-    >>> OneOf(UBInt8("foo"), [4,5,6,7]).parse("\\x05")
+    >>> from ..construct import UBInt8
+    >>> OneOf(UBInt8("foo"), [4,5,6,7]).parse(b"\\x05")
     5
-    >>> OneOf(UBInt8("foo"), [4,5,6,7]).parse("\\x08")
+    >>> OneOf(UBInt8("foo"), [4,5,6,7]).parse(b"\\x08")  # doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
         ...
-    construct.core.ValidationError: ('invalid object', 8)
+    ValidationError: ('invalid object', 8)
     >>>
     >>> OneOf(UBInt8("foo"), [4,5,6,7]).build(5)
-    '\\x05'
-    >>> OneOf(UBInt8("foo"), [4,5,6,7]).build(9)
+    b'\\x05'
+    >>> OneOf(UBInt8("foo"), [4,5,6,7]).build(9)  # doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
         ...
-    construct.core.ValidationError: ('invalid object', 9)
+    ValidationError: ('invalid object', 9)
     """
-    __slots__ = ["valids"]
-    def __init__(self, subcon, valids):
+    __slots__: list[str] = ["valids"]
+    def __init__(self, subcon: Construct, valids: list[object]) -> None:
         Validator.__init__(self, subcon)
         self.valids = valids
-    def _validate(self, obj, context):
+    def _validate(self, obj: object, context: Container) -> bool:
         return obj in self.valids
 
 class NoneOf(Validator):
@@ -456,16 +483,17 @@ class NoneOf(Validator):
     :param ``Construct`` subcon: object to validate
     :param iterable invalids: a set of invalid values
 
-    >>> NoneOf(UBInt8("foo"), [4,5,6,7]).parse("\\x08")
+    >>> from ..construct import UBInt8
+    >>> NoneOf(UBInt8("foo"), [4,5,6,7]).parse(b"\\x08")
     8
-    >>> NoneOf(UBInt8("foo"), [4,5,6,7]).parse("\\x06")
+    >>> NoneOf(UBInt8("foo"), [4,5,6,7]).parse(b"\\x06")  # doctest: +IGNORE_EXCEPTION_DETAIL
     Traceback (most recent call last):
         ...
-    construct.core.ValidationError: ('invalid object', 6)
+    ValidationError: ('invalid object', 6)
     """
-    __slots__ = ["invalids"]
-    def __init__(self, subcon, invalids):
+    __slots__: list[str] = ["invalids"]
+    def __init__(self, subcon: Construct, invalids: list[object]) -> None:
         Validator.__init__(self, subcon)
         self.invalids = invalids
-    def _validate(self, obj, context):
+    def _validate(self, obj: object, context: Container) -> bool:
         return obj not in self.invalids

commit b6b38a964a94b42f7936b8d7cfdd9cce6e65a217

construct: Narrow typing for buckets and chains
Add @overload for "buckets" and  "chains" to return narrower type `list[int]`.

Fixes: ba0da09 ("Modernize python code for `construct` and add types (#609)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/construct/lib/container.py
@@ -65,7 +65,7 @@ class Container(MutableMapping[str, Any]):
         "length",
         "n_descsz", "n_offset", "n_namesz",
         "sh_addralign", "sh_flags", "sh_size",
-        "bloom_size", "nbuckets",
+        "bloom_size", "nbuckets", "nchains",
     ]) -> int: ...
     @overload
     def __getitem__(self, name: Literal[
@@ -74,6 +74,10 @@ class Container(MutableMapping[str, Any]):
         "n_name", "n_type",
         "tag", "vendor_name",
     ]) -> str: ...
+    @overload
+    def __getitem__(self, name: Literal[
+        "buckets", "chains",
+    ]) -> list[int]: ...
     @overload
     def __getitem__(self, name: str) -> Any: ...
     def __getitem__(self, name: str) -> Any: