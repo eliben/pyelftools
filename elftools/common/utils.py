eliben/pyelftools

commit 89f40c422228147bd5bc891f2b8d4cffef1f5e56

mostly implemented extra information output for dwarf info section dump

elftools/dwarf/descriptions.py
@@ -91,35 +91,6 @@ _ATTR_DESCRIPTION_MAP = defaultdict(
 )
 
 
-
-def _describe_empty(attr, die, section_offset):
-    return ''
-
-def _describe_dw_inl_extra(attr, die, section_offset):
-    return _DESCR_DW_INL.get(
-        attr.value,
-        '  (Unknown inline attribute value: %x)' % attr.value)
-
-def _describe_dw_lang_extra(attr, die, section_offset):
-    return _DESCR_DW_LANG.get(
-        attr.value,
-        '  (Unknown: %x)' % attr.value)
-
-def _describe_dw_ate_extra(attr, die, section_offset):
-    return _DESCR_DW_ATE.get(
-        attr.value,
-        '  (unknown type)')
-
-
-_EXTRA_INFO_DESCRIPTION_MAP = defaultdict(
-    lambda: _describe_empty, # default_factory
-    
-    DW_AT_inline=_describe_dw_inl_extra,
-    DW_AT_language=_describe_dw_lang_extra,
-    DW_AT_encoding=_describe_dw_ate_extra,
-)
-
-
 _DESCR_DW_INL = {
     DW_INL_not_inlined: '(not inlined)',
     DW_INL_inlined: '(inlined)',
@@ -182,4 +153,97 @@ _DESCR_DW_ATE = {
     DW_ATE_HP_imaginary_float80: '(HP_imaginary_float80)',
     DW_ATE_HP_imaginary_float128: '(HP_imaginary_float128)',
 }
+
+_DESCR_DW_ACCESS = {
+    DW_ACCESS_public: '(public)',
+    DW_ACCESS_protected: '(protected)',
+    DW_ACCESS_private: '(private)',
+}
+
+_DESCR_DW_VIS = {
+    DW_VIS_local: '(local)',
+    DW_VIS_exported: '(exported)',
+    DW_VIS_qualified: '(qualified)',
+}
+
+_DESCR_DW_VIRTUALITY = {
+    DW_VIRTUALITY_none: '(none)',
+    DW_VIRTUALITY_virtual: '(virtual)',
+    DW_VIRTUALITY_pure_virtual: '(pure virtual)',
+}
+
+_DESCR_DW_ID_CASE = {
+    DW_ID_case_sensitive: '(case_sensitive)',
+    DW_ID_up_case: '(up_case)',
+    DW_ID_down_case: '(down_case)',
+    DW_ID_case_insensitive: '(case_insensitive)',
+}
+
+_DESCR_DW_CC = {
+    DW_CC_normal: '(normal)',
+    DW_CC_program: '(program)',
+    DW_CC_nocall: '(nocall)',
+}
+
+_DESCR_DW_ORD = {
+    DW_ORD_row_major: '(row major)',
+    DW_ORD_col_major: '(column major)',
+}
+
+
+def _make_extra_mapper(mapping, default, default_interpolate_value=False):
+    """ Create a mapping function from attribute parameters to an extra
+        value that should be displayed.
+    """
+    def mapper(attr, die, section_offset):
+        if default_interpolate_value:
+            d = default % attr.value
+        else:
+            d = default
+        return mapping.get(attr.value, d)
+    return mapper
+
+
+def _make_extra_string(s=''):
+    """ Create an extra function that just returns a constant string.
+    """
+    def extra(attr, die, section_offset):
+        return s
+    return extra
+
+_location_list_extra = _make_extra_string('(location list)')
+
+
+_EXTRA_INFO_DESCRIPTION_MAP = defaultdict(
+    lambda: _make_extra_string(''), # default_factory
+    
+    DW_AT_inline=_make_extra_mapper(
+        _DESCR_DW_INL, '(Unknown inline attribute value: %x',
+        default_interpolate_value=True),
+    DW_AT_language=_make_extra_mapper(
+        _DESCR_DW_LANG, '(Unknown: %x)', default_interpolate_value=True),
+    DW_AT_encoding=_make_extra_mapper(_DESCR_DW_ATE, '(unknown type)'),
+    DW_AT_accessibility=_make_extra_mapper(
+        _DESCR_DW_ACCESS, '(unknown accessibility)'),
+    DW_AT_visibility=_make_extra_mapper(
+        _DESCR_DW_VIS, '(unknown visibility)'),
+    DW_AT_virtuality=_make_extra_mapper(
+        _DESCR_DW_VIRTUALITY, '(unknown virtuality)'),
+    DW_AT_identifier_case=_make_extra_mapper(
+        _DESCR_DW_ID_CASE, '(unknown case)'),
+    DW_AT_calling_convention=_make_extra_mapper(
+        _DESCR_DW_CC, '(unknown convention)'),
+    DW_AT_ordering=_make_extra_mapper(
+        _DESCR_DW_ORD, '(undefined)'),
+    DW_AT_frame_base=_location_list_extra,
+    DW_AT_location=_location_list_extra,
+    DW_AT_string_length=_location_list_extra,
+    DW_AT_return_addr=_location_list_extra,
+    DW_AT_data_member_location=_location_list_extra,
+    DW_AT_vtable_elem_location=_location_list_extra,
+    DW_AT_segment=_location_list_extra,
+    DW_AT_static_link=_location_list_extra,
+    DW_AT_use_location=_location_list_extra,
+)
+
 

elftools/dwarf/constants.py
@@ -76,3 +76,45 @@ DW_ATE_HP_imaginary_float80=0x85
 DW_ATE_HP_imaginary_float128=0x86
 
 
+# Access
+#
+DW_ACCESS_public=1
+DW_ACCESS_protected=2
+DW_ACCESS_private=3
+
+
+# Visibility
+#
+DW_VIS_local=1
+DW_VIS_exported=2
+DW_VIS_qualified=3
+
+
+# Virtuality
+#
+DW_VIRTUALITY_none=0
+DW_VIRTUALITY_virtual=1
+DW_VIRTUALITY_pure_virtual=2
+
+
+# ID case
+#
+DW_ID_case_sensitive=0
+DW_ID_up_case=1
+DW_ID_down_case=2
+DW_ID_case_insensitive=3
+
+
+# Calling convention
+#
+DW_CC_normal=0x1
+DW_CC_program=0x2
+DW_CC_nocall=0x3
+
+
+# Ordering
+#
+DW_ORD_row_major=0
+DW_ORD_col_major=1
+
+

commit f0e6cd6776e0a80c15210c93807e513f286e73b9

Cleanup before typing (#635)
* test: Close ELFFile after test

> $ python3 -m unittest discover . test_debuglink.py
> ResourceWarning: unclosed file <_io.BufferedReader name='test/testfiles_for_unittests/debuglink'>

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* dwarf: Fix description of DW_AT

Add closing parenthesis.

Fixes: 89f40c4 ("mostly implemented extra information output for dwarf info section dump")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* utils: Modernize bytes2hex

Use generator expression and an f-string instead of map() with
str.format() to create hex-dump with custom separator.

This change is motivated my `pyrefly check` stumbling over it:

  ERROR Argument `bytes` is not assignable to parameter `iterable` with type
  `Iterable[LiteralString]` in function `map.__new__` [bad-argument-type]
     --> elftools/common/utils.py:149:42
  149 |     return sep.join(map('{:02x}'.format, b))
      |                                          ^

This looks like a bug in `pyrefly` as reveal_type():

  $ pyrefly snippet 'from typing import reveal_type;reveal_type("{:02x}".format)'
  INFO Found `/home/pmhahn/prog/GITLAB/python-packaging/pyproject.toml` marking
       project root, checking root directory with default configuration
  INFO revealed type: Overload[
   (self: LiteralString, *args: LiteralString, **kwargs: LiteralString) -> LiteralString
   (self: LiteralString, *args: object, **kwargs: object) -> str

Performance is nearly the same:
    $ python3 -m timeit '"".join(map("{:02x}".format, bytes(100)))'
    20000 loops, best of 5: 16.4 usec per loop
    $ python3 -m timeit '"".join(f"{o:02x}" for o in bytes(100))'
    20000 loops, best of 5: 16.7 usec per loop

    $ python3 -m timeit '"".join(map("{:02x}".format, bytes(1000)))'
    2000 loops, best of 5: 159 usec per loop
    $ python3 -m timeit '"".join(f"{o:02x}" for o in bytes(1000))'
    2000 loops, best of 5: 161 usec per loop

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/common/utils.py
@@ -127,7 +127,7 @@ def iterbytes(b):
 def bytes2hex(b, sep=''):
     if not sep:
         return b.hex()
-    return sep.join(map('{:02x}'.format, b))
+    return sep.join(f'{o:02x}' for o in b)
 
 #------------------------- PRIVATE -------------------------
 

elftools/dwarf/datatype_cpp.py
@@ -29,7 +29,7 @@ def parse_cpp_datatype(var_die):
     """
     t = TypeDesc()
 
-    if not 'DW_AT_type' in var_die.attributes:
+    if 'DW_AT_type' not in var_die.attributes:
         t.tag = ''
         return t
 
@@ -40,7 +40,7 @@ def parse_cpp_datatype(var_die):
     while type_die.tag in ('DW_TAG_const_type', 'DW_TAG_volatile_type', 'DW_TAG_pointer_type', 'DW_TAG_reference_type'):
         modifier = _strip_type_tag(type_die) # const/volatile/reference/pointer
         mods.insert(0, modifier)
-        if not 'DW_AT_type' in type_die.attributes: # void* is encoded as a pointer to nothing
+        if 'DW_AT_type' not in type_die.attributes: # void* is encoded as a pointer to nothing
             t.name = t.tag = "void"
             t.modifiers = tuple(mods)
             return t

elftools/dwarf/descriptions.py
@@ -500,7 +500,7 @@ _EXTRA_INFO_DESCRIPTION_MAP = defaultdict(
     lambda: _make_extra_string(''), # default_factory
 
     DW_AT_inline=_make_extra_mapper(
-        _DESCR_DW_INL, '(Unknown inline attribute value: %x',
+        _DESCR_DW_INL, '(Unknown inline attribute value: %x)',
         default_interpolate_value=True),
     DW_AT_language=_make_extra_mapper(
         _DESCR_DW_LANG, '(Unknown: %x)', default_interpolate_value=True),
@@ -609,20 +609,20 @@ class ExprDumper:
         )
 
     def _init_lookups(self):
-        self._ops_with_decimal_arg = set([
+        self._ops_with_decimal_arg = {
             'DW_OP_const1u', 'DW_OP_const1s', 'DW_OP_const2u', 'DW_OP_const2s',
             'DW_OP_const4u', 'DW_OP_const4s', 'DW_OP_const8u', 'DW_OP_const8s',
             'DW_OP_constu', 'DW_OP_consts', 'DW_OP_pick', 'DW_OP_plus_uconst',
             'DW_OP_bra', 'DW_OP_skip', 'DW_OP_fbreg', 'DW_OP_piece',
-            'DW_OP_deref_size', 'DW_OP_xderef_size', 'DW_OP_regx',])
+            'DW_OP_deref_size', 'DW_OP_xderef_size', 'DW_OP_regx'}
 
         for n in range(0, 32):
             self._ops_with_decimal_arg.add('DW_OP_breg%s' % n)
 
-        self._ops_with_two_decimal_args = set(['DW_OP_bregx'])
+        self._ops_with_two_decimal_args = {'DW_OP_bregx'}
 
-        self._ops_with_hex_arg = set(
-            ['DW_OP_addr', 'DW_OP_call2', 'DW_OP_call4', 'DW_OP_call_ref'])
+        self._ops_with_hex_arg = {
+            'DW_OP_addr', 'DW_OP_call2', 'DW_OP_call4', 'DW_OP_call_ref'}
 
     def _dump_to_string(self, opcode, opcode_name, args, cu_offset=None):
         # Some GNU ops contain an offset from the current CU as an argument,

elftools/dwarf/dwarf_util.py
@@ -19,7 +19,7 @@ def _get_base_offset(cu, base_attribute_name):
     encoded objects - range lists, location lists, strings, addresses.
     """
     cu_top_die = cu.get_top_DIE()
-    if not base_attribute_name in cu_top_die.attributes:
+    if base_attribute_name not in cu_top_die.attributes:
         raise DWARFError("The CU at offset 0x%x needs %s" % (cu.cu_offset, base_attribute_name))
     return cu_top_die.attributes[base_attribute_name].value
 

elftools/elf/sections.py
@@ -117,10 +117,7 @@ class Section:
         return self.header[name]
 
     def __eq__(self, other):
-        try:
-            return self.header == other.header
-        except AttributeError:
-            return False
+        return isinstance(other, Section) and self.header == other.header
 
     def __hash__(self):
         return hash(self.header)

scripts/readelf.py
@@ -1031,7 +1031,7 @@ class ReadElf:
 
         symbol = self._versioninfo['versym'].get_symbol(nsym)
         index = symbol.entry['ndx']
-        if not index in ('VER_NDX_LOCAL', 'VER_NDX_GLOBAL'):
+        if index not in ('VER_NDX_LOCAL', 'VER_NDX_GLOBAL'):
             index = int(index)
 
             if self._versioninfo['type'] == 'GNU':
@@ -1172,7 +1172,7 @@ class ReadElf:
 
                     attr_desc = describe_attr_value(attr, die, section_offset)
 
-                    if 'DW_OP_fbreg' in attr_desc and current_function and not 'DW_AT_frame_base' in current_function.attributes:
+                    if 'DW_OP_fbreg' in attr_desc and current_function and 'DW_AT_frame_base' not in current_function.attributes:
                         postfix = ' [without dw_at_frame_base]'
                     else:
                         postfix = ''
@@ -1416,7 +1416,7 @@ class ReadElf:
         """ Dump the aranges table
         """
         aranges_table = self._dwarfinfo.get_aranges()
-        if aranges_table == None:
+        if aranges_table is None:
             return
         # Seems redundant, but we need to get the unsorted set of entries
         # to match system readelf.

test/run_dwarfdump_tests.py
@@ -157,7 +157,7 @@ def main():
     args = argparser.parse_args()
 
     if args.parallel:
-        if args.verbose or args.keep_going == False:
+        if args.verbose or not args.keep_going:
             print('WARNING: parallel mode disables verbosity and always keeps going')
 
     if args.verbose:

test/run_readelf_tests.py
@@ -282,7 +282,7 @@ def main():
     args = argparser.parse_args()
 
     if args.parallel:
-        if args.verbose or args.keep_going == False:
+        if args.verbose or not args.keep_going:
             print('WARNING: parallel mode disables verbosity and always keeps going')
 
     if args.verbose:

test/test_corpus_naming.py
@@ -8,7 +8,6 @@ import unittest
 import os
 
 from elftools.elf.elffile import ELFFile
-from elftools.elf.segments import NoteSegment
 
 class TestCorpusNaming(unittest.TestCase):
     def scan_under(self, subdir):

test/test_debuglink.py
@@ -90,6 +90,7 @@ class TestDebuglink(unittest.TestCase):
         elf = ELFFile.load_from_path('test/testfiles_for_unittests/debuglink')
         subprograms = self.subprograms_from_debuglink(elf)
         self.assertEqual(subprograms, {b'main': (0x1161, 0x52), b'addNumbers': (0x1149, 0x18)})
+        elf.close()
 
     def test_relative_loader_rejects_path_traversal(self):
         with tempfile.TemporaryDirectory() as tmpdir:

test/test_dwarf_aranges.py
@@ -3,8 +3,8 @@ import unittest
 
 from elftools.elf.elffile import ELFFile
 
-address_a = 0x112f;
-address_b = 0x1154;
+address_a = 0x112f
+address_b = 0x1154
 
 class TestRangeLists(unittest.TestCase):
     def test_arange_absent(self):

test/test_dwarf_cu_and_die_cache.py
@@ -31,9 +31,9 @@ class TestCacheLUTandDIEref(unittest.TestCase):
             for (k, v) in pt.items():
                 ndie = dwarf.get_DIE_from_lut_entry(v)
                 self.dprint(ndie)
-                if not 'DW_AT_type' in ndie.attributes:
+                if 'DW_AT_type' not in ndie.attributes:
                     continue
-                if not 'DW_AT_name' in ndie.attributes:
+                if 'DW_AT_name' not in ndie.attributes:
                     continue
                 tlist = []
                 tdie = ndie