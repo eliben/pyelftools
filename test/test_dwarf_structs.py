cc4fc890e680ddc9129cf2598473c25cd00ab9f2
Philipp Hahn <phahn-oss@avm.de>
2026-04-29 10:03:13 +0200

    ISC003: Concatenate bytes literal
    <https://docs.astral.sh/ruff/rules/explicit-string-concatenation/>
    ruff check --select ISC003 --fix
    
    Let the Python parser concatenate several bytes while parsing instead of
    concatenating them during runtime.
    
    Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

--- test/test_dwarf_structs.py
+++ test/test_dwarf_structs.py
@@ -14,18 +14,18 @@ class TestDWARFStructs(unittest.TestCase):
         ds = DWARFStructs(little_endian=True, dwarf_format=32, address_size=4)
 
         c = ds.Dwarf_lineprog_header.parse(
-            b'\x04\x10\x00\x00' +    # initial length
-            b'\x02\x00' +            # version
-            b'\x20\x00\x00\x00' +    # header length
-            b'\x05\x10\x40\x50' +    # until and including line_range
-            b'\x06' +                # opcode_base
-            b'\x00\x01\x04\x08\x0C' + # standard_opcode_lengths
+            b'\x04\x10\x00\x00'    # initial length
+            b'\x02\x00'            # version
+            b'\x20\x00\x00\x00'    # header length
+            b'\x05\x10\x40\x50'    # until and including line_range
+            b'\x06'                # opcode_base
+            b'\x00\x01\x04\x08\x0C' # standard_opcode_lengths
             # 2 dir names followed by a NULL
-            b'\x61\x62\x00\x70\x00\x00' +
+            b'\x61\x62\x00\x70\x00\x00'
             # a file entry
-            b'\x61\x72\x00\x0C\x0D\x0F' +
+            b'\x61\x72\x00\x0C\x0D\x0F'
             # and another entry
-            b'\x45\x50\x51\x00\x86\x12\x07\x08' +
+            b'\x45\x50\x51\x00\x86\x12\x07\x08'
             # followed by NULL
             b'\x00')
         self.assertEqual(c.version, 2)
