eliben/pyelftools

commit ba0da09cf47383bf7ea44e21860b70b187bb7026

Modernize python code for `construct` and add types (#609)
* construct: Fix doctests

Python 3 is working on `bytes`.

Use raw-strings to allow backslash escaped bytes.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix String documentation

`paddir=both` -> `center`; see
[PaddedStringAdapter](elftools/construct/adapters.py#L186).

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix Switch documentation

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Fix BitStreamWriter bytes

Since Python 3 streams work on `bytes`, not (Unicode) `str`ings.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Implement IO for BitStreams

`BitStreamReader` and `BitStreamWriter` are supposed to be used instead
of `IO[bytes]`, but actually implement a different signature: that would
force all functions currently accepting `IO[bytes]` to be changed to
`IO[bytes] | BitStreamReader` respective `IO[bytes] | BitStreamWriter`.

Instead let both inherit from `io.RawIOBase` to inherit the complete API
and implement that interface by returning the correct values and types.

See <https://docs.python.org/3/library/io.html#io.RawIOBase>.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>^

* construct: Use named arguments instead of kwargs

Explicitly name the named parameters in the function signature instead
of using `**kwargs`, which is a pain to type annotate.
This also allows us to get rid of the manual check for extra unknown
arguments, as the compiler will do that for us.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* construct: Initialize variables out of loop

Type checkers complain about `pos` being undefined inside the exception
handling code. Actually `ConstructError` will only be raised by
`parse()` inside the loop, so both `c` and `pos` will be assigned at
least once, but all type-checkers fail to detect this.

Initialize both `c` and `pos` before the loop to silence them.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/construct/debug.py
@@ -1,10 +1,14 @@
 """
 Debugging utilities for constructs
 """
+from __future__ import annotations
+
 import sys
 import traceback
 import pdb
 import inspect
+from typing import IO, Any
+
 from .core import Construct, Subconstruct
 from .lib import HexString, Container, ListContainer
 
@@ -38,9 +42,9 @@ class Probe(Construct):
     ]
     counter = 0
 
-    def __init__(self, name = None, show_stream = True,
-                 show_context = True, show_stack = True,
-                 stream_lookahead = 100):
+    def __init__(self, name: str | None = None, show_stream: bool = True,
+            show_context: bool = True, show_stack: bool = True,
+            stream_lookahead: int = 100) -> None:
         Construct.__init__(self, None)
         if name is None:
             Probe.counter += 1
@@ -50,16 +54,16 @@ class Probe(Construct):
         self.show_context = show_context
         self.show_stack = show_stack
         self.stream_lookahead = stream_lookahead
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "%s(%r)" % (self.__class__.__name__, self.printname)
-    def _parse(self, stream, context):
+    def _parse(self, stream: IO[bytes], context: Container) -> None:
         self.printout(stream, context)
-    def _build(self, obj, stream, context):
+    def _build(self, obj: Any, stream: IO[bytes], context: Container) -> None:
         self.printout(stream, context)
-    def _sizeof(self, context):
+    def _sizeof(self, context: Container) -> int:
         return 0
 
-    def printout(self, stream, context):
+    def printout(self, stream: IO[bytes], context: Container) -> None:
         obj = Container()
         if self.show_stream:
             obj.stream_position = stream.tell()
@@ -105,7 +109,7 @@ class Debugger(Subconstruct):
     )
     """
     __slots__ = ["retval"]
-    def _parse(self, stream, context):
+    def _parse(self, stream: IO[bytes], context: Container) -> Any:
         try:
             return self.subcon._parse(stream, context)
         except Exception:
@@ -116,12 +120,12 @@ class Debugger(Subconstruct):
                 raise
             else:
                 return self.retval
-    def _build(self, obj, stream, context):
+    def _build(self, obj: Any, stream: IO[bytes], context: Container) -> None:
         try:
             self.subcon._build(obj, stream, context)
         except Exception:
             self.handle_exc()
-    def handle_exc(self, msg = None):
+    def handle_exc(self, msg: str | None = None) -> None:
         print("=" * 80)
         print("Debugging exception of %s:" % (self.subcon,))
         print("".join(traceback.format_exception(*sys.exc_info())[1:]))

elftools/construct/lib/container.py
@@ -1,14 +1,41 @@
 """
 Various containers.
 """
+from __future__ import annotations
 
 from collections.abc import MutableMapping
 from pprint import pformat
+from typing import IO, TYPE_CHECKING, Any, Literal, TypeVar, overload
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Iterator
+    from typing import Self  # 3.11+
+
+    from typing_extensions import Concatenate, ParamSpec  # 3.10+
+
+    from ..core import Construct
+    from .hex import HexString
+
+    _P = ParamSpec('_P')
+    _R = TypeVar('_R')
+    _T = TypeVar('_T')
 
 
-def recursion_lock(retval, lock_name = "__recursion_lock__"):
-    def decorator(func):
-        def wrapper(self, *args, **kw):
+__all__ = [
+    "recursion_lock",
+    "Container", "FlagsContainer", "ListContainer", "LazyContainer",
+]
+
+
+def recursion_lock(
+    retval: _R,
+    lock_name: str = "__recursion_lock__",
+) -> Callable[[Callable[Concatenate[Any, _P], _T]], Callable[Concatenate[Any, _P], _T | _R]]:
+
+    def decorator(
+        func: Callable[Concatenate[Any, _P], _T],
+    ) -> Callable[Concatenate[Any, _P], _T | _R]:
+        def wrapper(self: Any, *args: _P.args, **kw: _P.kwargs) -> _T | _R:
             if getattr(self, lock_name, False):
                 return retval
             setattr(self, lock_name, True)
@@ -20,45 +47,72 @@ def recursion_lock(retval, lock_name = "__recursion_lock__"):
         return wrapper
     return decorator
 
-class Container(MutableMapping):
+class Container(MutableMapping[str, Any]):
     """
     A generic container of attributes.
 
     Containers are the common way to express parsed data.
     """
 
-    def __init__(self, **kw):
+    def __init__(self, **kw: Any) -> None:
         self.__dict__ = kw
 
     # The core dictionary interface.
 
-    def __getitem__(self, name):
+    @overload
+    def __getitem__(self, name: Literal[
+        "ch_addralign", "ch_size",
+        "length",
+        "n_descsz", "n_offset", "n_namesz",
+        "sh_addralign", "sh_flags", "sh_size",
+        "bloom_size", "nbuckets",
+    ]) -> int: ...
+    @overload
+    def __getitem__(self, name: Literal[
+        "ch_type",
+        "sh_type",
+        "n_name", "n_type",
+        "tag", "vendor_name",
+    ]) -> str: ...
+    @overload
+    def __getitem__(self, name: str) -> Any: ...
+    def __getitem__(self, name: str) -> Any:
         return self.__dict__[name]
 
-    def __delitem__(self, name):
+    def __delitem__(self, name: str) -> None:
         del self.__dict__[name]
 
-    def __setitem__(self, name, value):
+    def __setitem__(self, name: str, value: Any) -> None:
         self.__dict__[name] = value
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self.__dict__)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.__dict__.keys())
 
     # Copy interface.
 
-    def copy(self):
+    def copy(self) -> Self:
         return self.__class__(**self.__dict__)
 
     __copy__ = copy
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "%s(%s)" % (self.__class__.__name__, repr(self.__dict__))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "%s(%s)" % (self.__class__.__name__, str(self.__dict__))
+
+    if TYPE_CHECKING:
+        # elftools.construct.debug Probe.printout()
+        stream_position: int
+        following_stream_data: str | HexString
+        context: Container
+        stack: ListContainer
+        # allow arbitray attributes
+        def __setattr__(self, name: str, value: object) -> None: ...
+        def __getattr__(self, name: str) -> Any: ...
 
 class FlagsContainer(Container):
     """
@@ -68,12 +122,12 @@ class FlagsContainer(Container):
     """
 
     @recursion_lock("<...>")
-    def __str__(self):
+    def __str__(self) -> str:
         d = dict((k, self[k]) for k in self
                  if self[k] and not k.startswith("_"))
         return "%s(%s)" % (self.__class__.__name__, pformat(d))
 
-class ListContainer(list):
+class ListContainer(list[Any]):
     """
     A container for lists.
     """
@@ -81,33 +135,33 @@ class ListContainer(list):
     __slots__ = ["__recursion_lock__"]
 
     @recursion_lock("[...]")
-    def __str__(self):
+    def __str__(self) -> str:
         return pformat(self)
 
 class LazyContainer:
 
     __slots__ = ["subcon", "stream", "pos", "context", "_value"]
 
-    def __init__(self, subcon, stream, pos, context):
+    def __init__(self, subcon: Construct, stream: IO[bytes], pos: int, context: Container) -> None:
         self.subcon = subcon
         self.stream = stream
         self.pos = pos
         self.context = context
         self._value = NotImplemented
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         try:
-            return self._value == other._value
+            return self._value == other._value  # type: ignore[attr-defined]
         except AttributeError:
             return False
 
-    def __ne__(self, other):
+    def __ne__(self, other: object) -> bool:
         return not (self == other)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__pretty_str__()
 
-    def __pretty_str__(self, nesting = 1, indentation = "    "):
+    def __pretty_str__(self, nesting: int = 1, indentation: str = "    ") -> str:
         if self._value is NotImplemented:
             text = "<unread>"
         elif hasattr(self._value, "__pretty_str__"):
@@ -116,17 +170,17 @@ class LazyContainer:
             text = str(self._value)
         return "%s: %s" % (self.__class__.__name__, text)
 
-    def read(self):
+    def read(self) -> Any:
         self.stream.seek(self.pos)
         return self.subcon._parse(self.stream, self.context)
 
-    def dispose(self):
-        self.subcon = None
-        self.stream = None
-        self.context = None
-        self.pos = None
+    def dispose(self) -> None:
+        del self.subcon
+        del self.stream
+        del self.context
+        del self.pos
 
-    def _get_value(self):
+    def _get_value(self) -> Any:
         if self._value is NotImplemented:
             self._value = self.read()
         return self._value

elftools/construct/__init__.py
@@ -12,24 +12,24 @@ Homepage:
     http://construct.wikispaces.com (including online tutorial)
 
 Typical usage:
-    >>> from construct import *
+    >>> from ..construct import *
 
 Hands-on example:
-    >>> from construct import *
+    >>> from ..construct import *
     >>> s = Struct("foo",
     ...     UBInt8("a"),
     ...     UBInt16("b"),
     ... )
-    >>> s.parse("\\x01\\x02\\x03")
-    Container(a = 1, b = 515)
-    >>> print(s.parse("\\x01\\x02\\x03"))
-    Container:
-        a = 1
-        b = 515
-    >>> s.build(Container(a = 1, b = 0x0203))
-    "\\x01\\x02\\x03"
+    >>> s.parse(b"\\x01\\x02\\x03")
+    Container({'a': 1, 'b': 515})
+    >>> print(s.parse(b"\\x01\\x02\\x03"))
+    Container({'a': 1, 'b': 515})
+    >>> s.build(Container(a=1, b=0x0203))
+    b'\\x01\\x02\\x03'
 """
+from __future__ import annotations
 
+from .lib.container import *
 from .core import *
 from .adapters import *
 from .macros import *
@@ -39,9 +39,9 @@ from .debug import Probe, Debugger
 #===============================================================================
 # Metadata
 #===============================================================================
-__author__ = "tomer filiba (tomerfiliba [at] gmail.com)"
-__maintainer__ = "Corbin Simpson <MostAwesomeDude@gmail.com>"
-__version__ = "2.06"
+__author__: str = "tomer filiba (tomerfiliba [at] gmail.com)"
+__maintainer__: str = "Corbin Simpson <MostAwesomeDude@gmail.com>"
+__version__: str = "2.06"
 
 #===============================================================================
 # Shorthand expressions
@@ -59,10 +59,20 @@ Embed = Embedded
 #===============================================================================
 import functools
 import warnings
+from typing import TYPE_CHECKING, TypeVar
 
-def deprecated(f):
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from typing_extensions import ParamSpec
+
+    _P = ParamSpec('_P')
+    _T = TypeVar('_T')
+
+
+def deprecated(f: Callable[_P, _T]) -> Callable[_P, _T]:
     @functools.wraps(f)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
         warnings.warn(
             "This name is deprecated, use %s instead" % f.__name__,
             DeprecationWarning, stacklevel=2)

commit df8f6b97c3f334f80c8eb54d9ee58e44d3324a8d

construct: Remove type annotation from __slots__ 2
`__slots__` does not need a type annotation, especially if a non-empty
sequence is assigned to it as all type checkers are able to deduce the
type then automatically.

Actually the type hint hurts as the code uses a mix of `list[str]` and
`tuple[str, ...]`, which creates additional issues.

As such remove the hints and convert all cases to tuples, as that also
saves some extra bytes.

Fixes: ba0da09 ("Modernize python code for `construct` and add types (#609)")
Fixes: a10a88a ("Typing fixes and improvements to construct (#636)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/construct/debug.py
@@ -36,10 +36,10 @@ class Probe(Construct):
         UBInt8("b"),
     )
     """
-    __slots__ = [
+    __slots__ = (
         "printname", "show_stream", "show_context", "show_stack",
         "stream_lookahead"
-    ]
+    )
     counter = 0
 
     def __init__(self, name: str | None = None, show_stream: bool = True,
@@ -108,7 +108,7 @@ class Debugger(Subconstruct):
         )
     )
     """
-    __slots__ = ["retval"]
+    __slots__ = ("retval",)
     def _parse(self, stream: IO[bytes], context: Container) -> Any:
         try:
             return self.subcon._parse(stream, context)

elftools/construct/lib/container.py
@@ -137,7 +137,7 @@ class ListContainer(list[Any]):
     A container for lists.
     """
 
-    __slots__ = ["__recursion_lock__"]
+    __slots__ = ("__recursion_lock__",)
 
     @recursion_lock("[...]")
     def __str__(self) -> str:
@@ -145,7 +145,7 @@ class ListContainer(list[Any]):
 
 class LazyContainer:
 
-    __slots__ = ["subcon", "stream", "pos", "context", "_value"]
+    __slots__ = ("subcon", "stream", "pos", "context", "_value")
 
     def __init__(self, subcon: Construct, stream: IO[bytes], pos: int, context: Container) -> None:
         self.subcon = subcon