09c688d6cdfa9f97c015e1776af4ace5c411f88e
Philipp Hahn <phahn-oss@avm.de>
2026-05-05 08:39:32 +0200

    typing: Declare before conditional
    Declare `lsda_pointer` and `aug_dict` and `last_line_in_CIE` before the
    conditional. Static type checking will complain otherwise that variables
    might not be declared in the `else` case or raise a NameError.
    
    Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

--- elftools/dwarf/callframe.py
+++ elftools/dwarf/callframe.py
@@ -135,6 +135,8 @@ class CallFrameInfo:
 
         # If the augmentation string is not empty, hope to find a length field
         # in order to skip the data specified augmentation.
+        lsda_pointer = None
+        aug_dict = None
         if is_CIE:
             aug_bytes, aug_dict = self._parse_cie_augmentation(
                     header, entry_structs)
@@ -147,8 +149,6 @@ class CallFrameInfo:
                 lsda_pointer = self._parse_lsda_pointer(entry_structs,
                                                         self.stream.tell() - len(aug_bytes),
                                                         lsda_encoding)
-            else:
-                lsda_pointer = None
 
         # For convenience, compute the end offset for this entry
         end_offset = (
@@ -495,6 +495,7 @@ class CFIEntry:
         """ Decode the instructions contained in the given CFI entry and return
             a DecodedCallFrameTable.
         """
+        last_line_in_CIE = None
         if isinstance(self, CIE):
             # For a CIE, initialize cur_line to an "empty" line
             cie = self
