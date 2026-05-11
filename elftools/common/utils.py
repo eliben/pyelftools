eliben/pyelftools

commit 8dd0aac2cf64607d502448381235522618628513

More cleanups before adding type hints (#641)
* test_debuglink: Fix existing type annotation

Fix returning tuple.
Make annotations a str.
Sort imports: ruff check --select I --fix

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* run_readelf_tests: Print filename on error

Print the input filename in case of errors:
> @@ aborting - scripts/readelf.py --debug-dump=frames-interp returned 1

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `reveal_type` for type change

`reveal_type` is first declared as `TypeDesc` and then changed to `str`,
which static type checkers like `mypy` do not like.

Rename the first variable.

PS: Better rename `reveal_type()` to somethings else as there is
`typing.reveal_type()`[^1].

[^1]: https://docs.python.org/3/library/typing.html#typing.reveal_type

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `all_offsets` for type change

`all_offsets` is first declared as a `set` and then changed to `list`,
which static type checkers like `mypy` do not like.

Rename the second variable to `sorted_offsets`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* Rework get_location_list_at_offset() logic

Combine the `if` statements to help type checking: Otherwise the 2nd
DWARF-5-case need another case to check for `die not None`.

BREAKING CHANGE:
This is a behavioral change as the file position gets changed in the
error case.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Build `params` via generator expression

`params` was first declared as a `tuple` and then changed to `str`,
which static type checkers like `mypy`/`pyright`/`pyrefly`/`ty` do not
like.

Instead build the concatenated string from a generator expression.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Make elffile.section_names a list

`section_names` is only used internally as a `list`, as section names
might get modified and the list is appended.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Fix print format

elftools/dwarf/callframe.py
@@ -135,6 +135,8 @@ class CallFrameInfo:
 
         # If the augmentation string is not empty, hope to find a length field
         # in order to skip the data specified augmentation.
+        lsda_pointer = None
+        aug_dict = None
         if is_CIE:
             aug_bytes, aug_dict = self._parse_cie_augmentation(
                     header, entry_structs)
@@ -147,8 +149,6 @@ class CallFrameInfo:
                 lsda_pointer = self._parse_lsda_pointer(entry_structs,
                                                         self.stream.tell() - len(aug_bytes),
                                                         lsda_encoding)
-            else:
-                lsda_pointer = None
 
         # For convenience, compute the end offset for this entry
         end_offset = (
@@ -495,6 +495,7 @@ class CFIEntry:
         """ Decode the instructions contained in the given CFI entry and return
             a DecodedCallFrameTable.
         """
+        last_line_in_CIE = None
         if isinstance(self, CIE):
             # For a CIE, initialize cur_line to an "empty" line
             cie = self

elftools/elf/descriptions.py
@@ -327,12 +327,15 @@ def describe_note_gnu_properties(properties, machine):
                 prop_desc = ' <corrupt length: 0x%x>' % sz
             else:
                 prop_desc = describe_note_gnu_property_bitmap_and(_DESCR_NOTE_GNU_PROPERTY_RISCV_FEATURE_1_AND, 'RISC-V AND feature', d)
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
-            prop_desc = '<processor-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
-            prop_desc = '<application-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+        elif isinstance(t, int):
+            if _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
+                prop_desc = f"<processor-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
+                prop_desc = f"<application-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            else:
+                prop_desc = f"<unknown type {t:#x} data: {bytes2hex(d, sep=' ')} >"
         else:
-            prop_desc = '<unknown type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+            prop_desc = "<unknown type {t:r} data: {bytes2hex(d, sep=' ')} >"
         descriptions.append(prop_desc)
     return '\n        '.join(descriptions)
 

test/run_readelf_tests.py
@@ -116,13 +116,13 @@ def run_test_on_file(filename, verbose=False, opt=None):
         stdouts = []
         for exe_path in [READELF_PATH, 'scripts/readelf.py']:
             args = [option, filename]
-            if verbose: testlog.info("....executing: '%s %s'" % (
-                exe_path, ' '.join(args)))
+            cmd = " ".join((exe_path, *args))
+            if verbose: testlog.info("....executing: '%s'" % (cmd,))
             t1 = time.time()
             rc, stdout = run_exe(exe_path, args)
             if verbose: testlog.info("....elapsed: %s" % (time.time() - t1,))
             if rc != 0:
-                testlog.error("@@ aborting - '%s %s' returned '%s'" % (exe_path, option, rc))
+                testlog.error("@@ aborting - '%s' returned '%s'" % (cmd, rc))
                 return False
             stdouts.append(stdout)
         if verbose: testlog.info('....comparing output...')

elftools/dwarf/datatype_cpp.py
@@ -60,12 +60,15 @@ def parse_cpp_datatype(var_die):
             ptr_prefix = ''
 
         if t.tag == 'subroutine':
-            params = tuple(format_function_param(p, p) for p in type_die.iter_children() if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes)
-            params = ", ".join(params)
+            params = ", ".join(
+                format_function_param(p, p)
+                for p in type_die.iter_children()
+                if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes
+            )
             if 'DW_AT_type' in type_die.attributes:
-                retval_type = parse_cpp_datatype(type_die)
-                is_pointer = retval_type.modifiers and retval_type.modifiers[-1] == 'pointer'
-                retval_type = str(retval_type)
+                datatype = parse_cpp_datatype(type_die)
+                is_pointer = datatype.modifiers and datatype.modifiers[-1] == 'pointer'
+                retval_type = str(datatype)
                 if not is_pointer:
                     retval_type += " "
             else:

elftools/dwarf/locationlists.py
@@ -92,10 +92,12 @@ class LocationLists:
         Passing the die is only neccessary in DWARF5+, for decoding
         location entry encodings that contain references to other sections.
         """
-        if self.version >= 5 and die is None:
-            raise DWARFError("For this binary, \"die\" needs to be provided")
         self.stream.seek(offset, os.SEEK_SET)
-        return self._parse_location_list_from_stream_v5(die.cu) if self.version >= 5 else self._parse_location_list_from_stream()
+        if self.version >= 5:
+            if die is None:
+                raise DWARFError("For this binary, \"die\" needs to be provided")
+            return self._parse_location_list_from_stream_v5(die.cu)
+        return self._parse_location_list_from_stream()
 
     def iter_location_lists(self):
         """ Iterates through location lists and view pairs. Returns lists of
@@ -151,8 +153,7 @@ class LocationLists:
                             list_offset = attr.value
                             all_offsets.add(list_offset)
                             cu_map[list_offset] = cu
-        all_offsets = list(all_offsets)
-        all_offsets.sort()
+        sorted_offsets = sorted(all_offsets)
 
         if ver5:
             # Loclists section is organized as an array of CUs, each length prefixed.
@@ -172,7 +173,7 @@ class LocationLists:
 
                 while stream.tell() < cu_end_offset:
                     # Skip the gap to the next object
-                    next_offset = all_offsets[offset_index]
+                    next_offset = sorted_offsets[offset_index]
                     if next_offset == stream.tell(): # At an object, either a loc list or a loc view pair
                         locview_pairs = self._parse_locview_pairs(locviews)
                         entries = self._parse_location_list_from_stream_v5(cu_map[stream.tell()])
@@ -183,7 +184,7 @@ class LocationLists:
                             next_offset = cu_end_offset # And implicitly quit the loop within the CU
                         stream.seek(next_offset, os.SEEK_SET)
         else:
-            for offset in all_offsets:
+            for offset in sorted_offsets:
                 list_offset = locviews.get(offset, offset)
                 if cu_map[list_offset].header.version < 5:
                     stream.seek(offset, os.SEEK_SET)

elftools/ehabi/ehabiinfo.py
@@ -154,11 +154,16 @@ class EHABIEntry:
             return None
 
     def __repr__(self):
-        return "<EHABIEntry function_offset=0x%x, personality=%d, %sbytecode=%s>" % (
-            self.function_offset,
-            self.personality,
-            "eh_table_offset=0x%x, " % self.eh_table_offset if self.eh_table_offset else "",
-            self.bytecode_array)
+        fo = self.function_offset
+        to = self.eh_table_offset
+        return (
+	    "<EHABIEntry"
+            f" function_offset={'' if fo is None else '{fo:#x}'}"
+            f", personaality={self.personality}"
+            f"{', eh_table_offset={to:#x}' if to else ''}"
+            f", bytecode={self.bytecode_array}"
+            ">"
+        )
 
 
 class CorruptEHABIEntry(EHABIEntry):

commit b29e21ccc18fd82d616b2bf5a0954d142d86198f

More cleanup to elftools.common (#644)
* run_dwarfdump_tests: Print filename on error

Print the input filename in case of errors:
> @@ aborting - 'scripts/dwarfdump.py --debug-info' returned '1'

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* common: Simplify parse_cstring_from_stream()

Get rid of `found` and return result or None directly from loop.

Use `bytes.partition()` to both find NUL and split on it if found.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* common: Simplify save_dwarf_section()

Re-use `preserve_stream_pos()`.

Remove 2nd `seek()` on same stream to the same position.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* common: Deprecate read_blob()

Directly use `IO.read(size) -> bytes`; `iter(bytes) -> int`, which is
close enough to `list[int]`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* common: Deprecate bytes2hex()

Use `bytes(..).hex(sep)`, which is available since Python 3.8.
<https://docs.python.org/3/library/stdtypes.html#bytes.hex>

Replace several more open-coded versions of it.

Convert to f-strings while we go and fix one broken conversion.

Fixes: 8dd0aac ("More cleanups before adding type hints (#641)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* common: Deprecate iterbytes()

This might have provided some value back in Python 2 types, when IO[str]
was the default, but with Python 3 IO[bytes] should be used and
`iter(bytes)` returns `int`s. There's no need to convert `int`s back to
`1-bytes`. Use `chr(int)` to get a `str`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

---------

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/common/utils.py
@@ -12,7 +12,7 @@ from contextlib import contextmanager
 from typing import IO, TYPE_CHECKING, Any, TypeVar, overload
 
 from .exceptions import ELFParseError, ELFError, DWARFError
-from ..construct import ConstructError, ULInt8
+from ..construct import ConstructError
 import os
 
 if TYPE_CHECKING:
@@ -73,19 +73,13 @@ def parse_cstring_from_stream(stream: IO[bytes], stream_pos: int | None = None) 
         stream.seek(stream_pos)
     CHUNKSIZE = 64
     chunks = []
-    found = False
     while True:
-        chunk = stream.read(CHUNKSIZE)
-        end_index = chunk.find(b'\x00')
-        if end_index >= 0:
-            chunks.append(chunk[:end_index])
-            found = True
-            break
-        else:
-            chunks.append(chunk)
+        chunk, sep, _tail = stream.read(CHUNKSIZE).partition(b'\x00')
+        chunks.append(chunk)
+        if sep:
+            return b''.join(chunks)
         if len(chunk) < CHUNKSIZE:
-            break
-    return b''.join(chunks) if found else None
+            return None
 
 
 def elf_assert(cond: object, msg: str = '') -> None:
@@ -119,36 +113,17 @@ def roundup(num: int, bits: int) -> int:
     """
     return (num - 1 | (1 << bits) - 1) + 1
 
-def read_blob(stream: IO[bytes], length: int) -> list[int]:
-    """Read length bytes from stream, return a list of ints
-    """
-    return [struct_parse(ULInt8(''), stream) for i in range(length)]
 
 def save_dwarf_section(section: DebugSectionDescriptor, filename: str) -> None:
     """Debug helper: dump section contents into a file
     Section is expected to be one of the debug_xxx_sec elements of DWARFInfo
     """
     stream = section.stream
-    pos = stream.tell()
-    stream.seek(0, os.SEEK_SET)
-    section.stream.seek(0)
-    with open(filename, 'wb') as file:
+    with preserve_stream_pos(stream), open(filename, 'wb') as file:
+        stream.seek(0, os.SEEK_SET)
         data = stream.read(section.size)
         file.write(data)
-    stream.seek(pos, os.SEEK_SET)
 
-def iterbytes(b: bytes) -> Iterator[bytes]:
-    """Return an iterator over the elements of a bytes object.
-
-    For example, for b'abc' yields b'a', b'b' and then b'c'.
-    """
-    for i in range(len(b)):
-        yield b[i:i+1]
-
-def bytes2hex(b: bytes, sep: str = '') -> str:
-    if not sep:
-        return b.hex()
-    return sep.join(f'{o:02x}' for o in b)
 
 #------------------------- PRIVATE -------------------------
 

elftools/construct/lib/hex.py
@@ -22,9 +22,9 @@ def hexdump(data: bytes, linesize: int) -> list[str]:
     fmt = fmt % (3 * linesize - 1,)
     for i in range(0, len(data), linesize):
         line = data[i : i + linesize]
-        hextext = " ".join('%02x' % b for b in line)
+        hextext = line.hex(" ")
         rawtext = "".join(_printable[b] for b in line)
-        prettylines.append(fmt % (i, str(hextext), str(rawtext)))
+        prettylines.append(fmt % (i, hextext, rawtext))
     return prettylines
 
 

elftools/dwarf/callframe.py
@@ -12,7 +12,7 @@ from typing import Any, NamedTuple
 from warnings import warn
 
 from ..common.utils import (
-    struct_parse, dwarf_assert, preserve_stream_pos, iterbytes)
+    struct_parse, dwarf_assert, preserve_stream_pos)
 from ..construct import Struct, Switch
 from .enums import DW_EH_encoding_flags
 from .structs import DWARFStructs
@@ -270,11 +270,11 @@ class CallFrameInfo:
             'Unhandled augmentation string: {}'.format(repr(augmentation)))
 
         available_fields = {
-            b'z': entry_structs.Dwarf_uleb128('length'),
-            b'L': entry_structs.Dwarf_uint8('LSDA_encoding'),
-            b'R': entry_structs.Dwarf_uint8('FDE_encoding'),
-            b'S': True,
-            b'P': Struct(
+            'z': entry_structs.Dwarf_uleb128('length'),
+            'L': entry_structs.Dwarf_uint8('LSDA_encoding'),
+            'R': entry_structs.Dwarf_uint8('FDE_encoding'),
+            'S': True,
+            'P': Struct(
                 'personality',
                 entry_structs.Dwarf_uint8('encoding'),
                 Switch('function', lambda ctx: ctx.encoding & 0x0f, {
@@ -288,9 +288,9 @@ class CallFrameInfo:
         fields = []
         aug_dict = {}
 
-        for b in iterbytes(augmentation):
+        for b in augmentation:
             try:
-                fld = available_fields[b]
+                fld = available_fields[chr(b)]
             except KeyError:
                 break
 

elftools/dwarf/descriptions.py
@@ -592,8 +592,9 @@ class ExprDumper:
         self._init_lookups()
 
     def dump_expr(self, expr, cu_offset=None):
-        """ Parse and dump a DWARF expression. expr should be a list of
-            (integer) byte values. cu_offset is the cu_offset
+        """ Parse and dump a DWARF expression.
+            expr should be bytes or a list of (integer) byte values.
+            cu_offset is the cu_offset
             value from the CU object where the expression resides.
             Only affects a handful of GNU opcodes, if None is provided,
             that's not a crash condition, only the expression dump will
@@ -601,7 +602,7 @@ class ExprDumper:
 
             Returns a string representing the expression.
         """
-        parsed = self.expr_parser.parse_expr(expr)
+        parsed = self.expr_parser.parse_expr(bytes(expr))
         return '; '.join(
             self._dump_to_string(deo.op, deo.op_name, deo.args, cu_offset)
             for deo in parsed

elftools/dwarf/dwarf_expr.py
@@ -9,7 +9,7 @@
 from io import BytesIO
 from typing import Any, NamedTuple
 
-from ..common.utils import struct_parse, read_blob
+from ..common.utils import struct_parse
 from ..common.exceptions import DWARFError
 
 
@@ -138,7 +138,7 @@ class DWARFExprParser:
         self._dispatch_table = _init_dispatch_table(structs)
 
     def parse_expr(self, expr):
-        """ Parses expr (a list of integers) into a list of DWARFExprOp.
+        """ Parses expr (bytes or a list of integers) into a list of DWARFExprOp.
 
         The list can potentially be nested.
         """
@@ -195,17 +195,17 @@ def _init_dispatch_table(structs):
     def parse_nestedexpr():
         def parse(stream):
             size = struct_parse(structs.the_Dwarf_uleb128, stream)
-            nested_expr_blob = read_blob(stream, size)
+            nested_expr_blob = stream.read(size)
             return [DWARFExprParser(structs).parse_expr(nested_expr_blob)]
         return parse
 
     # ULEB128, then a blob of that size
     def parse_blob():
-        return lambda stream: [read_blob(stream, struct_parse(structs.the_Dwarf_uleb128, stream))]
+        return lambda stream: [list(stream.read(struct_parse(structs.the_Dwarf_uleb128, stream)))]
 
     # ULEB128 with datatype DIE offset, then byte, then a blob of that size
     def parse_typedblob():
-        return lambda stream: [struct_parse(structs.the_Dwarf_uleb128, stream), read_blob(stream, struct_parse(structs.the_Dwarf_uint8, stream))]
+        return lambda stream: [struct_parse(structs.the_Dwarf_uleb128, stream), list(stream.read(struct_parse(structs.the_Dwarf_uint8, stream)))]
 
     # https://yurydelendik.github.io/webassembly-dwarf/
     # Byte, then variant: 0, 1, 2 => uleb128, 3 => uint32

elftools/elf/descriptions.py
@@ -15,7 +15,6 @@ from .enums import (
     ENUM_DT_FLAGS_1, ENUM_RELOC_TYPE_PPC)
 from .constants import (
     P_FLAGS, RH_FLAGS, SH_FLAGS, SUNW_SYMINFO_FLAGS, VER_FLAGS)
-from ..common.utils import bytes2hex
 
 
 def describe_ei_class(x):
@@ -212,7 +211,7 @@ def describe_note(x, machine):
                 _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
                 n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
         else:
-            desc = '\n   description data: %s ' % bytes2hex(x['n_descdata'])
+            desc = f'\n   description data: {bytes(x["n_descdata"]).hex()} '
     elif x['n_type'] == 'NT_GNU_BUILD_ID':
         desc = '\n    Build ID: %s' % (n_desc)
     elif x['n_type'] == 'NT_GNU_GOLD_VERSION':
@@ -221,9 +220,9 @@ def describe_note(x, machine):
         if x['n_name'] == 'GNU':
             desc = '\n      Properties: ' + describe_note_gnu_properties(x['n_desc'], machine)
         else:
-            desc = '\n   description data: %s ' % bytes2hex(x['n_descdata'])
+            desc = f'\n   description data: {bytes(x["n_descdata"]).hex()} '
     else:
-        desc = '\n      description data: {}'.format(bytes2hex(n_desc))
+        desc = f'\n      description data: {bytes(n_desc).hex()}'
 
     if x['n_type'] == 'NT_GNU_ABI_TAG' and x['n_name'] == 'Android':
         note_type = 'NT_VERSION'
@@ -329,13 +328,13 @@ def describe_note_gnu_properties(properties, machine):
                 prop_desc = describe_note_gnu_property_bitmap_and(_DESCR_NOTE_GNU_PROPERTY_RISCV_FEATURE_1_AND, 'RISC-V AND feature', d)
         elif isinstance(t, int):
             if _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
-                prop_desc = f"<processor-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<processor-specific type {t:#x} data: {bytes(d).hex(' ')} >"
             elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
-                prop_desc = f"<application-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<application-specific type {t:#x} data: {bytes(d).hex(' ')} >"
             else:
-                prop_desc = f"<unknown type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<unknown type {t:#x} data: {bytes(d).hex(' ')} >"
         else:
-            prop_desc = "<unknown type {t:r} data: {bytes2hex(d, sep=' ')} >"
+            prop_desc = f"<unknown type {t:r} data: {bytes(d).hex(' ')} >"
         descriptions.append(prop_desc)
     return '\n        '.join(descriptions)
 

elftools/elf/notes.py
@@ -6,7 +6,7 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-from ..common.utils import struct_parse, bytes2hex, roundup, bytes2str
+from ..common.utils import struct_parse, roundup, bytes2str
 from ..construct import CString
 
 
@@ -41,7 +41,7 @@ def iter_notes(elffile, offset, size):
                                           elffile.stream,
                                           offset)
         elif note['n_type'] == 'NT_GNU_BUILD_ID' and note['n_name'] == 'GNU':
-            note['n_desc'] = bytes2hex(desc_data)
+            note['n_desc'] = bytes(desc_data).hex()
         elif note['n_type'] == 'NT_GNU_GOLD_VERSION' and note['n_name'] == 'GNU':
             note['n_desc'] = bytes2str(desc_data)
         elif note['n_type'] == 'NT_PRPSINFO':

examples/elf_notes.py
@@ -15,7 +15,6 @@ sys.path[0:0] = ['.', '..']
 
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import NoteSection
-from elftools.common.utils import bytes2hex
 
 
 def process_file(filename):
@@ -39,7 +38,7 @@ def process_file(filename):
                 elif note['n_type'] in {'NT_GNU_BUILD_ID', 'NT_GNU_GOLD_VERSION'}:
                     print('    Desc:', desc)
                 else:
-                    print('    Desc:', bytes2hex(desc))
+                    print('    Desc:', bytes(desc).hex())
 
 
 if __name__ == '__main__':

scripts/dwarfdump.py
@@ -121,9 +121,9 @@ FORM_DESCRIPTIONS = dict(
     DW_FORM_data2=_desc_data,
     DW_FORM_data4=_desc_data,
     DW_FORM_data8=_desc_data,
-    DW_FORM_block1=lambda attr, die: "<0x%02x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
-    DW_FORM_block2=lambda attr, die: "<0x%04x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
-    DW_FORM_block4=lambda attr, die: "<0x%08x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
+    DW_FORM_block1=lambda attr, die: "<0x%02x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
+    DW_FORM_block2=lambda attr, die: "<0x%04x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
+    DW_FORM_block4=lambda attr, die: "<0x%08x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
     DW_FORM_ref=_desc_ref,
     DW_FORM_ref1=_desc_ref, DW_FORM_ref2=_desc_ref,
     DW_FORM_ref4=_desc_ref, DW_FORM_ref8=_desc_ref,

scripts/readelf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, '.')
 
 from elftools import __version__
 from elftools.common.exceptions import ELFError
-from elftools.common.utils import bytes2str, iterbytes
+from elftools.common.utils import bytes2str
 from elftools.elf.elffile import ELFFile
 from elftools.elf.dynamic import DynamicSection
 from elftools.elf.enums import ENUM_D_TAG
@@ -1332,10 +1332,7 @@ class ReadElf:
                 self._emitline('  Data alignment factor: %d' % entry['data_alignment_factor'])
                 self._emitline('  Return address column: %d' % entry['return_address_register'])
                 if entry.augmentation_bytes:
-                    self._emitline('  Augmentation data:     {}'.format(' '.join(
-                        '{:02x}'.format(ord(b))
-                        for b in iterbytes(entry.augmentation_bytes)
-                    )))
+                    self._emitline(f'  Augmentation data:     {entry.augmentation_bytes.hex(" ")}')
                 self._emitline()
 
             elif isinstance(entry, FDE):
@@ -1351,10 +1348,7 @@ class ReadElf:
                         entry['initial_location'] + entry['address_range'],
                         fullhex=True, lead0x=False)))
                 if entry.augmentation_bytes:
-                    self._emitline('  Augmentation data:     {}'.format(' '.join(
-                        '{:02x}'.format(ord(b))
-                        for b in iterbytes(entry.augmentation_bytes)
-                    )))
+                    self._emitline(f'  Augmentation data:     {entry.augmentation_bytes.hex(" ")}')
 
             else: # ZERO terminator
                 assert isinstance(entry, ZERO)

test/run_dwarfdump_tests.py
@@ -69,13 +69,13 @@ def run_test_on_file(filename, verbose=False, opt=None):
         stdouts = []
         for exe_path in [DWARFDUMP_PATH, 'scripts/dwarfdump.py']:
             args = [option, '--verbose', filename]
-            if verbose: testlog.info("....executing: '%s %s'" % (
-                exe_path, ' '.join(args)))
+            cmd = " ".join((exe_path, *args))
+            if verbose: testlog.info(f"....executing: '{cmd}'")
             t1 = time.time()
             rc, stdout = run_exe(exe_path, args)
             if verbose: testlog.info("....elapsed: %s" % (time.time() - t1,))
             if rc != 0:
-                testlog.error("@@ aborting - '%s %s' returned '%s'" % (exe_path, option, rc))
+                testlog.error(f"@@ aborting - '{cmd}' returned {rc}")
                 return False
             stdouts.append(stdout)
         if verbose: testlog.info('....comparing output...')

test/run_readelf_tests.py
@@ -117,12 +117,12 @@ def run_test_on_file(filename, verbose=False, opt=None):
         for exe_path in [READELF_PATH, 'scripts/readelf.py']:
             args = [option, filename]
             cmd = " ".join((exe_path, *args))
-            if verbose: testlog.info("....executing: '%s'" % (cmd,))
+            if verbose: testlog.info(f"....executing: '{cmd}'")
             t1 = time.time()
             rc, stdout = run_exe(exe_path, args)
             if verbose: testlog.info("....elapsed: %s" % (time.time() - t1,))
             if rc != 0:
-                testlog.error("@@ aborting - '%s' returned '%s'" % (cmd, rc))
+                testlog.error(f"@@ aborting - '{cmd}' returned {rc}")
                 return False
             stdouts.append(stdout)
         if verbose: testlog.info('....comparing output...')

test/test_dwarf_expr.py
@@ -72,13 +72,13 @@ class TestParseExpr(unittest.TestCase):
 
     def test_single(self):
         p = DWARFExprParser(self.structs32)
-        lst = p.parse_expr([0x1b])
+        lst = p.parse_expr(b'\x1b')
         self.assertEqual(lst, [DWARFExprOp(op=0x1B, op_name='DW_OP_div', args=[], offset=0)])
 
-        lst = p.parse_expr([0x90, 16])
+        lst = p.parse_expr(b'\x90\x10')
         self.assertEqual(lst, [DWARFExprOp(op=0x90, op_name='DW_OP_regx', args=[16], offset=0)])
 
-        lst = p.parse_expr([0xe0])
+        lst = p.parse_expr(b'\xe0')
         self.assertEqual(len(lst), 1)
         # 0xe0 maps to both DW_OP_GNU_push_tls_address and DW_OP_lo_user, so
         # check for both to prevent non-determinism.
@@ -96,7 +96,7 @@ class TestParseExpr(unittest.TestCase):
         # lit4
         # minus
         # deref
-        lst = p.parse_expr([0x97,0x6,0x12,0x28,0x4,0x0,0x30,0x2F,0x3,0x0,0x34,0x1C,0x6])
+        lst = p.parse_expr(b'\x97\x06\x12\x28\x04\x00\x30\x2F\x03\x00\x34\x1C\x06')
         self.assertEqual(len(lst), 9)
         self.assertEqual(lst, [
             DWARFExprOp(op=0x97, op_name='DW_OP_push_object_address', args=[], offset=0),
@@ -111,7 +111,7 @@ class TestParseExpr(unittest.TestCase):
 
         # This expression blob came from the test binary in issue 508,
         # DT_TAG_variable at 0x2a48C, DW_AT_location
-        lst = p.parse_expr([0x5f, 0xf0])
+        lst = p.parse_expr(b'\x5f\xf0')
         self.assertEqual(len(lst), 2)
         self.assertEqual(lst, [
             DWARFExprOp(op=0x5f, op_name='DW_OP_reg15', args=[], offset=0),

test/test_dwarf_formdata16.py
@@ -25,7 +25,7 @@ class TestFormData16(unittest.TestCase):
             self.assertEqual(lp.header.file_name_entry_format[2].content_type, 'DW_LNCT_MD5')
             # The correct hash value was taken from llvm-dwarfdump output
             hash = lp.header.file_names[0]['DW_LNCT_MD5']
-            hash = ''.join("%02x" % b for b in hash)
+            hash = bytes(hash).hex()
             self.assertEqual(hash, '00dbc7f4edc56417c80f1aa512c4c051')
 
 