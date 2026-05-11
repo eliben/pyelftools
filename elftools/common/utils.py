eliben/pyelftools

commit 8dd0aac2cf64607d502448381235522618628513

More cleanups before adding type hints (#641)
* test_debuglink: Fix existing type annotation

Fix returning tuple.
Make annotations a str.
Sort imports: ruff check --select I --fix

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* run_readelf_tests: Print filename on error

Print the input filename in case of errors:
> @@ aborting - scripts/readelf.py --debug-dump=frames-interp returned 1

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `reveal_type` for type change

`reveal_type` is first declared as `TypeDesc` and then changed to `str`,
which static type checkers like `mypy` do not like.

Rename the first variable.

PS: Better rename `reveal_type()` to somethings else as there is
`typing.reveal_type()`[^1].

[^1]: https://docs.python.org/3/library/typing.html#typing.reveal_type

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `all_offsets` for type change

`all_offsets` is first declared as a `set` and then changed to `list`,
which static type checkers like `mypy` do not like.

Rename the second variable to `sorted_offsets`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* Rework get_location_list_at_offset() logic

Combine the `if` statements to help type checking: Otherwise the 2nd
DWARF-5-case need another case to check for `die not None`.

BREAKING CHANGE:
This is a behavioral change as the file position gets changed in the
error case.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Build `params` via generator expression

`params` was first declared as a `tuple` and then changed to `str`,
which static type checkers like `mypy`/`pyright`/`pyrefly`/`ty` do not
like.

Instead build the concatenated string from a generator expression.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Make elffile.section_names a list

`section_names` is only used internally as a `list`, as section names
might get modified and the list is appended.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Fix print format

elftools/elf/descriptions.py
@@ -327,12 +327,15 @@ def describe_note_gnu_properties(properties, machine):
                 prop_desc = ' <corrupt length: 0x%x>' % sz
             else:
                 prop_desc = describe_note_gnu_property_bitmap_and(_DESCR_NOTE_GNU_PROPERTY_RISCV_FEATURE_1_AND, 'RISC-V AND feature', d)
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
-            prop_desc = '<processor-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
-            prop_desc = '<application-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+        elif isinstance(t, int):
+            if _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
+                prop_desc = f"<processor-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
+                prop_desc = f"<application-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            else:
+                prop_desc = f"<unknown type {t:#x} data: {bytes2hex(d, sep=' ')} >"
         else:
-            prop_desc = '<unknown type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+            prop_desc = "<unknown type {t:r} data: {bytes2hex(d, sep=' ')} >"
         descriptions.append(prop_desc)
     return '\n        '.join(descriptions)
 

elftools/dwarf/callframe.py
@@ -135,6 +135,8 @@ class CallFrameInfo:
 
         # If the augmentation string is not empty, hope to find a length field
         # in order to skip the data specified augmentation.
+        lsda_pointer = None
+        aug_dict = None
         if is_CIE:
             aug_bytes, aug_dict = self._parse_cie_augmentation(
                     header, entry_structs)
@@ -147,8 +149,6 @@ class CallFrameInfo:
                 lsda_pointer = self._parse_lsda_pointer(entry_structs,
                                                         self.stream.tell() - len(aug_bytes),
                                                         lsda_encoding)
-            else:
-                lsda_pointer = None
 
         # For convenience, compute the end offset for this entry
         end_offset = (
@@ -495,6 +495,7 @@ class CFIEntry:
         """ Decode the instructions contained in the given CFI entry and return
             a DecodedCallFrameTable.
         """
+        last_line_in_CIE = None
         if isinstance(self, CIE):
             # For a CIE, initialize cur_line to an "empty" line
             cie = self

elftools/dwarf/datatype_cpp.py
@@ -60,12 +60,15 @@ def parse_cpp_datatype(var_die):
             ptr_prefix = ''
 
         if t.tag == 'subroutine':
-            params = tuple(format_function_param(p, p) for p in type_die.iter_children() if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes)
-            params = ", ".join(params)
+            params = ", ".join(
+                format_function_param(p, p)
+                for p in type_die.iter_children()
+                if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes
+            )
             if 'DW_AT_type' in type_die.attributes:
-                retval_type = parse_cpp_datatype(type_die)
-                is_pointer = retval_type.modifiers and retval_type.modifiers[-1] == 'pointer'
-                retval_type = str(retval_type)
+                datatype = parse_cpp_datatype(type_die)
+                is_pointer = datatype.modifiers and datatype.modifiers[-1] == 'pointer'
+                retval_type = str(datatype)
                 if not is_pointer:
                     retval_type += " "
             else:

elftools/dwarf/locationlists.py
@@ -92,10 +92,12 @@ class LocationLists:
         Passing the die is only neccessary in DWARF5+, for decoding
         location entry encodings that contain references to other sections.
         """
-        if self.version >= 5 and die is None:
-            raise DWARFError("For this binary, \"die\" needs to be provided")
         self.stream.seek(offset, os.SEEK_SET)
-        return self._parse_location_list_from_stream_v5(die.cu) if self.version >= 5 else self._parse_location_list_from_stream()
+        if self.version >= 5:
+            if die is None:
+                raise DWARFError("For this binary, \"die\" needs to be provided")
+            return self._parse_location_list_from_stream_v5(die.cu)
+        return self._parse_location_list_from_stream()
 
     def iter_location_lists(self):
         """ Iterates through location lists and view pairs. Returns lists of
@@ -151,8 +153,7 @@ class LocationLists:
                             list_offset = attr.value
                             all_offsets.add(list_offset)
                             cu_map[list_offset] = cu
-        all_offsets = list(all_offsets)
-        all_offsets.sort()
+        sorted_offsets = sorted(all_offsets)
 
         if ver5:
             # Loclists section is organized as an array of CUs, each length prefixed.
@@ -172,7 +173,7 @@ class LocationLists:
 
                 while stream.tell() < cu_end_offset:
                     # Skip the gap to the next object
-                    next_offset = all_offsets[offset_index]
+                    next_offset = sorted_offsets[offset_index]
                     if next_offset == stream.tell(): # At an object, either a loc list or a loc view pair
                         locview_pairs = self._parse_locview_pairs(locviews)
                         entries = self._parse_location_list_from_stream_v5(cu_map[stream.tell()])
@@ -183,7 +184,7 @@ class LocationLists:
                             next_offset = cu_end_offset # And implicitly quit the loop within the CU
                         stream.seek(next_offset, os.SEEK_SET)
         else:
-            for offset in all_offsets:
+            for offset in sorted_offsets:
                 list_offset = locviews.get(offset, offset)
                 if cu_map[list_offset].header.version < 5:
                     stream.seek(offset, os.SEEK_SET)

elftools/ehabi/ehabiinfo.py
@@ -154,11 +154,16 @@ class EHABIEntry:
             return None
 
     def __repr__(self):
-        return "<EHABIEntry function_offset=0x%x, personality=%d, %sbytecode=%s>" % (
-            self.function_offset,
-            self.personality,
-            "eh_table_offset=0x%x, " % self.eh_table_offset if self.eh_table_offset else "",
-            self.bytecode_array)
+        fo = self.function_offset
+        to = self.eh_table_offset
+        return (
+	    "<EHABIEntry"
+            f" function_offset={'' if fo is None else '{fo:#x}'}"
+            f", personaality={self.personality}"
+            f"{', eh_table_offset={to:#x}' if to else ''}"
+            f", bytecode={self.bytecode_array}"
+            ">"
+        )
 
 
 class CorruptEHABIEntry(EHABIEntry):

elftools/elf/elffile.py
@@ -309,20 +309,21 @@ class ELFFile:
                 # is relative to the other file's directory as opposed to this file's directory.
                 return ext_elffile.get_dwarf_info(relocate_dwarf_sections=relocate_dwarf_sections, follow_links=True)
 
-        section_names = ('.debug_info', '.debug_aranges', '.debug_abbrev',
+        section_names = ['.debug_info', '.debug_aranges', '.debug_abbrev',
                          '.debug_str', '.debug_line', '.debug_frame',
                          '.debug_loc', '.debug_ranges', '.debug_pubtypes',
                          '.debug_pubnames', '.debug_addr',
                          '.debug_str_offsets', '.debug_line_str',
                          '.debug_loclists', '.debug_rnglists',
-                         '.debug_sup', '.gnu_debugaltlink', '.debug_types')
+                         '.debug_sup', '.gnu_debugaltlink', '.debug_types',
+                         ]
 
         compressed = self.has_section('.zdebug_info')
         if compressed:
-            section_names = tuple(map(lambda x: '.z' + x[1:], section_names))
+            section_names = [f'.z{s[1:]}' for s in section_names]
 
         # As it is loaded in the process image, .eh_frame cannot be compressed
-        section_names += ('.eh_frame', )
+        section_names.append('.eh_frame')
 
         (debug_info_sec_name, debug_aranges_sec_name, debug_abbrev_sec_name,
          debug_str_sec_name, debug_line_sec_name, debug_frame_sec_name,

elftools/elf/sections.py
@@ -90,9 +90,13 @@ class Section:
 
                 decomp = zlib.decompressobj()
                 result = decomp.decompress(compressed, self.data_size)
-            else:
+            elif isinstance(c_type, int):
                 raise ELFCompressionError(
                     'Unknown compression type: {:#0x}'.format(c_type)
+                )
+            else:
+                raise ELFCompressionError(
+                    'Unknown compression type: {!r}'.format(c_type)
                 )
 
             if len(result) != self._decompressed_size:
@@ -298,9 +302,14 @@ class StabSection(Section):
 class Attribute:
     """ Attribute object - representing a build attribute of ELF files.
     """
-    def __init__(self, tag):
-        self._tag = tag
+
+    def __init__(self, structs, stream):
+        self._tag = self._parse(structs, stream)
         self.extra = None
+
+    @classmethod
+    def _parse(cls, structs, stream):
+        raise NotImplementedError
 
     @property
     def tag(self):
@@ -316,11 +325,10 @@ class Attribute:
 class AttributesSubsubsection(Section):
     """ Subsubsection of an ELF attribute section's subsection.
     """
-    def __init__(self, stream, structs, offset, attribute):
+    def __init__(self, stream, structs, offset):
         self.stream = stream
         self.offset = offset
         self.structs = structs
-        self.attribute = attribute
 
         self.header = self.attribute(self.structs, self.stream)
 
@@ -365,13 +373,14 @@ class AttributesSubsubsection(Section):
 class AttributesSubsection(Section):
     """ Subsection of an ELF attributes section.
     """
-    def __init__(self, stream, structs, offset, header, subsubsection):
+    subsubsection = AttributesSubsubsection
+
+    def __init__(self, stream, structs, offset):
         self.stream = stream
         self.offset = offset
         self.structs = structs
-        self.subsubsection = subsubsection
 
-        self.header = struct_parse(header, self.stream, self.offset)
+        self.header = struct_parse(structs.Elf_Attr_Subsection_Header, self.stream, self.offset)
 
         self.subsubsec_start = self.stream.tell()
 
@@ -422,9 +431,10 @@ class AttributesSubsection(Section):
 class AttributesSection(Section):
     """ ELF attributes section.
     """
-    def __init__(self, header, name, elffile, subsection):
+    subsection = AttributesSubsection
+
+    def __init__(self, header, name, elffile):
         super().__init__(header, name, elffile)
-        self.subsection = subsection
 
         fv = struct_parse(self.structs.Elf_byte('format_version'),
                           self.stream,
@@ -472,9 +482,13 @@ class AttributesSection(Section):
 class ARMAttribute(Attribute):
     """ ARM attribute object - representing a build attribute of ARM ELF files.
     """
+
+    @classmethod
+    def _parse(cls, structs, stream):
+        return struct_parse(structs.Elf_Arm_Attribute_Tag, stream)
+
     def __init__(self, structs, stream):
-        super().__init__(
-            struct_parse(structs.Elf_Arm_Attribute_Tag, stream))
+        super().__init__(structs, stream)
 
         if self.tag in ('TAG_FILE', 'TAG_SECTION', 'TAG_SYMBOL'):
             self.value = struct_parse(structs.Elf_word('value'), stream)
@@ -514,35 +528,31 @@ class ARMAttribute(Attribute):
 class ARMAttributesSubsubsection(AttributesSubsubsection):
     """ Subsubsection of an ELF .ARM.attributes section's subsection.
     """
-    def __init__(self, stream, structs, offset):
-        super().__init__(
-            stream, structs, offset, ARMAttribute)
+    attribute = ARMAttribute
 
 
 class ARMAttributesSubsection(AttributesSubsection):
     """ Subsection of an ELF .ARM.attributes section.
     """
-    def __init__(self, stream, structs, offset):
-        super().__init__(
-            stream, structs, offset,
-            structs.Elf_Attr_Subsection_Header,
-            ARMAttributesSubsubsection)
+    subsubsection = ARMAttributesSubsubsection
 
 
 class ARMAttributesSection(AttributesSection):
     """ ELF .ARM.attributes section.
     """
-    def __init__(self, header, name, elffile):
-        super().__init__(
-            header, name, elffile, ARMAttributesSubsection)
+    subsection = ARMAttributesSubsection
 
 
 class RISCVAttribute(Attribute):
     """ Attribute of an ELF .riscv.attributes section.
     """
+
+    @classmethod
+    def _parse(cls, structs, stream):
+        return struct_parse(structs.Elf_RiscV_Attribute_Tag, stream)
+
     def __init__(self, structs, stream):
-        super().__init__(
-            struct_parse(structs.Elf_RiscV_Attribute_Tag, stream))
+        super().__init__(structs, stream)
 
         if self.tag in ('TAG_FILE', 'TAG_SECTION', 'TAG_SYMBOL'):
             self.value = struct_parse(structs.Elf_word('value'), stream)
@@ -568,24 +578,16 @@ class RISCVAttribute(Attribute):
 class RISCVAttributesSubsubsection(AttributesSubsubsection):
     """ Subsubsection of an ELF .riscv.attributes subsection.
     """
-    def __init__(self, stream, structs, offset):
-        super().__init__(
-            stream, structs, offset, RISCVAttribute)
+    attribute = RISCVAttribute
 
 
 class RISCVAttributesSubsection(AttributesSubsection):
     """ Subsection of an ELF .riscv.attributes section.
     """
-    def __init__(self, stream, structs, offset):
-        super().__init__(
-            stream, structs, offset,
-            structs.Elf_Attr_Subsection_Header,
-            RISCVAttributesSubsubsection)
+    subsubsection = RISCVAttributesSubsubsection
 
 
 class RISCVAttributesSection(AttributesSection):
     """ ELF .riscv.attributes section.
     """
-    def __init__(self, header, name, elffile):
-        super().__init__(
-            header, name, elffile, RISCVAttributesSubsection)
+    subsection = RISCVAttributesSubsection

test/run_readelf_tests.py
@@ -116,13 +116,13 @@ def run_test_on_file(filename, verbose=False, opt=None):
         stdouts = []
         for exe_path in [READELF_PATH, 'scripts/readelf.py']:
             args = [option, filename]
-            if verbose: testlog.info("....executing: '%s %s'" % (
-                exe_path, ' '.join(args)))
+            cmd = " ".join((exe_path, *args))
+            if verbose: testlog.info("....executing: '%s'" % (cmd,))
             t1 = time.time()
             rc, stdout = run_exe(exe_path, args)
             if verbose: testlog.info("....elapsed: %s" % (time.time() - t1,))
             if rc != 0:
-                testlog.error("@@ aborting - '%s %s' returned '%s'" % (exe_path, option, rc))
+                testlog.error("@@ aborting - '%s' returned '%s'" % (cmd, rc))
                 return False
             stdouts.append(stdout)
         if verbose: testlog.info('....comparing output...')

commit ec9db76591841449fc4bb6722b67f88e49693823

common: Deprecate bytes2hex()
Use `bytes(..).hex(sep)`, which is available since Python 3.8.
<https://docs.python.org/3/library/stdtypes.html#bytes.hex>

Replace several more open-coded versions of it.

Convert to f-strings while we go and fix one broken conversion.

Fixes: 8dd0aac ("More cleanups before adding type hints (#641)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/common/utils.py
@@ -132,10 +132,6 @@ def iterbytes(b: bytes) -> Iterator[bytes]:
     for i in range(len(b)):
         yield b[i:i+1]
 
-def bytes2hex(b: bytes, sep: str = '') -> str:
-    if not sep:
-        return b.hex()
-    return sep.join(f'{o:02x}' for o in b)
 
 #------------------------- PRIVATE -------------------------
 

elftools/construct/lib/hex.py
@@ -22,9 +22,9 @@ def hexdump(data: bytes, linesize: int) -> list[str]:
     fmt = fmt % (3 * linesize - 1,)
     for i in range(0, len(data), linesize):
         line = data[i : i + linesize]
-        hextext = " ".join('%02x' % b for b in line)
+        hextext = line.hex(" ")
         rawtext = "".join(_printable[b] for b in line)
-        prettylines.append(fmt % (i, str(hextext), str(rawtext)))
+        prettylines.append(fmt % (i, hextext, rawtext))
     return prettylines
 
 

elftools/elf/descriptions.py
@@ -15,7 +15,6 @@ from .enums import (
     ENUM_DT_FLAGS_1, ENUM_RELOC_TYPE_PPC)
 from .constants import (
     P_FLAGS, RH_FLAGS, SH_FLAGS, SUNW_SYMINFO_FLAGS, VER_FLAGS)
-from ..common.utils import bytes2hex
 
 
 def describe_ei_class(x):
@@ -212,7 +211,7 @@ def describe_note(x, machine):
                 _DESCR_NOTE_ABI_TAG_OS.get(n_desc['abi_os'], _unknown),
                 n_desc['abi_major'], n_desc['abi_minor'], n_desc['abi_tiny'])
         else:
-            desc = '\n   description data: %s ' % bytes2hex(x['n_descdata'])
+            desc = f'\n   description data: {bytes(x["n_descdata"]).hex()} '
     elif x['n_type'] == 'NT_GNU_BUILD_ID':
         desc = '\n    Build ID: %s' % (n_desc)
     elif x['n_type'] == 'NT_GNU_GOLD_VERSION':
@@ -221,9 +220,9 @@ def describe_note(x, machine):
         if x['n_name'] == 'GNU':
             desc = '\n      Properties: ' + describe_note_gnu_properties(x['n_desc'], machine)
         else:
-            desc = '\n   description data: %s ' % bytes2hex(x['n_descdata'])
+            desc = f'\n   description data: {bytes(x["n_descdata"]).hex()} '
     else:
-        desc = '\n      description data: {}'.format(bytes2hex(n_desc))
+        desc = f'\n      description data: {bytes(n_desc).hex()}'
 
     if x['n_type'] == 'NT_GNU_ABI_TAG' and x['n_name'] == 'Android':
         note_type = 'NT_VERSION'
@@ -329,13 +328,13 @@ def describe_note_gnu_properties(properties, machine):
                 prop_desc = describe_note_gnu_property_bitmap_and(_DESCR_NOTE_GNU_PROPERTY_RISCV_FEATURE_1_AND, 'RISC-V AND feature', d)
         elif isinstance(t, int):
             if _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
-                prop_desc = f"<processor-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<processor-specific type {t:#x} data: {bytes(d).hex(' ')} >"
             elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
-                prop_desc = f"<application-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<application-specific type {t:#x} data: {bytes(d).hex(' ')} >"
             else:
-                prop_desc = f"<unknown type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+                prop_desc = f"<unknown type {t:#x} data: {bytes(d).hex(' ')} >"
         else:
-            prop_desc = "<unknown type {t:r} data: {bytes2hex(d, sep=' ')} >"
+            prop_desc = f"<unknown type {t:r} data: {bytes(d).hex(' ')} >"
         descriptions.append(prop_desc)
     return '\n        '.join(descriptions)
 

elftools/elf/notes.py
@@ -6,7 +6,7 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-from ..common.utils import struct_parse, bytes2hex, roundup, bytes2str
+from ..common.utils import struct_parse, roundup, bytes2str
 from ..construct import CString
 
 
@@ -41,7 +41,7 @@ def iter_notes(elffile, offset, size):
                                           elffile.stream,
                                           offset)
         elif note['n_type'] == 'NT_GNU_BUILD_ID' and note['n_name'] == 'GNU':
-            note['n_desc'] = bytes2hex(desc_data)
+            note['n_desc'] = bytes(desc_data).hex()
         elif note['n_type'] == 'NT_GNU_GOLD_VERSION' and note['n_name'] == 'GNU':
             note['n_desc'] = bytes2str(desc_data)
         elif note['n_type'] == 'NT_PRPSINFO':

examples/elf_notes.py
@@ -15,7 +15,6 @@ sys.path[0:0] = ['.', '..']
 
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import NoteSection
-from elftools.common.utils import bytes2hex
 
 
 def process_file(filename):
@@ -39,7 +38,7 @@ def process_file(filename):
                 elif note['n_type'] in {'NT_GNU_BUILD_ID', 'NT_GNU_GOLD_VERSION'}:
                     print('    Desc:', desc)
                 else:
-                    print('    Desc:', bytes2hex(desc))
+                    print('    Desc:', bytes(desc).hex())
 
 
 if __name__ == '__main__':

scripts/dwarfdump.py
@@ -121,9 +121,9 @@ FORM_DESCRIPTIONS = dict(
     DW_FORM_data2=_desc_data,
     DW_FORM_data4=_desc_data,
     DW_FORM_data8=_desc_data,
-    DW_FORM_block1=lambda attr, die: "<0x%02x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
-    DW_FORM_block2=lambda attr, die: "<0x%04x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
-    DW_FORM_block4=lambda attr, die: "<0x%08x> %s " % (len(attr.value), " ".join("%02x" %b for b in attr.value)),
+    DW_FORM_block1=lambda attr, die: "<0x%02x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
+    DW_FORM_block2=lambda attr, die: "<0x%04x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
+    DW_FORM_block4=lambda attr, die: "<0x%08x> %s " % (len(attr.value), bytes(attr.value).hex(" ")),
     DW_FORM_ref=_desc_ref,
     DW_FORM_ref1=_desc_ref, DW_FORM_ref2=_desc_ref,
     DW_FORM_ref4=_desc_ref, DW_FORM_ref8=_desc_ref,

scripts/readelf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, '.')
 
 from elftools import __version__
 from elftools.common.exceptions import ELFError
-from elftools.common.utils import bytes2str, iterbytes
+from elftools.common.utils import bytes2str
 from elftools.elf.elffile import ELFFile
 from elftools.elf.dynamic import DynamicSection
 from elftools.elf.enums import ENUM_D_TAG
@@ -1332,10 +1332,7 @@ class ReadElf:
                 self._emitline('  Data alignment factor: %d' % entry['data_alignment_factor'])
                 self._emitline('  Return address column: %d' % entry['return_address_register'])
                 if entry.augmentation_bytes:
-                    self._emitline('  Augmentation data:     {}'.format(' '.join(
-                        '{:02x}'.format(ord(b))
-                        for b in iterbytes(entry.augmentation_bytes)
-                    )))
+                    self._emitline(f'  Augmentation data:     {entry.augmentation_bytes.hex(" ")}')
                 self._emitline()
 
             elif isinstance(entry, FDE):
@@ -1351,10 +1348,7 @@ class ReadElf:
                         entry['initial_location'] + entry['address_range'],
                         fullhex=True, lead0x=False)))
                 if entry.augmentation_bytes:
-                    self._emitline('  Augmentation data:     {}'.format(' '.join(
-                        '{:02x}'.format(ord(b))
-                        for b in iterbytes(entry.augmentation_bytes)
-                    )))
+                    self._emitline(f'  Augmentation data:     {entry.augmentation_bytes.hex(" ")}')
 
             else: # ZERO terminator
                 assert isinstance(entry, ZERO)

test/test_dwarf_formdata16.py
@@ -25,7 +25,7 @@ class TestFormData16(unittest.TestCase):
             self.assertEqual(lp.header.file_name_entry_format[2].content_type, 'DW_LNCT_MD5')
             # The correct hash value was taken from llvm-dwarfdump output
             hash = lp.header.file_names[0]['DW_LNCT_MD5']
-            hash = ''.join("%02x" % b for b in hash)
+            hash = bytes(hash).hex()
             self.assertEqual(hash, '00dbc7f4edc56417c80f1aa512c4c051')
 
 