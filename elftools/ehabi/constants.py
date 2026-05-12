eliben/pyelftools

commit 8dd0aac2cf64607d502448381235522618628513

More cleanups before adding type hints (#641)
* test_debuglink: Fix existing type annotation

Fix returning tuple.
Make annotations a str.
Sort imports: ruff check --select I --fix

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* run_readelf_tests: Print filename on error

Print the input filename in case of errors:
> @@ aborting - scripts/readelf.py --debug-dump=frames-interp returned 1

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `reveal_type` for type change

`reveal_type` is first declared as `TypeDesc` and then changed to `str`,
which static type checkers like `mypy` do not like.

Rename the first variable.

PS: Better rename `reveal_type()` to somethings else as there is
`typing.reveal_type()`[^1].

[^1]: https://docs.python.org/3/library/typing.html#typing.reveal_type

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* mypy: Rename `all_offsets` for type change

`all_offsets` is first declared as a `set` and then changed to `list`,
which static type checkers like `mypy` do not like.

Rename the second variable to `sorted_offsets`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* Rework get_location_list_at_offset() logic

Combine the `if` statements to help type checking: Otherwise the 2nd
DWARF-5-case need another case to check for `die not None`.

BREAKING CHANGE:
This is a behavioral change as the file position gets changed in the
error case.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Build `params` via generator expression

`params` was first declared as a `tuple` and then changed to `str`,
which static type checkers like `mypy`/`pyright`/`pyrefly`/`ty` do not
like.

Instead build the concatenated string from a generator expression.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Make elffile.section_names a list

`section_names` is only used internally as a `list`, as section names
might get modified and the list is appended.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Fix print format

elftools/ehabi/ehabiinfo.py
@@ -154,11 +154,16 @@ class EHABIEntry:
             return None
 
     def __repr__(self):
-        return "<EHABIEntry function_offset=0x%x, personality=%d, %sbytecode=%s>" % (
-            self.function_offset,
-            self.personality,
-            "eh_table_offset=0x%x, " % self.eh_table_offset if self.eh_table_offset else "",
-            self.bytecode_array)
+        fo = self.function_offset
+        to = self.eh_table_offset
+        return (
+	    "<EHABIEntry"
+            f" function_offset={'' if fo is None else '{fo:#x}'}"
+            f", personaality={self.personality}"
+            f"{', eh_table_offset={to:#x}' if to else ''}"
+            f", bytecode={self.bytecode_array}"
+            ">"
+        )
 
 
 class CorruptEHABIEntry(EHABIEntry):

elftools/dwarf/callframe.py
@@ -135,6 +135,8 @@ class CallFrameInfo:
 
         # If the augmentation string is not empty, hope to find a length field
         # in order to skip the data specified augmentation.
+        lsda_pointer = None
+        aug_dict = None
         if is_CIE:
             aug_bytes, aug_dict = self._parse_cie_augmentation(
                     header, entry_structs)
@@ -147,8 +149,6 @@ class CallFrameInfo:
                 lsda_pointer = self._parse_lsda_pointer(entry_structs,
                                                         self.stream.tell() - len(aug_bytes),
                                                         lsda_encoding)
-            else:
-                lsda_pointer = None
 
         # For convenience, compute the end offset for this entry
         end_offset = (
@@ -495,6 +495,7 @@ class CFIEntry:
         """ Decode the instructions contained in the given CFI entry and return
             a DecodedCallFrameTable.
         """
+        last_line_in_CIE = None
         if isinstance(self, CIE):
             # For a CIE, initialize cur_line to an "empty" line
             cie = self

elftools/dwarf/datatype_cpp.py
@@ -60,12 +60,15 @@ def parse_cpp_datatype(var_die):
             ptr_prefix = ''
 
         if t.tag == 'subroutine':
-            params = tuple(format_function_param(p, p) for p in type_die.iter_children() if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes)
-            params = ", ".join(params)
+            params = ", ".join(
+                format_function_param(p, p)
+                for p in type_die.iter_children()
+                if p.tag in ("DW_TAG_formal_parameter", "DW_TAG_unspecified_parameters") and 'DW_AT_artificial' not in p.attributes
+            )
             if 'DW_AT_type' in type_die.attributes:
-                retval_type = parse_cpp_datatype(type_die)
-                is_pointer = retval_type.modifiers and retval_type.modifiers[-1] == 'pointer'
-                retval_type = str(retval_type)
+                datatype = parse_cpp_datatype(type_die)
+                is_pointer = datatype.modifiers and datatype.modifiers[-1] == 'pointer'
+                retval_type = str(datatype)
                 if not is_pointer:
                     retval_type += " "
             else:

elftools/dwarf/locationlists.py
@@ -92,10 +92,12 @@ class LocationLists:
         Passing the die is only neccessary in DWARF5+, for decoding
         location entry encodings that contain references to other sections.
         """
-        if self.version >= 5 and die is None:
-            raise DWARFError("For this binary, \"die\" needs to be provided")
         self.stream.seek(offset, os.SEEK_SET)
-        return self._parse_location_list_from_stream_v5(die.cu) if self.version >= 5 else self._parse_location_list_from_stream()
+        if self.version >= 5:
+            if die is None:
+                raise DWARFError("For this binary, \"die\" needs to be provided")
+            return self._parse_location_list_from_stream_v5(die.cu)
+        return self._parse_location_list_from_stream()
 
     def iter_location_lists(self):
         """ Iterates through location lists and view pairs. Returns lists of
@@ -151,8 +153,7 @@ class LocationLists:
                             list_offset = attr.value
                             all_offsets.add(list_offset)
                             cu_map[list_offset] = cu
-        all_offsets = list(all_offsets)
-        all_offsets.sort()
+        sorted_offsets = sorted(all_offsets)
 
         if ver5:
             # Loclists section is organized as an array of CUs, each length prefixed.
@@ -172,7 +173,7 @@ class LocationLists:
 
                 while stream.tell() < cu_end_offset:
                     # Skip the gap to the next object
-                    next_offset = all_offsets[offset_index]
+                    next_offset = sorted_offsets[offset_index]
                     if next_offset == stream.tell(): # At an object, either a loc list or a loc view pair
                         locview_pairs = self._parse_locview_pairs(locviews)
                         entries = self._parse_location_list_from_stream_v5(cu_map[stream.tell()])
@@ -183,7 +184,7 @@ class LocationLists:
                             next_offset = cu_end_offset # And implicitly quit the loop within the CU
                         stream.seek(next_offset, os.SEEK_SET)
         else:
-            for offset in all_offsets:
+            for offset in sorted_offsets:
                 list_offset = locviews.get(offset, offset)
                 if cu_map[list_offset].header.version < 5:
                     stream.seek(offset, os.SEEK_SET)

elftools/elf/descriptions.py
@@ -327,12 +327,15 @@ def describe_note_gnu_properties(properties, machine):
                 prop_desc = ' <corrupt length: 0x%x>' % sz
             else:
                 prop_desc = describe_note_gnu_property_bitmap_and(_DESCR_NOTE_GNU_PROPERTY_RISCV_FEATURE_1_AND, 'RISC-V AND feature', d)
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
-            prop_desc = '<processor-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
-        elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
-            prop_desc = '<application-specific type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+        elif isinstance(t, int):
+            if _DESCR_NOTE_GNU_PROPERTY_TYPE_LOPROC <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIPROC:
+                prop_desc = f"<processor-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            elif _DESCR_NOTE_GNU_PROPERTY_TYPE_LOUSER <= t <= _DESCR_NOTE_GNU_PROPERTY_TYPE_HIUSER:
+                prop_desc = f"<application-specific type {t:#x} data: {bytes2hex(d, sep=' ')} >"
+            else:
+                prop_desc = f"<unknown type {t:#x} data: {bytes2hex(d, sep=' ')} >"
         else:
-            prop_desc = '<unknown type 0x%x data: %s >' % (t, bytes2hex(d, sep=' '))
+            prop_desc = "<unknown type {t:r} data: {bytes2hex(d, sep=' ')} >"
         descriptions.append(prop_desc)
     return '\n        '.join(descriptions)
 

elftools/elf/elffile.py
@@ -309,20 +309,21 @@ class ELFFile:
                 # is relative to the other file's directory as opposed to this file's directory.
                 return ext_elffile.get_dwarf_info(relocate_dwarf_sections=relocate_dwarf_sections, follow_links=True)
 
-        section_names = ('.debug_info', '.debug_aranges', '.debug_abbrev',
+        section_names = ['.debug_info', '.debug_aranges', '.debug_abbrev',
                          '.debug_str', '.debug_line', '.debug_frame',
                          '.debug_loc', '.debug_ranges', '.debug_pubtypes',
                          '.debug_pubnames', '.debug_addr',
                          '.debug_str_offsets', '.debug_line_str',
                          '.debug_loclists', '.debug_rnglists',
-                         '.debug_sup', '.gnu_debugaltlink', '.debug_types')
+                         '.debug_sup', '.gnu_debugaltlink', '.debug_types',
+                         ]
 
         compressed = self.has_section('.zdebug_info')
         if compressed:
-            section_names = tuple(map(lambda x: '.z' + x[1:], section_names))
+            section_names = [f'.z{s[1:]}' for s in section_names]
 
         # As it is loaded in the process image, .eh_frame cannot be compressed
-        section_names += ('.eh_frame', )
+        section_names.append('.eh_frame')
 
         (debug_info_sec_name, debug_aranges_sec_name, debug_abbrev_sec_name,
          debug_str_sec_name, debug_line_sec_name, debug_frame_sec_name,

commit f2d436c2bf072df0addbc015f7605676cd55dc83

Type hints for elftools.ehabi (#645)
* typing[ehabi]: Add / fix PEP-484 type hints

- [x] `pyright 1.1.409`
- [x] `mypy 1.20.2` with several issues:
  > Cannot determine type of "dwarfinfo"
  > Cannot determine type of "structs"
- [x] `pyrefly 0.63.1`
- [x] `ty 0.0.33`
- [x] `typeguard`: create the file
  `.venv/lib/python3.12/site-packages/sitecustomize.py` with line
  `__import__("typeguard").install_import_hook("elftools")`

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing[ehabi]: Hint static attributes

Several classes inheriting from `Construct`, which only has a generic
`__getattr__(self, name: str) -> Any`.
To improve typing explicitly name several attributes with their type.

For `name` we know that it (almost) never will be `None` – an unnamed
entity does not make much sense. Overwrite the type-hint `str | None`
inherited from `Construct` with just `str` which saves us from a ton if
`name is not None` checks.

Fixes: 8dd0aac ("More cleanups before adding type hints (#641)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

---------

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/ehabi/constants.py
@@ -1,1 +1,1 @@
-EHABI_INDEX_ENTRY_SIZE = 8
+EHABI_INDEX_ENTRY_SIZE: int = 8

elftools/ehabi/decoder.py
@@ -30,13 +30,13 @@ class EHABIBytecodeDecoder:
 
     """
 
-    def __init__(self, bytecode_array):
+    def __init__(self, bytecode_array: list[int]) -> None:
         self._bytecode_array = bytecode_array
-        self._index = None
-        self.mnemonic_array = None
+        self._index: int = 0
+        self.mnemonic_array: list[MnemonicItem] | None = None
         self._decode()
 
-    def _decode(self):
+    def _decode(self) -> None:
         """ Decode bytecode array, put result into mnemonic_array.
         """
         self._index = 0
@@ -51,14 +51,14 @@ class EHABIBytecodeDecoder:
                         MnemonicItem(self._bytecode_array[start_idx: end_idx], mnemonic))
                     break
 
-    def _decode_00xxxxxx(self):
+    def _decode_00xxxxxx(self) -> str:
         #   SW.startLine() << format("0x%02X      ; vsp = vsp + %u\n", Opcode,
         #                            ((Opcode & 0x3f) << 2) + 4);
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'vsp = vsp + %u' % (((opcode & 0x3f) << 2) + 4)
 
-    def _decode_01xxxxxx(self):
+    def _decode_01xxxxxx(self) -> str:
         # SW.startLine() << format("0x%02X      ; vsp = vsp - %u\n", Opcode,
         #                          ((Opcode & 0x3f) << 2) + 4);
         opcode = self._bytecode_array[self._index]
@@ -68,18 +68,18 @@ class EHABIBytecodeDecoder:
     gpr_register_names = ("r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
                           "r8", "r9", "r10", "fp", "ip", "sp", "lr", "pc")
 
-    def _calculate_range(self, start, count):
+    def _calculate_range(self, start: int, count: int) -> int:
         return ((1 << (count + 1)) - 1) << start
 
-    def _printGPR(self, gpr_mask):
+    def _printGPR(self, gpr_mask: int) -> str:
         hits = [self.gpr_register_names[i] for i in range(32) if gpr_mask & (1 << i) != 0]
         return '{%s}' % ', '.join(hits)
 
-    def _print_registers(self, vfp_mask, prefix):
+    def _print_registers(self, vfp_mask: int, prefix: str) -> str:
         hits = [prefix + str(i) for i in range(32) if vfp_mask & (1 << i) != 0]
         return '{%s}' % ', '.join(hits)
 
-    def _decode_1000iiii_iiiiiiii(self):
+    def _decode_1000iiii_iiiiiiii(self) -> str:
         op0 = self._bytecode_array[self._index]
         self._index += 1
         op1 = self._bytecode_array[self._index]
@@ -96,40 +96,40 @@ class EHABIBytecodeDecoder:
         else:
             return 'pop %s' % self._printGPR(gpr_mask)
 
-    def _decode_10011101(self):
+    def _decode_10011101(self) -> str:
         self._index += 1
         return 'reserved (ARM MOVrr)'
 
-    def _decode_10011111(self):
+    def _decode_10011111(self) -> str:
         self._index += 1
         return 'reserved (WiMMX MOVrr)'
 
-    def _decode_1001nnnn(self):
+    def _decode_1001nnnn(self) -> str:
         # SW.startLine() << format("0x%02X      ; vsp = r%u\n", Opcode, (Opcode & 0x0f));
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'vsp = r%u' % (opcode & 0x0f)
 
-    def _decode_10100nnn(self):
+    def _decode_10100nnn(self) -> str:
         # SW.startLine() << format("0x%02X      ; pop ", Opcode);
         # PrintGPR((((1 << ((Opcode & 0x7) + 1)) - 1) << 4));
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'pop %s' % self._printGPR(self._calculate_range(4, opcode & 0x07))
 
-    def _decode_10101nnn(self):
+    def _decode_10101nnn(self) -> str:
         # SW.startLine() << format("0x%02X      ; pop ", Opcode);
         # PrintGPR((((1 << ((Opcode & 0x7) + 1)) - 1) << 4) | (1 << 14));
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'pop %s' % self._printGPR(self._calculate_range(4, opcode & 0x07) | (1 << 14))
 
-    def _decode_10110000(self):
+    def _decode_10110000(self) -> str:
         # SW.startLine() << format("0x%02X      ; finish\n", Opcode);
         self._index += 1
         return 'finish'
 
-    def _decode_10110001_0000iiii(self):
+    def _decode_10110001_0000iiii(self) -> str:
         # SW.startLine()
         #   << format("0x%02X 0x%02X ; %s", Opcode0, Opcode1,
         #             ((Opcode1 & 0xf0) || Opcode1 == 0x00) ? "spare" : "pop ");
@@ -143,7 +143,7 @@ class EHABIBytecodeDecoder:
         else:
             return 'pop %s' % self._printGPR(op1 & 0x0f)
 
-    def _decode_10110010_uleb128(self):
+    def _decode_10110010_uleb128(self) -> str:
         #  SmallVector<uint8_t, 4> ULEB;
         #  do { ULEB.push_back(Opcodes[OI ^ 3]); } while (Opcodes[OI++ ^ 3] & 0x80);
         #  uint64_t Value = 0;
@@ -161,21 +161,21 @@ class EHABIBytecodeDecoder:
             value = (value << 7) + (b & 0x7F)
         return 'vsp = vsp + %u' % (0x204 + (value << 2))
 
-    def _decode_10110011_sssscccc(self):
+    def _decode_10110011_sssscccc(self) -> str:
         # these two decoders are equal
         return self._decode_11001001_sssscccc()
 
-    def _decode_101101nn(self):
+    def _decode_101101nn(self) -> str:
         return self._spare()
 
-    def _decode_10111nnn(self):
+    def _decode_10111nnn(self) -> str:
         #  SW.startLine() << format("0x%02X      ; pop ", Opcode);
         #  PrintRegisters((((1 << ((Opcode & 0x07) + 1)) - 1) << 8), "d");
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'pop %s' % self._print_registers(self._calculate_range(8, opcode & 0x07), "d")
 
-    def _decode_11000110_sssscccc(self):
+    def _decode_11000110_sssscccc(self) -> str:
         #  SW.startLine() << format("0x%02X 0x%02X ; pop ", Opcode0, Opcode1);
         #  uint8_t Start = ((Opcode1 & 0xf0) >> 4);
         #  uint8_t Count = ((Opcode1 & 0x0f) >> 0);
@@ -187,7 +187,7 @@ class EHABIBytecodeDecoder:
         count = ((op1 & 0x0f) >> 0)
         return 'pop %s' % self._print_registers(self._calculate_range(start, count), "wR")
 
-    def _decode_11000111_0000iiii(self):
+    def _decode_11000111_0000iiii(self) -> str:
         #   SW.startLine()
         #     << format("0x%02X 0x%02X ; %s", Opcode0, Opcode1,
         #               ((Opcode1 & 0xf0) || Opcode1 == 0x00) ? "spare" : "pop ");
@@ -201,7 +201,7 @@ class EHABIBytecodeDecoder:
         else:
             return 'pop %s' % self._print_registers(op1 & 0x0f, "wCGR")
 
-    def _decode_11001000_sssscccc(self):
+    def _decode_11001000_sssscccc(self) -> str:
         #   SW.startLine() << format("0x%02X 0x%02X ; pop ", Opcode0, Opcode1);
         #   uint8_t Start = 16 + ((Opcode1 & 0xf0) >> 4);
         #   uint8_t Count = ((Opcode1 & 0x0f) >> 0);
@@ -213,7 +213,7 @@ class EHABIBytecodeDecoder:
         count = ((op1 & 0x0f) >> 0)
         return 'pop %s' % self._print_registers(self._calculate_range(start, count), "d")
 
-    def _decode_11001001_sssscccc(self):
+    def _decode_11001001_sssscccc(self) -> str:
         #   SW.startLine() << format("0x%02X 0x%02X ; pop ", Opcode0, Opcode1);
         #   uint8_t Start = ((Opcode1 & 0xf0) >> 4);
         #   uint8_t Count = ((Opcode1 & 0x0f) >> 0);
@@ -225,24 +225,24 @@ class EHABIBytecodeDecoder:
         count = ((op1 & 0x0f) >> 0)
         return 'pop %s' % self._print_registers(self._calculate_range(start, count), "d")
 
-    def _decode_11001yyy(self):
+    def _decode_11001yyy(self) -> str:
         return self._spare()
 
-    def _decode_11000nnn(self):
+    def _decode_11000nnn(self) -> str:
         #   SW.startLine() << format("0x%02X      ; pop ", Opcode);
         #   PrintRegisters((((1 << ((Opcode & 0x07) + 1)) - 1) << 10), "wR");
         opcode = self._bytecode_array[self._index]
         self._index += 1
         return 'pop %s' % self._print_registers(self._calculate_range(10, opcode & 0x07), "wR")
 
-    def _decode_11010nnn(self):
+    def _decode_11010nnn(self) -> str:
         # these two decoders are equal
         return self._decode_10111nnn()
 
-    def _decode_11xxxyyy(self):
+    def _decode_11xxxyyy(self) -> str:
         return self._spare()
 
-    def _spare(self):
+    def _spare(self) -> str:
         self._index += 1
         return 'spare'
 
@@ -281,9 +281,9 @@ class MnemonicItem:
     """ Single mnemonic item.
     """
 
-    def __init__(self, bytecode, mnemonic):
+    def __init__(self, bytecode: list[int], mnemonic: str) -> None:
         self.bytecode = bytecode
         self.mnemonic = mnemonic
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '%s ; %s' % (' '.join(['0x%02x' % x for x in self.bytecode]), self.mnemonic)

elftools/ehabi/ehabiinfo.py
@@ -6,12 +6,19 @@
 # LeadroyaL (leadroyal@qq.com)
 # This code is in the public domain
 # -------------------------------------------------------------------------------
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from ..common.utils import struct_parse
 
 from .decoder import EHABIBytecodeDecoder
 from .constants import EHABI_INDEX_ENTRY_SIZE
 from .structs import EHABIStructs
+
+if TYPE_CHECKING:
+    from ..elf.sections import Section
+    from .decoder import MnemonicItem
 
 
 class EHABIInfo:
@@ -26,25 +33,25 @@ class EHABIInfo:
                 bool, endianness of elf file.
     """
 
-    def __init__(self, arm_idx_section, little_endian):
+    def __init__(self, arm_idx_section: Section, little_endian: bool) -> None:
         self._arm_idx_section = arm_idx_section
         self._struct = EHABIStructs(little_endian)
-        self._num_entry = None
+        self._num_entry: int | None = None
 
-    def section_name(self):
+    def section_name(self) -> str:
         return self._arm_idx_section.name
 
-    def section_offset(self):
+    def section_offset(self) -> int:
         return self._arm_idx_section['sh_offset']
 
-    def num_entry(self):
+    def num_entry(self) -> int:
         """ Number of exception handler entry in the section.
         """
         if self._num_entry is None:
             self._num_entry = self._arm_idx_section['sh_size'] // EHABI_INDEX_ENTRY_SIZE
         return self._num_entry
 
-    def get_entry(self, n):
+    def get_entry(self, n: int) -> EHABIEntry:
         """ Get the exception handler entry at index #n. (EHABIEntry object or a subclass)
         """
         if n >= self.num_entry():
@@ -134,12 +141,12 @@ class EHABIEntry:
     """
 
     def __init__(self,
-                 function_offset,
-                 personality,
-                 bytecode_array,
-                 eh_table_offset=None,
-                 unwindable=True,
-                 corrupt=False):
+                 function_offset: int | None,
+                 personality: int | None,
+                 bytecode_array: list[int] | None,
+                 eh_table_offset: int | None = None,
+                 unwindable: bool = True,
+                 corrupt: bool = False) -> None:
         self.function_offset = function_offset
         self.personality = personality
         self.bytecode_array = bytecode_array
@@ -147,17 +154,17 @@ class EHABIEntry:
         self.unwindable = unwindable
         self.corrupt = corrupt
 
-    def mnmemonic_array(self):
+    def mnmemonic_array(self) -> list[MnemonicItem] | None:
         if self.bytecode_array:
             return EHABIBytecodeDecoder(self.bytecode_array).mnemonic_array
         else:
             return None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         fo = self.function_offset
         to = self.eh_table_offset
         return (
-	    "<EHABIEntry"
+            "<EHABIEntry"
             f" function_offset={'' if fo is None else '{fo:#x}'}"
             f", personaality={self.personality}"
             f"{', eh_table_offset={to:#x}' if to else ''}"
@@ -170,12 +177,12 @@ class CorruptEHABIEntry(EHABIEntry):
     """ This entry is corrupt. Attribute #corrupt will be True.
     """
 
-    def __init__(self, reason):
+    def __init__(self, reason: str) -> None:
         super().__init__(function_offset=None, personality=None, bytecode_array=None,
                                                 corrupt=True)
         self.reason = reason
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<CorruptEHABIEntry reason=%s>" % self.reason
 
 
@@ -183,11 +190,14 @@ class CannotUnwindEHABIEntry(EHABIEntry):
     """ This function cannot be unwind. Attribute #unwindable will be False.
     """
 
-    def __init__(self, function_offset):
+    if TYPE_CHECKING:
+        function_offset: int  # instead of `int|None` to save `is None` checks everywhere
+
+    def __init__(self, function_offset: int) -> None:
         super().__init__(function_offset, personality=None, bytecode_array=None,
                                                      unwindable=False)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<CannotUnwindEHABIEntry function_offset=0x%x>" % self.function_offset
 
 
@@ -195,14 +205,18 @@ class GenericEHABIEntry(EHABIEntry):
     """ This entry is generic model rather than ARM compact model.Attribute #bytecode_array will be None.
     """
 
-    def __init__(self, function_offset, personality):
+    if TYPE_CHECKING:
+        function_offset: int  # instead of `int|None` to save `is None` checks everywhere
+        personality: int
+
+    def __init__(self, function_offset: int, personality: int) -> None:
         super().__init__(function_offset, personality, bytecode_array=None)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "<GenericEHABIEntry function_offset=0x%x, personality=0x%x>" % (self.function_offset, self.personality)
 
 
-def arm_expand_prel31(address, place):
+def arm_expand_prel31(address: int, place: int) -> int:
     """
        address: uint32
        place: uint32

elftools/ehabi/structs.py
@@ -21,11 +21,11 @@ class EHABIStructs:
                 Struct of item in section .ARM.extab.
     """
 
-    def __init__(self, little_endian):
+    def __init__(self, little_endian: bool) -> None:
         self._little_endian = little_endian
         self._create_structs()
 
-    def _create_structs(self):
+    def _create_structs(self) -> None:
         if self._little_endian:
             self.EHABI_uint32 = ULInt32
         else:
@@ -33,14 +33,14 @@ class EHABIStructs:
         self._create_exception_handler_index()
         self._create_exception_handler_table()
 
-    def _create_exception_handler_index(self):
+    def _create_exception_handler_index(self) -> None:
         self.EH_index_struct = Struct(
             'EH_index',
             self.EHABI_uint32('word0'),
             self.EHABI_uint32('word1')
         )
 
-    def _create_exception_handler_table(self):
+    def _create_exception_handler_table(self) -> None:
         self.EH_table_struct = Struct(
             'EH_table',
             self.EHABI_uint32('word0'),