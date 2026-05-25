eliben/pyelftools

commit db2c15b355b6d4b6306290ebb983f9d0d0b47316

Typing elftools.dwarf (#649)
* typing: Add some Protocols

`pyelftools` does not use class hierarchies in all cases, but relies on
_duck typing_.
Introduce Protocols for those cases to allow type hinting.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Add / fix PEP-484 type hints

- [x] `pyright 1.1.409`
- [x] `mypy 1.20.2` with several issues:
  > Cannot determine type of "dwarfinfo"
  > Cannot determine type of "structs"
- [x] `pyrefly 0.63.1`
- [x] `ty 0.0.33`
- [x] `typeguard`: create the file
  `.venv/lib/python3.12/site-packages/sitecustomize.py` with line
  `__import__("typeguard").install_import_hook("elftools")`

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Make dwarf_expr constant

Build DW_OP_name2opcode with dict-comprehension and mark it as constant.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Assert not None

Several variable may have an `Optional` type and can be `None`.
Explicitly assert that those variables are `not None`.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* typing: Assert isinstance

Several variable may have a `Union` type.
Explicitly assert that those variables have the expected type.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

---------

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/dwarf/lineprogram.py
@@ -10,10 +10,14 @@ from __future__ import annotations
 
 import os
 import copy
-from typing import NamedTuple
+from typing import IO, TYPE_CHECKING, Any, NamedTuple
 
 from ..common.utils import struct_parse, dwarf_assert
 from .constants import DW_LNE, DW_LNS
+
+if TYPE_CHECKING:
+    from ..construct.lib.container import Container
+    from .structs import DWARFStructs
 
 
 # LineProgramEntry - an entry in the line program.
@@ -85,8 +89,14 @@ class LineProgram:
         sorted by increasing address, so it can be used to obtain the
         state information for each address.
     """
-    def __init__(self, header, stream, structs,
-                 program_start_offset, program_end_offset):
+    def __init__(
+        self,
+        header: Container,
+        stream: IO[bytes],
+        structs: DWARFStructs,
+        program_start_offset: int,
+            program_end_offset: int,
+    ) -> None:
         """
             header:
                 The header of this line program. Note: LineProgram may modify
@@ -110,9 +120,9 @@ class LineProgram:
         self.structs = structs
         self.program_start_offset = program_start_offset
         self.program_end_offset = program_end_offset
-        self._decoded_entries = None
+        self._decoded_entries: list[LineProgramEntry] | None = None
 
-    def get_entries(self):
+    def get_entries(self) -> list[LineProgramEntry]:
         """ Get the decoded entries for this line program. Return a list of
             LineProgramEntry objects.
             Note that this contains more information than absolutely required
@@ -127,12 +137,12 @@ class LineProgram:
 
     #------ PRIVATE ------#
 
-    def __getitem__(self, name):
+    def __getitem__(self, name: str) -> Any:
         """ Implement dict-like access to header entries
         """
         return self.header[name]
 
-    def _decode_line_program(self):
+    def _decode_line_program(self) -> list[LineProgramEntry]:
         entries = []
         state = LineState(self.header['default_is_stmt'])
 

elftools/dwarf/abbrevtable.py
@@ -6,14 +6,24 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
+from __future__ import annotations
+
+from typing import IO, TYPE_CHECKING, Any
+
 from ..common.utils import struct_parse
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from ..construct.lib.container import Container
+    from .structs import DWARFStructs
 
 
 class AbbrevTable:
     """ Represents a DWARF abbreviation table.
     """
     __slots__ = ('structs', 'stream', 'offset', '_abbrev_map')
-    def __init__(self, structs, stream, offset):
+    def __init__(self, structs: DWARFStructs, stream: IO[bytes], offset: int) -> None:
         """ Create new abbreviation table. Parses the actual table from the
             stream and stores it internally.
 
@@ -30,16 +40,16 @@ class AbbrevTable:
 
         self._abbrev_map = self._parse_abbrev_table()
 
-    def get_abbrev(self, code):
+    def get_abbrev(self, code: int) -> AbbrevDecl:
         """ Get the AbbrevDecl for a given code. Raise KeyError if no
             declaration for this code exists.
         """
         return self._abbrev_map[code]
 
-    def _parse_abbrev_table(self):
+    def _parse_abbrev_table(self) -> dict[int, AbbrevDecl]:
         """ Parse the abbrev table from the stream
         """
-        map = {}
+        map: dict[int, AbbrevDecl] = {}
         self.stream.seek(self.offset)
         while True:
             decl_code: int = struct_parse(
@@ -61,7 +71,7 @@ class AbbrevDecl:
         The abbreviation declaration represents an "entry" that points to it.
     """
     __slots__ = ('code', 'decl', '_has_children')
-    def __init__(self, code, decl):
+    def __init__(self, code: int, decl: Container) -> None:
         self.code = code
         self.decl = decl
         self._has_children = decl['children_flag'] == 'DW_CHILDREN_yes'
@@ -69,12 +79,12 @@ class AbbrevDecl:
     def has_children(self) -> bool:
         return self._has_children
 
-    def iter_attr_specs(self):
+    def iter_attr_specs(self) -> Iterator[tuple[str, str]]:
         """ Iterate over the attribute specifications for the entry. Yield
             (name, form) pairs.
         """
         for attr_spec in self['attr_spec']:
             yield attr_spec.name, attr_spec.form
 
-    def __getitem__(self, entry):
+    def __getitem__(self, entry: str) -> Any:
         return self.decl[entry]

elftools/dwarf/aranges.py
@@ -6,11 +6,20 @@
 # Dorothy Chen (dorothchen@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-from typing import NamedTuple
+from __future__ import annotations
+
+from typing import IO, TYPE_CHECKING, NamedTuple
 
 from ..common.utils import struct_parse
 from bisect import bisect_right
 import math
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from ..construct.core import Construct
+    from .structs import DWARFStructs
+
 
 # An entry in the aranges table;
 # begin_addr: The beginning address in the CU
@@ -36,7 +45,7 @@ class ARanges:
         structs:
             A DWARFStructs instance for parsing the data
     """
-    def __init__(self, stream, size, structs):
+    def __init__(self, stream: IO[bytes], size: int, structs: DWARFStructs) -> None:
         self.stream = stream
         self.size = size
         self.structs = structs
@@ -63,7 +72,7 @@ class ARanges:
 
 
     #------ PRIVATE ------#
-    def _get_entries(self, need_empty=False):
+    def _get_entries(self, need_empty: bool = False) -> list[ARangeEntry]:
         """ Populate self.entries with ARangeEntry tuples for each range of addresses
 
             Terminating null entries of CU blocks are not returned, unless
@@ -72,7 +81,7 @@ class ARanges:
             set to 0.
         """
         self.stream.seek(0)
-        entries = []
+        entries: list[ARangeEntry] = []
         offset = 0
 
         # one loop == one "set" == one CU
@@ -122,7 +131,7 @@ class ARanges:
 
         return entries
 
-    def _get_addr_size_struct(self, addr_header_value):
+    def _get_addr_size_struct(self, addr_header_value: int) -> Callable[[str], Construct]:
         """ Given this set's header value (int) for the address size,
             get the Construct representation of that size
         """

commit aec9d6483e3bbc23cf45ebdbab5ae5b8984b3c59

Minor typing cleanups (#655)
* dwarf: Fix wrong indentation

Fixes: db2c15b355b6 ("Typing elftools.dwarf (#649)")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* elf.description: Convert to list-comprehension

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* dynamic: Re-implement get_table_offset()

Use next() to find first tag.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* dynamic: Reimplement DynamicSegment

Use next() to find first element instead of using break from a loop.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* example/dwarf_location_info: Convert to generator

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* readelf: Convert cu_map to dict-comprehension

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

* elffile: Raise exception from _get_section_header()

Raise an IndexError instead of returning None as that requires checking
for None in multiple locations.

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

---------

Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/dwarf/lineprogram.py
@@ -96,7 +96,7 @@ class LineProgram:
         stream: IO[bytes],
         structs: DWARFStructs,
         program_start_offset: int,
-            program_end_offset: int,
+        program_end_offset: int,
     ) -> None:
         """
             header:

elftools/elf/descriptions.py
@@ -288,10 +288,11 @@ def describe_note_gnu_property_bitmap_and(
     prefix: str,
     value: int,
 ) -> str:
-    descs = []
-    for mask, desc in values:
-        if value & mask:
-            descs.append(desc)
+    descs = [
+        desc
+        for mask, desc in values
+        if value & mask
+    ]
     return '%s: %s' % (prefix, ', '.join(descs))
 
 def describe_note_gnu_properties(properties: list[Container], machine: str) -> str:

elftools/elf/dynamic.py
@@ -127,16 +127,17 @@ class Dynamic:
     def get_table_offset(self, tag_name: str) -> tuple[int | None, int | None]:
         """ Return the virtual address and file offset of a dynamic table.
         """
-        ptr: int | None = None
-        for tag in self._iter_tags(type=tag_name):
-            ptr = tag['d_ptr']
-            break
+        try:
+            ptr: int = next(
+                tag['d_ptr']
+                for tag in self._iter_tags(type=tag_name)
+            )
+        except StopIteration:
+            return (None, None)
 
         # If we found a virtual address, locate the offset in the file
         # by using the program headers.
-        offset: int | None = None
-        if ptr:
-            offset = next(self.elffile.address_offsets(ptr), None)
+        offset = next(self.elffile.address_offsets(ptr), None)
 
         return ptr, offset
 
@@ -272,12 +273,14 @@ class DynamicSegment(Segment, Dynamic):
         # So we must look for the dynamic section contained in the dynamic
         # segment, we do so by searching for the dynamic section whose content
         # is located at the same offset as the dynamic segment
-        stringtable = None
-        for section in elffile.iter_sections():
-            if (isinstance(section, DynamicSection) and
-                    section['sh_offset'] == header['p_offset']):
-                stringtable = elffile.get_section(section['sh_link'])
-                break
+        stringtable = next(
+            (
+                elffile.get_section(section['sh_link'])
+                for section in elffile.iter_sections()
+                if isinstance(section, DynamicSection) and section['sh_offset'] == header['p_offset']
+            ),
+            None,
+        )
         Segment.__init__(self, header, stream)
         Dynamic.__init__(self, stream, elffile, stringtable, self['p_offset'],
              self['p_filesz'] == 0)

elftools/elf/elffile.py
@@ -157,7 +157,6 @@ class ELFFile:
         # member of the initial entry contains 0)."
         if self['e_shnum'] == 0:
             section_header = self._get_section_header(0)
-            assert section_header is not None
             return section_header['sh_size']
         return self['e_shnum']
 
@@ -640,7 +639,6 @@ class ELFFile:
             return self['e_shstrndx']
         else:
             section_header = self._get_section_header(0)
-            assert section_header is not None
             return section_header['sh_link']
 
     #-------------------------------- PRIVATE --------------------------------#
@@ -705,13 +703,14 @@ class ELFFile:
         else:
             return Segment(segment_header, self.stream)
 
-    def _get_section_header(self, n: int) -> Container | None:
+    def _get_section_header(self, n: int) -> Container:
         """ Find the header of section #n, parse it and return the struct
         """
 
         stream_pos = self._section_offset(n)
         if stream_pos > self.stream_len:
-            return None
+            msg = f"Reading section {n} at offset {stream_pos} past EOF {self.stream_len}"
+            raise ELFParseError(msg)
 
         return struct_parse(
             self.structs.Elf_Shdr,
@@ -874,9 +873,10 @@ class ELFFile:
         """
         stringtable_section_num = self.get_shstrndx()
 
-        stringtable_section_header = self._get_section_header(stringtable_section_num)
-        if stringtable_section_header is None:
-            raise ELFParseError("String Table not found")
+        try:
+            stringtable_section_header = self._get_section_header(stringtable_section_num)
+        except ELFParseError as ex:
+            raise ELFParseError("String Table not found") from ex
 
         return StringTableSection(
                 header=stringtable_section_header,

examples/dwarf_location_info.py
@@ -93,15 +93,11 @@ def show_loclist(loclist, dwarfinfo, indent, cu_offset):
     """ Display a location list nicely, decoding the DWARF expressions
         contained within.
     """
-    d = []
-    for loc_entity in loclist:
-        if isinstance(loc_entity, LocationEntry):
-            d.append('%s <<%s>>' % (
-                loc_entity,
-                describe_DWARF_expr(loc_entity.loc_expr, dwarfinfo.structs, cu_offset)))
-        else:
-            d.append(str(loc_entity))
-    return '\n'.join(indent + s for s in d)
+    return "\n".join(
+        f"{indent}{loc_entity}"
+        f"{f' <<{describe_DWARF_expr(loc_entity.loc_expr, dwarfinfo.structs, cu_offset)}>>' if isinstance(loc_entity, LocationEntry) else ''}"
+        for loc_entity in loclist
+    )
 
 if __name__ == '__main__':
     if sys.argv[1] == '--test':

scripts/readelf.py
@@ -1581,14 +1581,14 @@ class ReadElf:
         # Scroll through DIEs once, list the known location list offsets.
         # Don't need this CU/DIE scan if all entries are absolute or prefixed by base,
         # but let's not optimize for that yet.
-        cu_map = dict() # Loc list offset => CU
-        for cu in di.iter_CUs():
-            for die in cu.iter_DIEs():
-                for key in die.attributes:
-                    attr = die.attributes[key]
-                    if (LocationParser.attribute_has_location(attr, cu['version']) and
-                        LocationParser._attribute_has_loc_list(attr, cu['version'])):
-                        cu_map[attr.value] = cu
+        cu_map = {  # Loc list offset => CU
+            attr.value: cu
+            for cu in di.iter_CUs()
+            for die in cu.iter_DIEs()
+            for attr in die.attributes.values()
+            if (LocationParser.attribute_has_location(attr, cu['version']) and
+                LocationParser._attribute_has_loc_list(attr, cu['version']))
+        }
 
         addr_size = di.config.default_address_size # In bytes, 4 or 8
         addr_width = addr_size * 2 # In hex digits, 8 or 16