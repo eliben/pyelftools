eliben/pyelftools

commit 12e96365cb7524859753628ae78a7e09b496f82e

Fix a bunch of other ruff check issues

elftools/dwarf/structs.py
@@ -429,7 +429,8 @@ class DWARFStructs:
                     context[self.format_field + "_parser"] = parser
                 return parser._parse(stream, context)
 
-        ver5 = lambda ctx: ctx.version >= 5
+        def ver5(ctx):
+            return ctx.version >= 5
 
         self.Dwarf_lineprog_header = Struct('Dwarf_lineprog_header',
             self.Dwarf_initial_length('unit_length'),

elftools/common/construct_utils.py
@@ -125,8 +125,8 @@ class UBInt24(StaticField):
         StaticField.__init__(self, name, 3)
 
     def _parse(self, stream: IO[bytes], context: Container) -> int:
-        (h, l) = _UBInt24_packer.unpack(StaticField._parse(self, stream, context))
-        return l | (h << 16)
+        (hi, lo) = _UBInt24_packer.unpack(StaticField._parse(self, stream, context))
+        return lo | (hi << 16)
 
     def _build(self, obj: int, stream: IO[bytes], context: Container) -> None:
         StaticField._build(self, _UBInt24_packer.pack(obj >> 16, obj & 0xFFFF), stream, context)
@@ -137,8 +137,8 @@ class ULInt24(StaticField):
         StaticField.__init__(self, name, 3)
 
     def _parse(self, stream: IO[bytes], context: Container) -> int:
-        (l, h) = _ULInt24_packer.unpack(StaticField._parse(self, stream, context))
-        return l | (h << 16)
+        (lo, hi) = _ULInt24_packer.unpack(StaticField._parse(self, stream, context))
+        return lo | (hi << 16)
 
     def _build(self, obj: int, stream: IO[bytes], context: Container) -> None:
         StaticField._build(self, _ULInt24_packer.pack(obj & 0xFFFF, obj >> 16), stream, context)

elftools/elf/dynamic.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 import itertools
 from collections import defaultdict
 from functools import cached_property
-from typing import IO, TYPE_CHECKING, Any, Protocol, TypedDict, cast, runtime_checkable
+from typing import IO, TYPE_CHECKING, Any, Protocol, TypedDict, runtime_checkable
 
 from ..common.exceptions import ELFError
 from ..common.utils import elf_assert, struct_parse, parse_cstring_from_stream

examples/dwarf_decode_address.py
@@ -9,10 +9,6 @@
 #-------------------------------------------------------------------------------
 import sys
 
-# If pyelftools is not installed, the example can also run from the root or
-# examples/ dir of the source distribution.
-sys.path[0:0] = ['.', '..']
-
 from elftools.common.utils import bytes2str
 from elftools.dwarf.descriptions import describe_form_class
 from elftools.elf.elffile import ELFFile

examples/dwarf_die_tree.py
@@ -10,10 +10,6 @@
 from pathlib import Path
 import sys
 
-# If pyelftools is not installed, the example can also run from the root or
-# examples/ dir of the source distribution.
-sys.path[0:0] = ['.', '..']
-
 from elftools.elf.elffile import ELFFile
 
 

examples/dwarf_lineprogram_filenames.py
@@ -12,10 +12,6 @@ from collections import defaultdict
 import sys
 import posixpath
 
-# If pyelftools is not installed, the example can also run from the root or
-# examples/ dir of the source distribution.
-sys.path[0:0] = ['.', '..']
-
 from elftools.elf.elffile import ELFFile
 
 

examples/dwarf_location_info.py
@@ -21,10 +21,6 @@
 #-------------------------------------------------------------------------------
 import sys
 
-# If pyelftools is not installed, the example can also run from the root or
-# examples/ dir of the source distribution.
-sys.path[0:0] = ['.', '..']
-
 from elftools.elf.elffile import ELFFile
 from elftools.dwarf.descriptions import (
     describe_DWARF_expr, set_global_machine_arch)

examples/dwarf_pubnames_types.py
@@ -11,10 +11,6 @@
 #-------------------------------------------------------------------------------
 import sys
 
-# If pyelftools is not installed, the example can also run from the root or
-# examples/ dir of the source distribution.
-sys.path[0:0] = ['.', '..']
-
 from elftools.elf.elffile import ELFFile
 from elftools.common.utils import bytes2str
 

commit c8b576661eac216527bdf086368694f3fb76c412

dwarf/struct: Add missing type information
Fixes: 12e96365cb75 ("Fix a bunch of other ruff check issues")
Signed-off-by: Philipp Hahn <phahn-oss@avm.de>

elftools/dwarf/structs.py
@@ -429,7 +429,7 @@ class DWARFStructs:
                     context[self.format_field + "_parser"] = parser
                 return parser._parse(stream, context)
 
-        def ver5(ctx):
+        def ver5(ctx: Container) -> bool:
             return ctx.version >= 5
 
         self.Dwarf_lineprog_header = Struct('Dwarf_lineprog_header',